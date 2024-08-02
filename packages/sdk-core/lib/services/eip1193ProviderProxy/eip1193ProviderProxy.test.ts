cb0c6880f8bec04fafb9faa724685d6607be190c
not-reed <reed@happy.tech>
2024-08-01 17:16:47 -0700

    fix failing test lint

--- packages/sdk-core/lib/services/eip1193ProviderProxy/eip1193ProviderProxy.test.ts
+++ packages/sdk-core/lib/services/eip1193ProviderProxy/eip1193ProviderProxy.test.ts
@@ -1,150 +1,153 @@
-import { beforeEach, describe, expect, it, mock } from "bun:test";
-import { setTimeout } from "node:timers/promises";
-import type { RpcBlock } from "viem";
-import { config } from "../../config";
-import { EventBusChannel, EventBus, type EventBusOptions } from "../eventBus";
-import { EIP1193ProviderProxy } from "./eip1193ProviderProxy";
-import { GenericProviderRpcError } from "./errors";
-import type { EIP1193ProxiedEvents } from "./events";
+// eslint-disable-next-line import/no-unresolved
+import { beforeEach, describe, expect, it, mock } from 'bun:test'
+import { setTimeout } from 'node:timers/promises'
+import type { RpcBlock } from 'viem'
+
+import { config } from '../../config'
+import { EventBus, EventBusChannel, type EventBusOptions } from '../eventBus'
+
+import { EIP1193ProviderProxy } from './eip1193ProviderProxy'
+import { GenericProviderRpcError } from './errors'
+import type { EIP1193ProxiedEvents } from './events'
 
 const emptyRpcBlock = {
-	baseFeePerGas: null,
-	blobGasUsed: "0x",
-	difficulty: "0x",
-	excessBlobGas: "0x",
-	extraData: "0x",
-	gasLimit: "0x",
-	gasUsed: "0x",
-	hash: null,
-	logsBloom: null,
-	miner: "0x",
-	mixHash: "0x",
-	nonce: null,
-	number: null,
-	parentHash: "0x",
-	receiptsRoot: "0x",
-	sealFields: [],
-	sha3Uncles: "0x",
-	size: "0x",
-	stateRoot: "0x",
-	timestamp: "0x",
-	totalDifficulty: "0x",
-	transactions: [],
-	transactionsRoot: "0x",
-	uncles: [],
-} satisfies RpcBlock;
+    baseFeePerGas: null,
+    blobGasUsed: '0x',
+    difficulty: '0x',
+    excessBlobGas: '0x',
+    extraData: '0x',
+    gasLimit: '0x',
+    gasUsed: '0x',
+    hash: null,
+    logsBloom: null,
+    miner: '0x',
+    mixHash: '0x',
+    nonce: null,
+    number: null,
+    parentHash: '0x',
+    receiptsRoot: '0x',
+    sealFields: [],
+    sha3Uncles: '0x',
+    size: '0x',
+    stateRoot: '0x',
+    timestamp: '0x',
+    totalDifficulty: '0x',
+    transactions: [],
+    transactionsRoot: '0x',
+    uncles: [],
+} satisfies RpcBlock
 
-describe("EIP1193ProviderProxy", () => {
-	let busConfig: EventBusOptions;
-	beforeEach(() => {
-		busConfig = {
-			scope: crypto.randomUUID(),
-			logger: { log: mock(), warn: mock(), error: mock() },
-			mode: EventBusChannel.Broadcast,
-		} satisfies EventBusOptions;
-	});
+describe('EIP1193ProviderProxy', () => {
+    let busConfig: EventBusOptions
+    beforeEach(() => {
+        busConfig = {
+            scope: crypto.randomUUID(),
+            logger: { log: mock(), warn: mock(), error: mock() },
+            mode: EventBusChannel.Broadcast,
+        } satisfies EventBusOptions
+    })
 
-	it("transmits payload using bus", async () => {
-		const iframeBus = new EventBus<EIP1193ProxiedEvents>(busConfig);
-		const providerProxyBus = new EventBus<EIP1193ProxiedEvents>(busConfig);
+    it('transmits payload using bus', async () => {
+        const iframeBus = new EventBus<EIP1193ProxiedEvents>(busConfig)
+        const providerProxyBus = new EventBus<EIP1193ProxiedEvents>(busConfig)
 
-		const provider = new EIP1193ProviderProxy(providerProxyBus, config);
+        const provider = new EIP1193ProviderProxy(providerProxyBus, config)
 
-		const callback = mock(({ key, error, payload }) => {});
+        const callback = mock(({ key, error, payload }) => {})
 
-		const payload = {
-			method: "eth_getBlockByNumber",
-			params: ["latest", false],
-		} as {
-			// make viem happy
-			method: "eth_getBlockByNumber";
-			params: ["latest", false];
-		};
+        const payload = {
+            method: 'eth_getBlockByNumber',
+            params: ['latest', false],
+        } as {
+            // make viem happy
+            method: 'eth_getBlockByNumber'
+            params: ['latest', false]
+        }
 
-		// within iframe
-		iframeBus.on("request:approve", callback);
+        // within iframe
+        iframeBus.on('request:approve', callback)
 
-		// provider request, unanswered so don't await
-		provider.request(payload);
+        // provider request, unanswered so don't await
+        provider.request(payload)
 
-		await setTimeout(0);
+        await setTimeout(0)
 
-		expect(callback).toBeCalledTimes(1);
-		expect(callback.mock.calls[0][0].error).toBe(null);
-		expect(callback.mock.calls[0][0].key).toBeString();
-		expect(callback.mock.calls[0][0].payload).toEqual(payload);
-	});
+        expect(callback).toBeCalledTimes(1)
+        expect(callback.mock.calls[0][0].error).toBe(null)
+        expect(callback.mock.calls[0][0].key).toBeString()
+        expect(callback.mock.calls[0][0].payload).toEqual(payload)
+    })
 
-	it("resolves on success", async () => {
-		const iframeBus = new EventBus<EIP1193ProxiedEvents>(busConfig);
-		const providerProxyBus = new EventBus<EIP1193ProxiedEvents>(busConfig);
+    it('resolves on success', async () => {
+        const iframeBus = new EventBus<EIP1193ProxiedEvents>(busConfig)
+        const providerProxyBus = new EventBus<EIP1193ProxiedEvents>(busConfig)
 
-		const provider = new EIP1193ProviderProxy(providerProxyBus, config);
+        const provider = new EIP1193ProviderProxy(providerProxyBus, config)
 
-		// within iframe
-		iframeBus.on("request:approve", ({ key }) => {
-			iframeBus.emit("provider:request:complete", {
-				key,
-				error: null,
-				payload: emptyRpcBlock,
-			});
-		});
+        // within iframe
+        iframeBus.on('request:approve', ({ key }) => {
+            iframeBus.emit('provider:request:complete', {
+                key,
+                error: null,
+                payload: emptyRpcBlock,
+            })
+        })
 
-		// provider request
-		expect(
-			provider.request({
-				method: "eth_getBlockByNumber",
-				params: ["latest", false],
-			})
-		).resolves.toStrictEqual(emptyRpcBlock);
-	});
+        // provider request
+        expect(
+            provider.request({
+                method: 'eth_getBlockByNumber',
+                params: ['latest', false],
+            }),
+        ).resolves.toStrictEqual(emptyRpcBlock)
+    })
 
-	it("rejects on error", async () => {
-		const iframeBus = new EventBus<EIP1193ProxiedEvents>(busConfig);
-		const providerProxyBus = new EventBus<EIP1193ProxiedEvents>(busConfig);
+    it('rejects on error', async () => {
+        const iframeBus = new EventBus<EIP1193ProxiedEvents>(busConfig)
+        const providerProxyBus = new EventBus<EIP1193ProxiedEvents>(busConfig)
 
-		const provider = new EIP1193ProviderProxy(providerProxyBus, config);
+        const provider = new EIP1193ProviderProxy(providerProxyBus, config)
 
-		// within iframe
-		iframeBus.on("request:approve", ({ key }) => {
-			iframeBus.emit("provider:request:complete", {
-				key,
-				error: {
-					code: 4001,
-					message: "User Rejected",
-					data: "User Rejected ",
-				},
-				payload: null,
-			});
-		});
+        // within iframe
+        iframeBus.on('request:approve', ({ key }) => {
+            iframeBus.emit('provider:request:complete', {
+                key,
+                error: {
+                    code: 4001,
+                    message: 'User Rejected',
+                    data: 'User Rejected ',
+                },
+                payload: null,
+            })
+        })
 
-		// provider request
-		expect(
-			provider.request({
-				method: "eth_getBlockByNumber",
-				params: ["latest", false],
-			})
-		).rejects.toThrowError(GenericProviderRpcError);
-	});
+        // provider request
+        expect(
+            provider.request({
+                method: 'eth_getBlockByNumber',
+                params: ['latest', false],
+            }),
+        ).rejects.toThrowError(GenericProviderRpcError)
+    })
 
-	it("subscribes and unsubscribes to native eip1193 events", async () => {
-		const providerProxyBus = new EventBus<EIP1193ProxiedEvents>(busConfig);
+    it('subscribes and unsubscribes to native eip1193 events', async () => {
+        const providerProxyBus = new EventBus<EIP1193ProxiedEvents>(busConfig)
 
-		const provider = new EIP1193ProviderProxy(providerProxyBus, config);
+        const provider = new EIP1193ProviderProxy(providerProxyBus, config)
 
-		const callback = mock(() => {});
-		provider.on("connect", callback);
+        const callback = mock(() => {})
+        provider.on('connect', callback)
 
-		provider.emit("connect");
+        provider.emit('connect')
 
-		expect(callback).toHaveBeenCalledTimes(1);
+        expect(callback).toHaveBeenCalledTimes(1)
 
-		provider.removeListener("connect", callback);
+        provider.removeListener('connect', callback)
 
-		provider.emit("connect");
-		provider.emit("connect");
-		provider.emit("connect");
+        provider.emit('connect')
+        provider.emit('connect')
+        provider.emit('connect')
 
-		expect(callback).toHaveBeenCalledTimes(1);
-	});
-});
+        expect(callback).toHaveBeenCalledTimes(1)
+    })
+})
