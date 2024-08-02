5b1d50c148ee5870636e3a52f17ba71b4e2bc7fe
not-reed <reed@happy.tech>
2024-08-02 08:27:55 -0700

    rename firebase social logos file

--- packages/sdk-firebase-web3auth-strategy/lib/hooks/useFirebaseWeb3AuthStrategy.ts
+++ packages/sdk-firebase-web3auth-strategy/lib/hooks/useFirebaseWeb3AuthStrategy.ts
@@ -1,37 +1,37 @@
-import type { ConnectionProvider, HappyUser } from "@happychain/core";
+import { useMemo } from 'react'
 
-import { useMemo } from "react";
-import type { EIP1193Provider } from "viem";
-import { firebaseAuth } from "../services/firebase";
-import { web3AuthEvmProvider } from "../services/web3auth";
-import { useFirebaseAuth } from "./useFirebaseAuth";
-import { googleLogo } from "../constants";
+import type { ConnectionProvider, HappyUser } from '@happychain/core'
+import type { EIP1193Provider } from 'viem'
+
+import { googleLogo } from '../logos'
+import { firebaseAuth } from '../services/firebase'
+import { web3AuthEvmProvider } from '../services/web3auth'
+
+import { useFirebaseAuth } from './useFirebaseAuth'
 
 export function useFirebaseWeb3AuthStrategy(): {
-	providers: ConnectionProvider[];
-	onAuthChange: (
-		callback: (user: HappyUser | null, provider: EIP1193Provider) => void
-	) => void;
+    providers: ConnectionProvider[]
+    onAuthChange: (callback: (user: HappyUser | null, provider: EIP1193Provider) => void) => void
 } {
-	const { signIn, signOut, onAuthChange } = useFirebaseAuth(firebaseAuth);
+    const { signIn, signOut, onAuthChange } = useFirebaseAuth(firebaseAuth)
 
-	const providers = useMemo(
-		() => [
-			{
-				type: "social",
-				id: "firebase:google",
-				name: "Google",
-				icon: googleLogo,
-				enable: () => signIn("google"),
-				disable: () => signOut(),
-				getProvider: () => web3AuthEvmProvider,
-			},
-		],
-		[signIn, signOut]
-	);
+    const providers = useMemo(
+        () => [
+            {
+                type: 'social',
+                id: 'firebase:google',
+                name: 'Google',
+                icon: googleLogo,
+                enable: () => signIn('google'),
+                disable: () => signOut(),
+                getProvider: () => web3AuthEvmProvider,
+            },
+        ],
+        [signIn, signOut],
+    )
 
-	return {
-		providers: providers,
-		onAuthChange,
-	};
+    return {
+        providers: providers,
+        onAuthChange,
+    }
 }
