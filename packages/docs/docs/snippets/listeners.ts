dbc6b7befbbb6c4f5bdc96087e31c07c002b72d3
not-reed <reed@happy.tech>
2024-08-20 13:32:00 -0700

    update docs

--- packages/docs/docs/snippets/listeners.ts
+++ packages/docs/docs/snippets/listeners.ts
@@ -1,7 +1,12 @@
-import { onUserUpdate } from '@happychain/js'
+import { getCurrentUser, onUserUpdate } from '@happychain/js'
 
 // [!region onUserUpdate]
 onUserUpdate((user) => {
-    //
+    console.log('HappyChain User: ', user)
 })
 // [!endregion onUserUpdate]
+
+// [!region getCurrentUser]
+const user = getCurrentUser()
+console.log('HappyChain User: ', user)
+// [!endregion getCurrentUser]
