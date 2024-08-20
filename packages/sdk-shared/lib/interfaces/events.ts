14180a09193ba6b0f749e988906807bc209b1431
not-reed <reed@happy.tech>
2024-08-20 13:08:46 -0700

    update injected-wallet:request/response comments

--- packages/sdk-shared/lib/interfaces/events.ts
+++ packages/sdk-shared/lib/interfaces/events.ts
@@ -1,7 +1,9 @@
 import type { HappyUser } from './happyUser'
 
-// When RDNS is undefined a disconnect occurs, when its a string
-// an EIP-6963 lookup occurs for this wallet to connect on dapp side
+/**
+ * When RDNS is undefined a disconnect occurs, when its a string
+ * an EIP-6963 lookup occurs for this wallet to connect on dapp side
+ */
 type WalletRDNS = string | undefined
 
 export interface HappyEvents {
@@ -11,11 +13,19 @@ export interface HappyEvents {
     // user auth
     'auth-changed': HappyUser | undefined
 
-    // iframe requests a connection to a specific injected wallet
-    // dapp attempts to connect with wallet who's rdns matches
+    /**
+     * Sent by the iframe to request a connection or disconnection with an injected wallet.
+     * Received by the dapp, which attempts to connect with the wallet whose rdns matches.
+     */
     'injected-wallet:request': WalletRDNS
 
-    // if user is defined, dapp connects to injected wallet, and returns user details to iframe
-    // if user is undefined, dapp disconnects from injected wallet
+    /**
+     * Sent by the dapp in response to `injected-wallet:request` with the user details obtained from
+     * connecting to the requested injected wallet. If the connection attempt failed, the user will
+     * be undefined.
+     *
+     * Alternatively sent by the dapp as a response to other dapp side events such as
+     * when the injected wallet disconnect or changes account and the iframe needs to be updated
+     */
     'injected-wallet:response': { user?: HappyUser }
 }
