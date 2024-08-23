9df4c1fb8121c53cfe3ca23daa66a3fe7b42cd7c
not-reed <reed@happy.tech>
2024-08-23 08:24:49 -0700

    remove .prettierignore

--- packages/iframe/src/vite-env.d.ts
+++ packages/iframe/src/vite-env.d.ts
@@ -8,7 +8,6 @@ interface ImportMetaEnv {
     readonly VITE_DEFAULT_RPC_URL: string
 }
 
-// biome-ignore lint/correctness/noUnusedVariables: needed for interface augmentation
 interface ImportMeta {
     readonly env: ImportMetaEnv
 }
