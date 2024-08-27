ca70959a1bd8f2738ed35cb8eb21c38d822823c5
not-reed <reed@happy.tech>
2024-08-27 13:55:56 -0700

    add biome to demo-wagmi-vue

--- makefiles/vite.mk
+++ makefiles/vite.mk
@@ -1,19 +1,22 @@
-dev:
+dev: node_modules
 	@./node_modules/.bin/vite
 .PHONY: dev
 
 # Define 'dist' target in package makefile with a list of dependencies
-build: dist
+build: node_modules dist
 .PHONY: build
 
-build.watch:
+build.watch: node_modules
 	@./node_modules/.bin/vite build --watch
 .PHONY: build.watch
 
-preview: dist
+preview: node_modules dist
 	@./node_modules/.bin/vite preview
 .PHONY: preview
 
 dist: $(shell find . -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.css" -o -name "*.json" -o -name "*.js" -o -name "*.html" -o -name "*.vue" \) -not -path "./dist/*")
 	@./node_modules/.bin/tsc -b
 	@./node_modules/.bin/vite build
+
+node_modules: package.json
+	pnpm install
