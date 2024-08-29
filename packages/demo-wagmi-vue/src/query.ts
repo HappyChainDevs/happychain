df053ce937fb13bf539bc3ce9c5ce7d7ded462c0
not-reed <reed@happy.tech>
2024-08-29 14:11:51 -0700

    add vue.js wagmi demo (using vanillajs sdk)

---
+++ packages/demo-wagmi-vue/src/query.ts
@@ -0,0 +1,4 @@
+import { QueryClient } from "@tanstack/vue-query"
+export { VueQueryPlugin } from "@tanstack/vue-query"
+
+export const queryClient = new QueryClient()
--- packages/demo-wagmi-vue/tailwind.config.ts
+++ packages/demo-wagmi-vue/tailwind.config.ts
@@ -1,9 +1,25 @@
 import type { Config } from "tailwindcss"
+import defaultTheme from "tailwindcss/defaultTheme"
 
 export default {
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Pixelify Sans", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "theme-bg": "#0000A8",
+                "theme-text": "#fff",
+                "theme-text-highlight": "#F0F00A",
+                "theme-btn": "#00A8A8",
+                "theme-highlight": "#00A800",
+            },
+            boxShadow: {
+                theme: "10px 10px 0 0 rgba(0,0,0,1)",
+                "theme-sm": "5px 5px 0 0 rgba(0,0,0,1)",
+            },
+        },
     },
     plugins: [],
 } satisfies Config
