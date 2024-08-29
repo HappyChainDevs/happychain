e477847755a09d608108a43338b158e162f91981
not-reed <reed@happy.tech>
2024-08-29 14:11:51 -0700

    add vue.js wagmi demo (using vanillajs sdk)

---
+++ packages/demo-wagmi-vue/src/components/Boolean.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+defineProps<{ bool: boolean }>()
+</script>
+
+<template>
+    <span :class="bool ? 'text-green-500' : 'text-red-500'">{{ bool }}</span>
+</template>
---
+++ packages/demo-wagmi-vue/src/components/Container.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{ title?: string, shadow?: boolean }>()
+</script>
+
+<template>
+  <div class="bg-theme-bg text-white text-thin px-1 pb-2 pt-3 relative" :class="{'shadow-theme': shadow}">
+    <div class="border-4 border-double border-white p-4">
+      <div class="absolute top-0 left-0 right-0">
+        <div v-if="title" class="bg-theme-bg mx-auto w-min whitespace-nowrap px-1 my-1 text-center">{{ title }}</div>
+      </div>
+
+      <slot />
+    </div>
+  </div>
+</template>
