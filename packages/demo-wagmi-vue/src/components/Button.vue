b6f642625424e8f390ce9df5683482c9d619c4ea
not-reed <reed@happy.tech>
2024-08-29 14:11:51 -0700

    add vue.js wagmi demo (using vanillajs sdk)

---
+++ packages/demo-wagmi-vue/src/components/Button.vue
@@ -0,0 +1,5 @@
+<template>
+    <button class="bg-theme-btn text-black px-2 w-full disabled:opacity-75">
+        <slot />
+    </button>
+</template>
--- packages/demo-wagmi-vue/src/main.ts
+++ packages/demo-wagmi-vue/src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue"
+import { VueQueryPlugin, queryClient } from "./query"
+import { WagmiPlugin, config } from "./wagmi"
+import "./style.css"
+import App from "./App.vue"
 
-createApp(App).mount('#app')
+createApp(App) //
+    .use(WagmiPlugin, { config })
+    .use(VueQueryPlugin, { queryClient })
+    .mount("#app")
