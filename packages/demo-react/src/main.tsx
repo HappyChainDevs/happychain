509c12bbeea228c35b131a190c6987a4c47796c5
not-reed <reed@happy.tech>
2024-09-04 14:27:36 -0700

    update global happyProvider demo comment

--- packages/demo-react/src/main.tsx
+++ packages/demo-react/src/main.tsx
@@ -7,7 +7,16 @@ import App from "./App.tsx"
 
 import "./index.css"
 
-// biome-ignore lint/suspicious/noExplicitAny: demo purposes only
+/**
+ * Console demo. to prompt for connection, or login, in the console try typing
+ * await window.happyProvider.request({ method: 'eth_requestAccounts' })
+ * or
+ * await window.happyProvider.request({ method: 'wallet_requestPermissions', params: [{eth_accounts: {}}] })
+ *
+ * to disconnect
+ * await window.happyProvider.request({ method: 'wallet_revokePermissions', params: [{eth_accounts: {}}] })
+ */
+// biome-ignore lint/suspicious/noExplicitAny: demo purposes only. not needed under regular usage
 ;(window as any).happyProvider = happyProvider
 
 // biome-ignore lint/style/noNonNullAssertion: vite boilerplate
