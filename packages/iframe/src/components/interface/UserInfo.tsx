4ab8a3ae4fe40382728d73605c06d8778669f049
ultraviolet10 <aritrac1998@hotmail.com>
2024-09-04 20:44:29 +0530

    decompose wallet view into components

---
+++ packages/iframe/src/components/interface/UserInfo.tsx
@@ -0,0 +1,34 @@
+import type { HappyUser } from "@happychain/sdk-shared";
+
+interface UserProps {
+  user: HappyUser;
+}
+
+const UserInfo = ({ user }: UserProps) => {
+  return (
+    <div className="flex flex-row space-x-4">
+      <img
+        src={user.avatar}
+        alt={`${user.name}'s avatar`}
+        className="h-12 rounded-full"
+      />
+      <div className="flex flex-col items-start justify-between">
+        <p>{user?.email || user?.name}</p>
+        <div className="flex flex-row items-center justify-center space-x-1">
+          <p>{user?.name}</p>
+          <button
+            className="w-4 h-4 rounded-xl opacity-50"
+            onClick={(e) => {
+              e.stopPropagation();
+              navigator.clipboard.writeText(user?.address);
+            }}
+          >
+            <img src={"/wallet-interface/clipboard-text.svg"}></img>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserInfo;
--- packages/iframe/src/routes/connect.lazy.tsx
+++ packages/iframe/src/routes/connect.lazy.tsx
@@ -9,12 +9,14 @@ import { useSocialProviders } from "../hooks/useSocialProviders"
 import { dappMessageBus } from "../services/eventBus"
 import { authStateAtom } from "../state/authState"
 
-import { hasPermission } from "../services/permissions/hasPermission"
 import { userAtom } from "../state/user"
-import { tokenList } from "../utils/lists"
 import { publicClientAtom } from "../state/publicClient"
-import { formatEther } from "viem"
 import { happySepChain } from "../utils/chainConfig"
+import UserInfo from "../components/interface/UserInfo"
+import HappyBalance from "../components/interface/HappyBalance"
+import ActionButtons from "../components/interface/ActionButtons"
+import WalletTabs from "../components/interface/WalletTabs"
+import AppStatus from "../components/interface/AppStatus"
 
 export const Route = createLazyFileRoute("/connect")({
     component: Connect,
@@ -48,13 +50,9 @@ function Connect() {
                 address: user?.address,
             })
         }
-
-        return undefined
     }
 
-    // useEffect to call getBalance when the component mounts
     useEffect(() => {
-        // should there be a constant listener for this?
         const fetchBalance = async () => {
             const balance = await getBalance();
             setHappyBalance(balance);
@@ -99,54 +97,14 @@ function Connect() {
                             <span className="text-black text-[20px]">🤠 HappyChain</span>
 
                             <div className="flex flex-row w-full items-center justify-between gap-2 bg-slate-200 p-2">
-                                <div className="flex flex-row space-x-4">
-                                    <img src={user.avatar} alt={`${user.name}'s avatar`} className="h-12 rounded-full" />
-                                    <div className="flex flex-col items-start justify-between">
-                                        <p>{user?.email || user?.name}</p>
-                                        <div className="flex flex-row items-center justify-center space-x-1">
-                                            <p>{user?.name}</p>
-                                            <button className="w-4 h-4 rounded-xl opacity-50" onClick={(e) => {
-                                                e.preventDefault();
-                                                navigator.clipboard.writeText(user?.address)
-                                            }}>
-                                                <img src={"/wallet-interface/clipboard-text.svg"}></img>
-                                            </button>
-                                        </div>
-                                        
-                                    </div>
-                                </div>
+                                <UserInfo user={user} />
                                 <img className="w-6 h-6 rounded-xl" src={"/wallet-interface/power.svg"} onClick={disconnect}></img>
                             </div>
                             <div className="flex h-full w-full grow flex-col items-start justify-start rounded bg-slate-200 p-2">
-                                <div className="flex flex-row w-full items-center justify-between">
-                                    <p className="text-[18px]">$HAPPY</p>
-                                    <div className="flex flex-col items-center">
-                                        <p className="text-[25px]">{happyBalance ? formatEther(happyBalance) : "--"}</p>
-                                    </div>
-                                </div>
-                                <div className="flex flex-row w-full items-center justify-between px-2 py-4">
-                                    <button className="h-10 w-24 bg-slate-400 rounded-xl">Send</button>
-                                    <button className="h-10 w-24 bg-slate-400 rounded-xl disabled:opacity-80" disabled>Buy / Sell</button>
-                                    <button className="h-10 w-24 bg-slate-400 rounded-xl disabled:opacity-80" disabled>Topup</button>
-                                </div>
-                                <div className="flex w-full h-full items-start justify-center flex-col px-2">
-                                    <div className="flex flex-row items-start justify-center space-x-2">
-                                        <button className="h-10 w-24 bg-slate-300 rounded-t-xl">Tokens</button>
-                                        <button className="h-10 w-24 bg-slate-300 rounded-t-xl disabled:opacity-80" disabled>Games</button>
-                                    </div>
-                                    <div className="flex flex-col w-full h-[80%] p-2 bg-slate-300 rounded-b-xl rounded-tr-xl">
-                                        {tokenList.map((token) => (
-                                            <div className="flex flex-row items-center justify-between px-2 h-12">
-                                                <span>{`${token.name} (${token.symbol})`}</span>
-                                                <span>{`${token.balance}`}</span>
-                                            </div>
-                                            
-                                        ))}
-                                    </div>
-                                </div>
-                                <div className="flex w-full items-center justify-center text-sm font-bold">
-                                    {`${hasPermission({ eth_accounts: {} }) ? "\u2705" : "\u274C"} ${document.referrer ? new URL(document.referrer).host : window.location.origin}`}
-                                </div>
+                                <HappyBalance balance={happyBalance} />
+                                <ActionButtons />
+                                <WalletTabs />
+                                <AppStatus />
                             </div>
                         </div>
                     )}
