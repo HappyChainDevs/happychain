6318d2ae795876b788b423dd6da2658274ba1d37
ultraviolet10 <aritrac1998@hotmail.com>
2024-09-05 12:25:51 +0530

    UI real estate management

--- packages/iframe/src/components/interface/WalletTabs.tsx
+++ packages/iframe/src/components/interface/WalletTabs.tsx
@@ -2,7 +2,7 @@ import { tokenList } from "../../utils/lists";
 
 const WalletTabs = () => {
   return (
-    <div className="flex w-full h-full items-start justify-center flex-col px-2">
+    <div className="flex w-full h-full items-start justify-center flex-col px-1">
       <div className="flex flex-row items-start justify-center space-x-2">
         <button className="h-10 w-24 bg-slate-300 rounded-t-xl">Tokens</button>
         <button
@@ -13,8 +13,8 @@ const WalletTabs = () => {
         </button>
       </div>
       <div className="flex flex-col w-full h-[80%] p-2 bg-slate-300 rounded-b-xl rounded-tr-xl">
-        {tokenList.map((token) => (
-          <div className="flex flex-row items-center justify-between px-2 h-12">
+        {tokenList.map((token, index) => (
+          <div key={index} className="flex flex-row items-center justify-between px-2 h-12">
             <span>{`${token.name} (${token.symbol})`}</span>
             <span>{`${token.balance}`}</span>
           </div>
