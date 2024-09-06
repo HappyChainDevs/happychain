4eb8dc17a5bf9c4de707bc57b6fff1acdf553505
Nicolas "Norswap" Laurent <norswap@gmail.com>
2024-09-07 00:36:12 +0800

    add a semicolon to every command accesses via a Makefile PATH extension

--- makefiles/vite.mk
+++ makefiles/vite.mk
@@ -2,7 +2,7 @@
 PATH := ./node_modules/.bin:$(PATH)
 
 dev: node_modules
-	vite
+	vite;
 .PHONY: dev
 
 # Define 'dist' target in package makefile with a list of dependencies
@@ -10,16 +10,16 @@ build: node_modules dist
 .PHONY: build
 
 build.watch: node_modules
-	vite build --watch
+	vite build --watch;
 .PHONY: build.watch
 
 preview: node_modules dist
-	vite preview
+	vite preview;
 .PHONY: preview
 
 dist: $(shell find . -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.css" -o -name "*.json" -o -name "*.js" -o -name "*.html" -o -name "*.vue" \) -not -path "./dist/*")
-	tsc -b
-	vite build
+	tsc -b;
+	vite build;
 
 node_modules: package.json
 	pnpm install
