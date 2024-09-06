6d60ef03b274d082706212820d4c5e31700232b0
Aryan Godara <aryangodara03@gmail.com>
2024-09-07 00:38:56 +0530

    temp commit with json import in demo

--- packages/contracts/scripts/account_abstraction_demo.ts
+++ packages/contracts/scripts/account_abstraction_demo.ts
@@ -1,53 +1,72 @@
-import "dotenv/config";
+import {config} from "dotenv";
+config();
 
 import {
-    type Hex, createClient, createPublicClient, createWalletClient, http,  formatEther, parseEther, defineChain
-} from 'viem'
-import {type SmartAccount, entryPoint07Address} from "viem/account-abstraction";
-import {generatePrivateKey, privateKeyToAccount, privateKeyToAddress} from 'viem/accounts';
+    http,
+    type Hex,
+    createClient,
+    createPublicClient,
+    createWalletClient,
+    defineChain,
+    formatEther,
+    parseEther,
+} from "viem"
+import { type SmartAccount, entryPoint07Address } from "viem/account-abstraction"
+import { generatePrivateKey, privateKeyToAccount, privateKeyToAddress } from "viem/accounts"
 
-import {type SmartAccountClient, createSmartAccountClient} from "permissionless";
-import {toEcdsaKernelSmartAccount} from "permissionless/accounts"
-import {createPimlicoClient} from 'permissionless/clients/pimlico';
+import { type SmartAccountClient, createSmartAccountClient } from "permissionless"
+import { toEcdsaKernelSmartAccount } from "permissionless/accounts"
+import { createPimlicoClient } from "permissionless/clients/pimlico"
 
-const privateKey = process.env.PRIVATE_KEY_LOCAL;
-const bundlerRpc = process.env.BUNDLER_URL_LOCAL;
-const rpcURL = process.env.RPC_URL_LOCAL;
+const privateKey = process.env.PRIVATE_KEY_LOCAL
+const bundlerRpc = process.env.BUNDLER_URL_LOCAL
+const rpcURL = process.env.RPC_URL_LOCAL
 
 if (!privateKey || !bundlerRpc || !rpcURL) {
-    throw new Error("Please provide PRIVATE_KEY_LOCAL, BUNDLER_URL_LOCAL and RPC_URL_LOCAL in .env file");
+    throw new Error("Please provide PRIVATE_KEY_LOCAL, BUNDLER_URL_LOCAL and RPC_URL_LOCAL in .env file")
 }
+
+// Define a type that matches the structure of deployments.json
+type Deployments = {
+    ECDSAValidator: string
+    Kernel: string
+    KernelFactory: string
+    FactoryStaker: string
+}
+
+import deploymentsJson from "../out/deployment.json" assert { type: 'json' };
+const deployments: Deployments = deploymentsJson;
 
 const chain = defineChain({
     id: 1_337,
-    name: 'Anvil',
+    name: "Anvil",
     nativeCurrency: {
         decimals: 18,
-        name: 'Ether',
-        symbol: 'ETH',
+        name: "Ether",
+        symbol: "ETH",
     },
     rpcUrls: {
         default: {
-            http: ['http://127.0.0.1:8545'],
-            webSocket: ['ws://127.0.0.1:8545'],
+            http: ["http://127.0.0.1:8545"],
+            webSocket: ["ws://127.0.0.1:8545"],
         },
     },
-});
+})
 
-const normalizedPrivateKey: Hex = privateKey.startsWith('0x') ? (privateKey as Hex) : (`0x${privateKey}` as Hex);
-const account = privateKeyToAccount(normalizedPrivateKey);
+const normalizedPrivateKey: Hex = privateKey.startsWith("0x") ? (privateKey as Hex) : (`0x${privateKey}` as Hex)
+const account = privateKeyToAccount(normalizedPrivateKey)
 
 const walletClient = createWalletClient({
     account,
     chain,
     transport: http(rpcURL),
-});
+})
 
 const client = createClient({
     account,
     chain,
     transport: http(rpcURL),
-});
+})
 
 const publicClient = createPublicClient({
     chain,
@@ -61,27 +80,25 @@ const pimlicoClient = createPimlicoClient({
         address: entryPoint07Address,
         version: "0.7",
     },
-});
+})
 
 async function getKernelAccount(): Promise<SmartAccount> {
-
     return await toEcdsaKernelSmartAccount({
         client,
-        entryPoint:{
+        entryPoint: {
             address: entryPoint07Address,
             version: "0.7",
         },
         owners: [account],
         version: "0.3.1",
-        ecdsaValidatorAddress: "0x2cA67e9cF717c3A210574AA1fcDB38C0bE7b6b63",
-        accountLogicAddress: "0x15A1C1a83810984D0d9c26386171878ADa04D034",
-        factoryAddress: "0x560cbC094b16fe929Cf889Ffa59B42e7BB0b86Fa",
-        metaFactoryAddress: "0xF2F43D2eA39c24EeC8Ff985B192935C50e976E4c",
-    });
+        ecdsaValidatorAddress: deployments.ECDSAValidator ? deployments.ECDSAValidator as Hex : "0x2cA67e9cF717c3A210574AA1fcDB38C0bE7b6b63",
+        accountLogicAddress: deployments.Kernel ? deployments.Kernel as Hex : "0x15A1C1a83810984D0d9c26386171878ADa04D034",
+        factoryAddress: deployments.KernelFactory ? deployments.KernelFactory as Hex : "0x560cbC094b16fe929Cf889Ffa59B42e7BB0b86Fa",
+        metaFactoryAddress: deployments.FactoryStaker ? deployments.FactoryStaker as Hex : "0xF2F43D2eA39c24EeC8Ff985B192935C50e976E4c",
+    })
 }
 
 function getKernelClient(kernelAccount: SmartAccount): SmartAccountClient {
-
     return createSmartAccountClient({
         account: kernelAccount,
         chain: chain,
@@ -92,13 +109,13 @@ function getKernelClient(kernelAccount: SmartAccount): SmartAccountClient {
             estimateFeesPerGas: async () => {
                 return (await pimlicoClient.getUserOperationGasPrice()).fast
             },
-        }
-    });
+        },
+    })
 }
 
 async function prefund_smart_account(kernelAccount: SmartAccount): Promise<string> {
-    const accountAddress = await kernelAccount.getAddress();
-    console.log("Funding kernel account address = ", accountAddress);
+    const accountAddress = await kernelAccount.getAddress()
+    console.log("Funding kernel account address = ", accountAddress)
 
     try {
         const txHash = await walletClient.sendTransaction({
@@ -106,45 +123,45 @@ async function prefund_smart_account(kernelAccount: SmartAccount): Promise<strin
             to: accountAddress,
             chain,
             kzg: undefined,
-            value: parseEther('0.1')
-        });
+            value: parseEther("0.1"),
+        })
 
-        console.log(`Prefund transaction hash: ${txHash}`);
+        console.log(`Prefund transaction hash: ${txHash}`)
 
         const receipt = await publicClient.waitForTransactionReceipt({
             hash: txHash,
-            confirmations: 1
+            confirmations: 1,
         })
 
-        return receipt.status;
+        return receipt.status
     } catch (error) {
-        console.error("Error receiving transaction receipt:", error);
+        console.error("Error receiving transaction receipt:", error)
     }
 
-    return "success";
+    return "success"
 }
 
 export function getRandomAccount(): Hex {
     const privateKey = generatePrivateKey()
-    return privateKeyToAddress(privateKey).toString() as Hex;
+    return privateKeyToAddress(privateKey).toString() as Hex
 }
 
-async function main () {
-    const kernelAccount: SmartAccount = await getKernelAccount();
+async function main() {
+    const kernelAccount: SmartAccount = await getKernelAccount()
 
-    const receiverAddress = getRandomAccount();
-    const AMOUNT = '0.001';
+    const receiverAddress = getRandomAccount()
+    const AMOUNT = "0.001"
 
-    console.log("Receiver address = ", receiverAddress);
+    console.log("Receiver address = ", receiverAddress)
 
-    const res = await prefund_smart_account(kernelAccount);
-    if (res === 'success') {
-        console.log('Transaction was successful');
+    const res = await prefund_smart_account(kernelAccount)
+    if (res === "success") {
+        console.log("Transaction was successful")
     } else {
-        console.error('Transaction failed');
+        console.error("Transaction failed")
     }
 
-    const kernelClient = getKernelClient(kernelAccount);
+    const kernelClient = getKernelClient(kernelAccount)
 
     try {
         const txHash = await kernelClient.sendTransaction({
@@ -152,44 +169,45 @@ async function main () {
             to: receiverAddress,
             chain,
             kzg: undefined,
-            value: parseEther(AMOUNT)
-        });
+            value: parseEther(AMOUNT),
+        })
 
-        console.log(`Transaction hash: ${txHash}`);
+        console.log(`Transaction hash: ${txHash}`)
 
         const receipt = await publicClient.waitForTransactionReceipt({
             hash: txHash,
-            confirmations: 1
+            confirmations: 1,
         })
 
-        if (receipt.status === 'success') {
-            console.log('Transaction was successful');
+        if (receipt.status === "success") {
+            console.log("Transaction was successful")
         } else {
-            console.log('Transaction failed');
+            console.log("Transaction failed")
         }
-
     } catch (error) {
-        console.error("Error receiving transaction receipt:", error);
+        console.error("Error receiving transaction receipt:", error)
     }
 
     const balance = await publicClient.getBalance({
         address: receiverAddress,
-        blockTag: 'latest'
+        blockTag: "latest",
     })
 
-    const balanceAsEther = formatEther(balance);
+    const balanceAsEther = formatEther(balance)
 
     if (balanceAsEther === AMOUNT) {
-        console.log('Balance is correct', balanceAsEther, 'ETH');
+        console.log("Balance is correct", balanceAsEther, "ETH")
     } else {
-        console.error('Balance is not correct', balanceAsEther, 'ETH');
+        console.error("Balance is not correct", balanceAsEther, "ETH")
     }
 }
 
-main().then(() => {
-    console.log("Demo Successful.");
-    process.exit(0);  // Explicitly exit with success code
-}).catch(error => {
-    console.error(error);
-    process.exit(1);  // Exit with failure code
-});
+main()
+    .then(() => {
+        console.log("Demo Successful.")
+        process.exit(0) // Explicitly exit with success code
+    })
+    .catch((error) => {
+        console.error(error)
+        process.exit(1) // Exit with failure code
+    })
