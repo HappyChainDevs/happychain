d8e6376faa8f8347824160617e8326cf37da0562
Aryan Godara <aryangodara03@gmail.com>
2024-09-06 16:10:45 +0530

    working script, for anvil, checked with docker geth

--- packages/contracts/scripts/deploy_deterministic_deployer.sh
+++ packages/contracts/scripts/deploy_deterministic_deployer.sh
@@ -2,13 +2,16 @@
 
 set -x
 
-JSON_RPC="http://localhost:1234"
+# After PR review, maybe fetch this from .env file, assuming the RPC is up and running
+JSON_RPC="http://localhost:8545"
 
-# Start geth in a local container
-docker container run --rm -d --name deployment-proxy-geth -p 1234:8545 -e GETH_VERBOSITY=3 keydonix/geth-clique
-
-# Wait for geth to become responsive
-until curl --silent --fail $JSON_RPC -X 'POST' -H 'Content-Type: application/json' --data "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\": \"net_version\", \"params\": []}"; do sleep 1; done
+# Uncomment the following lines to spin up a docker container with geth-clique
+#JSON_RPC="http://localhost:1234"
+## Start geth in a local container
+#docker container run --rm -d --name deployment-proxy-geth -p 1234:8545 -e GETH_VERBOSITY=3 keydonix/geth-clique
+#
+## Wait for geth to become responsive
+#until curl --silent --fail $JSON_RPC -X 'POST' -H 'Content-Type: application/json' --data "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\": \"net_version\", \"params\": []}"; do sleep 1; done
 
 # Variables
 MY_ADDRESS="0x913dA4198E6bE1D5f5E4a40D0667f70C0B5430Eb"
@@ -44,26 +47,29 @@ else
   fi
 fi
 
-# Proceed with further steps
-BYTECODE="6080604052348015600f57600080fd5b5060848061001e6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c8063c3cafc6f14602d575b600080fd5b6033604f565b604051808260ff1660ff16815260200191505060405180910390f35b6000602a90509056fea165627a7a72305820ab7651cb86b8c1487590004c2444f26ae30077a6b96c6bc62dda37f1328539250029"
-MY_CONTRACT_ADDRESS=$(curl $JSON_RPC -X POST -H 'Content-Type: application/json' --silent --data "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\": \"eth_call\", \"params\": [{\"from\":\"$MY_ADDRESS\",\"to\":\"$DEPLOYER_ADDRESS\", \"data\":\"0x0000000000000000000000000000000000000000000000000000000000000000$BYTECODE\"}, \"latest\"]}" | jq --raw-output '.result')
+# Uncomment below code when deploying fresh contract on a new chain, to verify that it works
 
-curl $JSON_RPC -X POST -H 'Content-Type: application/json' --data "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\": \"eth_sendTransaction\", \"params\": [{\"from\":\"$MY_ADDRESS\",\"to\":\"$DEPLOYER_ADDRESS\", \"gas\":\"0xf4240\", \"data\":\"0x0000000000000000000000000000000000000000000000000000000000000000$BYTECODE\"}]}"
+## Proceed with further steps
+#BYTECODE="6080604052348015600f57600080fd5b5060848061001e6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c8063c3cafc6f14602d575b600080fd5b6033604f565b604051808260ff1660ff16815260200191505060405180910390f35b6000602a90509056fea165627a7a72305820ab7651cb86b8c1487590004c2444f26ae30077a6b96c6bc62dda37f1328539250029"
+#MY_CONTRACT_ADDRESS=$(curl $JSON_RPC -X POST -H 'Content-Type: application/json' --silent --data "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\": \"eth_call\", \"params\": [{\"from\":\"$MY_ADDRESS\",\"to\":\"$DEPLOYER_ADDRESS\", \"data\":\"0x0000000000000000000000000000000000000000000000000000000000000000$BYTECODE\"}, \"latest\"]}" | jq --raw-output '.result')
+#
+#curl $JSON_RPC -X POST -H 'Content-Type: application/json' --data "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\": \"eth_sendTransaction\", \"params\": [{\"from\":\"$MY_ADDRESS\",\"to\":\"$DEPLOYER_ADDRESS\", \"gas\":\"0xf4240\", \"data\":\"0x0000000000000000000000000000000000000000000000000000000000000000$BYTECODE\"}]}"
+#
+## Call our contract to verify (NOTE: MY_CONTRACT_ADDRESS is the same no matter what chain we deploy to!)
+#MY_CONTRACT_METHOD_SIGNATURE="c3cafc6f"
+#RAW_RESULT=$(curl --silent $JSON_RPC -X POST -H 'Content-Type: application/json' --data "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\": \"eth_call\", \"params\": [{\"to\":\"$MY_CONTRACT_ADDRESS\", \"data\":\"0x$MY_CONTRACT_METHOD_SIGNATURE\"}, \"latest\"]}")
+#RESULT=$(echo "$RAW_RESULT" | jq --raw-output '.result')
+#
+#echo "Contract call result: $RESULT"
+#
+#EXPECTED_RESULT="0x000000000000000000000000000000000000000000000000000000000000002a"
+#
+#if [ "$RESULT" = "$EXPECTED_RESULT" ]; then
+#  echo "Contract deployed and verified successfully!"
+#else
+#  echo "Deployment failed or contract did not return the expected result."
+#fi
 
-# Call our contract to verify (NOTE: MY_CONTRACT_ADDRESS is the same no matter what chain we deploy to!)
-MY_CONTRACT_METHOD_SIGNATURE="c3cafc6f"
-RAW_RESULT=$(curl --silent $JSON_RPC -X POST -H 'Content-Type: application/json' --data "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\": \"eth_call\", \"params\": [{\"to\":\"$MY_CONTRACT_ADDRESS\", \"data\":\"0x$MY_CONTRACT_METHOD_SIGNATURE\"}, \"latest\"]}")
-RESULT=$(echo "$RAW_RESULT" | jq --raw-output '.result')
-
-echo "Contract call result: $RESULT"
-
-EXPECTED_RESULT="0x000000000000000000000000000000000000000000000000000000000000002a"
-
-if [ "$RESULT" = "$EXPECTED_RESULT" ]; then
-  echo "Contract deployed and verified successfully!"
-else
-  echo "Deployment failed or contract did not return the expected result."
-fi
-
-# Shutdown Parity
-docker container stop deployment-proxy-geth
+# Uncomment the following lines to shutdown the docker container (if started)
+## Shutdown Parity
+#docker container stop deployment-proxy-geth
