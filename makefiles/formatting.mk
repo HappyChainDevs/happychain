5e9e7b3695a5a94707f3a65530c19255918f47b3
Nicolas "Norswap" Laurent <norswap@gmail.com>
2024-09-06 20:14:19 +0800

    fix PATH issue on mac + use dependencies for recursive invocation in top-level Makefile when possible

--- makefiles/formatting.mk
+++ makefiles/formatting.mk
@@ -1,12 +1,15 @@
 # Use the path to the biome version installed in configs directory
 # This ensures all packages are using the same biome version, 
 # along with the same biome config
-PATH := ./node_modules/.bin/:./node_modules/@happychain/configs/node_modules/.bin/:$(PATH)
+PATH := ./node_modules/@happychain/configs/node_modules/.bin:$(PATH)
 
 check:
-	biome check ./
+	@# The semicolon is needed to prevent a Mac-specific issue where the path is not being recognized.
+	@# Might be an issue related to the handling of symlinks.
+	@# https://stackoverflow.com/a/21709821/298664
+	biome check ./;
 .PHONT: check
 
 format:
-	biome check ./ --write
+	biome check ./ --write;
 .PHONT: format
