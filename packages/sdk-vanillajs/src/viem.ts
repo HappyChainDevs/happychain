bbe4488e14d7e316e2ee9b5ab93b3657f2f87584
not-reed <reed@happy.tech>
2024-09-06 14:40:45 -0700

    configure happychain as op stack for viem

--- packages/sdk-vanillajs/src/viem.ts
+++ packages/sdk-vanillajs/src/viem.ts
@@ -1,6 +1,6 @@
+import { convertToViemChain } from "@happychain/sdk-shared"
 import { type AddEthereumChainParameter, createPublicClient, createWalletClient, custom, defineChain } from "viem"
 import * as viemChainObj from "viem/chains"
-
 import { happyProvider } from "../lib"
 
 export const publicClient = createPublicClient({ transport: custom(happyProvider) })
@@ -14,32 +14,4 @@ export const findViemChain = (chain: AddEthereumChainParameter) => {
     if (viemChain) return viemChain
 
     return convertToViemChain(chain)
-}
-
-function convertToViemChain(chain: AddEthereumChainParameter) {
-    const httpRpcs = chain.rpcUrls.filter((a) => a.startsWith("http"))
-    const wsRpcs = chain.rpcUrls.filter((a) => a.startsWith("ws"))
-    return defineChain({
-        id: Number(chain.chainId),
-        name: chain.chainName,
-        nativeCurrency: {
-            decimals: chain.nativeCurrency?.decimals ?? 18,
-            name: chain.nativeCurrency?.name ?? "Ether",
-            symbol: chain.nativeCurrency?.symbol ?? "ETH",
-        },
-        rpcUrls: {
-            default: {
-                http: httpRpcs,
-                webSocket: wsRpcs,
-            },
-        },
-        ...(chain.blockExplorerUrls?.length && {
-            blockExplorers: {
-                default: {
-                    name: `${chain.chainName} Explorer`,
-                    url: chain.blockExplorerUrls[0],
-                },
-            },
-        }),
-    })
 }
