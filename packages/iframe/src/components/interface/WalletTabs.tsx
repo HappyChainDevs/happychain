d71a5728fbe49fd03179081b7fc2096775dd2e8f
ultraviolet10 <aritrac1998@hotmail.com>
2024-09-06 21:57:11 +0530

    add Tab component

--- packages/iframe/src/components/interface/WalletTabs.tsx
+++ packages/iframe/src/components/interface/WalletTabs.tsx
@@ -1,18 +1,13 @@
 import { tokenList } from "../../utils/lists"
+import Tab from "./Tab"
 
 const WalletTabs = () => {
     return (
         <div className="flex w-full h-full items-start justify-center flex-col px-1">
             <div className="flex flex-row items-start justify-center space-x-2">
-                <button className="h-10 w-24 bg-slate-300 rounded-t-xl" type="button">
-                    Tokens
-                </button>
-                <button className="h-10 w-24 bg-slate-300 rounded-t-xl disabled:opacity-80" type="button" disabled>
-                    Games
-                </button>
-                <button className="h-10 w-24 bg-slate-300 rounded-t-xl disabled:opacity-80" type="button" disabled>
-                    History
-                </button>
+                <Tab title={"Tokens"} />
+                <Tab title={"Games"} />
+                <Tab title={"History"} />
             </div>
             <div className="flex flex-col w-full h-4/5 p-2 bg-slate-300 rounded-b-xl rounded-tr-xl">
                 {tokenList.map((token) => (
