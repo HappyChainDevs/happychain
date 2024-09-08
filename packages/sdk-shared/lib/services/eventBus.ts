a120ff61da7bc32fa6e8c5bcbf46973fb46b9c65
Nicolas "Norswap" Laurent <norswap@gmail.com>
2024-09-08 22:15:48 +0800

    get rid of eventBus dir (move files) and sort exports in sdk-shared

--- packages/sdk-shared/lib/services/eventBus.ts
+++ packages/sdk-shared/lib/services/eventBus.ts
@@ -1,6 +1,6 @@
-import { waitForCondition } from "../../utils/waitForCondition"
-import type { Logger } from "../logger"
-import { logger } from "../logger"
+import { waitForCondition } from "../utils/waitForCondition"
+import type { Logger } from "./logger"
+import { logger } from "./logger"
 
 /**
  * Defines different modes in which the event bus can operate.
