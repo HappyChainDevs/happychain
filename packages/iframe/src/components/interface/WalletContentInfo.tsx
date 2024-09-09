e345a3a9c53514730ca52f52f7256fc7318cc0e4
ultraviolet10 <aritrac1998@hotmail.com>
2024-09-09 12:47:53 +0530

    create TabContent component to displau selected info

---
+++ packages/iframe/src/components/interface/WalletContentInfo.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import Tab from "./Tab"
+import TabContent from "./TabContent"
+
+export enum ContentType {
+    TOKENS = "Tokens",
+    GAMES = "Games",
+    HISTORY = "History",
+}
+
+const WalletContentInfo = () => {
+    const [view, setView] = useState<ContentType>(ContentType.TOKENS)
+
+    return (
+        <div className="flex w-full h-full items-start justify-center flex-col px-1">
+            <div className="flex flex-row items-start justify-center space-x-2">
+                <Tab title={ContentType.TOKENS} setView={setView} />
+                <Tab title={ContentType.GAMES} setView={setView} />
+                <Tab title={ContentType.HISTORY} setView={setView} />
+            </div>
+            <div className="flex flex-col w-full h-4/5 p-2 bg-slate-300 rounded-b-xl rounded-tr-xl">
+                <TabContent view={view} />
+            </div>
+        </div>
+    )
+}
+
+export default WalletContentInfo
