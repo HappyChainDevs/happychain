fed1e280438bba41988a19e130c82502d427d10b
not-reed <reed@happy.tech>
2024-09-09 12:29:26 -0700

    add iframe request middleware tests - permission tests

--- packages/iframe/src/utils/getDappOrigin.ts
+++ packages/iframe/src/utils/getDappOrigin.ts
@@ -3,7 +3,6 @@ export type URLString = `http://${string}` | `https://${string}`
 export function getDappOrigin(): URLString {
     const origin = document.referrer
     if (!origin) {
-        console.warn("Unable to determine dApp Origin")
         return window.location.origin as URLString
     }
     return origin as URLString
