7be7d2ee7e7c897b3ff17163cea818995b721faa
not-reed <reed@happy.tech>
2024-09-09 09:06:35 -0700

    fix wagmi-vue demo build

--- makefiles/vite.mk
+++ makefiles/vite.mk
@@ -1,3 +1,5 @@
+TSC_BIN ?= tsc
+
 # Like build.watch but also serves the page on localhost if applicable
 dev: node_modules
 	if [[ -r index.html ]]; then \
@@ -16,7 +18,7 @@ clean:
 
 # Rebuilds on file change, but does not bundle — site can still be served locally via `vite`
 build.watch: node_modules
-	tsc --build --watch --preserveWatchOutput;
+	$(TSC_BIN) --build --watch --preserveWatchOutput;
 .PHONY: build.watch
 
 preview: node_modules dist
@@ -25,7 +27,7 @@ preview: node_modules dist
 
 # You can add dependencies to this rule in the Makefile in which `vite.mk` is inluded.
 dist: $(shell find . -type f \( -name "*.ts" -o -name "*.tsx" -o -name "*.css" -o -name "*.json" -o -name "*.js" -o -name "*.html" -o -name "*.vue" \) -not -path "./dist/*")
-	tsc --build;
+	$(TSC_BIN) --build;
 	vite build;
 
 node_modules: package.json
