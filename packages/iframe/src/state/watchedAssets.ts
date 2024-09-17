97bae2685852446d92ceea7a1b9e471fa0d0fb75
ultraviolet10 <aritrac1998@hotmail.com>
2024-09-17 11:30:01 +0530

    rearrange comment

--- packages/iframe/src/state/watchedAssets.ts
+++ packages/iframe/src/state/watchedAssets.ts
@@ -1,10 +1,9 @@
 import { atomWithStorage } from "jotai/utils"
 import type { Address, WatchAssetParameters } from "viem"
 
+export type UserWatchedAssetsRecord = Record<Address, WatchAssetParameters[]>
+
 /**
  * Atom to manage watched assets mapped to user's address, using localStorage.
  */
-
-export type UserWatchedAssetsRecord = Record<Address, WatchAssetParameters[]>
-
 export const watchedAssetsAtom = atomWithStorage<UserWatchedAssetsRecord>("watchedAssets", {})
