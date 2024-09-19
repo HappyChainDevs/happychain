8062656795c8a2cd9df68624a915664252362ec9
ultraviolet10 <aritrac1998@hotmail.com>
2024-09-19 19:14:25 +0530

    remove semicolons

--- packages/common/lib/utils/regexChecks.ts
+++ packages/common/lib/utils/regexChecks.ts
@@ -3,11 +3,11 @@
  */
 export const validateNumericInput = (input: string) => {
     return /^\d*\.?\d*$/.test(input)
-};
+}
 
 /**
  * Validates if the input is a valid Ethereum address.
  */
 export const validateAddressInput = (input: string) => {
     return /^0x[a-fA-F0-9]{40}$/.test(input)
-};
+}
