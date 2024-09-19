4719dfe8ec1194e6b31163a2c3d854a112e176de
ultraviolet10 <aritrac1998@hotmail.com>
2024-09-19 20:53:06 +0530

    add providers

--- packages/iframe/src/main.tsx
+++ packages/iframe/src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { RouterProvider, createRouter } from "@tanstack/react-router"
+import { WagmiProvider } from "wagmi"
 
 import { HappyAccountProvider } from "./providers/HappyAccountProvider"
 // Import the generated route tree
@@ -10,9 +12,12 @@ import { routeTree } from "./routeTree.gen"
 import "./listeners"
 
 import "./index.css"
+import { config } from "./wagmi/config"
 
 // Create a new router instance
 const router = createRouter({ routeTree })
+
+const queryClient = new QueryClient()
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
@@ -25,9 +30,13 @@ const rootElement = document.getElementById("root")
 if (rootElement && !rootElement.innerHTML) {
     ReactDOM.createRoot(rootElement).render(
         <StrictMode>
-            <HappyAccountProvider>
-                <RouterProvider router={router} />
-            </HappyAccountProvider>
+            <WagmiProvider config={config}>
+                <QueryClientProvider client={queryClient}>
+                    <HappyAccountProvider>
+                        <RouterProvider router={router} />
+                    </HappyAccountProvider>
+                </QueryClientProvider>
+            </WagmiProvider>
         </StrictMode>,
     )
 }
--- packages/iframe/src/wagmi/config.ts
+++ packages/iframe/src/wagmi/config.ts
@@ -4,7 +4,6 @@ import { createConfig } from "wagmi"
 import { happyConnector } from "./connector"
 import { customProvider } from "./provider"
 
-
 /**
  * Create Wagmi Config with custom connector to support HappyChain Sepolia.
  * Enables discovery of injected providers via EIP-6963 using the `mipd` library and converting to injected connectors.
