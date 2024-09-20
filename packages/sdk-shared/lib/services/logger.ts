84401b88a0f4b7b60f9aa71cb2d7bb96292d1c1f
Nicolas "Norswap" Laurent <norswap@gmail.com>
2024-09-21 05:24:33 +0800

    remove forgotten log + turn verbosity down by default on eventbus logger

--- packages/sdk-shared/lib/services/logger.ts
+++ packages/sdk-shared/lib/services/logger.ts
@@ -2,4 +2,5 @@ export type Logger = Pick<typeof console, "log" | "warn" | "error">
 
 const isProd = ["test", "production"].some((mode) => mode === import.meta.env.MODE)
 const noop = () => {}
-export const logger: Logger = isProd ? { log: noop, warn: noop, error: noop } : console
+export const silentLogger: Logger = { log: noop, warn: noop, error: noop }
+export const logger: Logger = isProd ? silentLogger : console
