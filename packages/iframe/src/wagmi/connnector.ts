30abd62b49719daa9630ee251f3c337911f701ee
ultraviolet10 <aritrac1998@hotmail.com>
2024-09-27 01:54:57 +0530

    refactor request handling utils

--- packages/iframe/src/wagmi/connnector.ts
+++ packages/iframe/src/wagmi/connnector.ts
@@ -2,14 +2,14 @@ import { injected } from "wagmi/connectors"
 import { iframeProvider } from "./provider"
 
 /**
- * custom connector: cf. https://wagmi.sh/core/api/connectors/injected#target
+ * Custom connector ({@link https://wagmi.sh/core/api/connectors/injected#target | injected connectors})
  */
 export const happyConnector = injected({
     shimDisconnect: false,
     target() {
         return {
-            id: "happyProvider",
-            name: "HappyChain (iframe) Provider",
+            id: "iframeProvider",
+            name: "HappyChain (Iframe)Provider",
             provider: iframeProvider,
         }
     },
