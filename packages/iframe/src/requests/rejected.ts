c30e1c585e567a1c869302143bfdee3c3e735d6b
ultraviolet10 <aritrac1998@hotmail.com>
2024-09-26 22:01:42 +0530

    handle request rejection for iframe

--- packages/iframe/src/requests/rejected.ts
+++ packages/iframe/src/requests/rejected.ts
@@ -1,20 +1,26 @@
 import { EIP1193ErrorCodes, Msgs, type PopupMsgs, getEIP1193ErrorObjectFromCode } from "@happychain/sdk-shared"
 import { happyProviderBus } from "../services/eventBus"
-import { confirmWindowId } from "./utils"
+import { iframeProvider } from "../wagmi/provider"
+import { confirmIframeId, confirmWindowId } from "./utils"
 
 /**
  * Processes requests rejected by the user in the pop-up, forwarding the rejection to the app.
  */
-export async function handleRejectedRequest(request: PopupMsgs[Msgs.PopupReject]): Promise<void> {
-    if (!confirmWindowId(request.windowId)) return
+export async function handleRejectedRequest(data: PopupMsgs[Msgs.PopupReject]): Promise<void> {
+    const error = data.error || getEIP1193ErrorObjectFromCode(EIP1193ErrorCodes.UserRejectedRequest)
 
-    if (request.error) {
-        void happyProviderBus.emit(Msgs.RequestResponse, request)
-    } else {
+    if (confirmWindowId(data.windowId)) {
         void happyProviderBus.emit(Msgs.RequestResponse, {
-            key: request.key,
-            windowId: request.windowId,
-            error: getEIP1193ErrorObjectFromCode(EIP1193ErrorCodes.UserRejectedRequest),
+            key: data.key,
+            windowId: data.windowId,
+            error: error,
+            payload: null,
+        })
+    } else if (confirmIframeId(data.windowId)) {
+        iframeProvider.handleRequestResolution({
+            key: data.key,
+            windowId: data.windowId,
+            error: error,
             payload: null,
         })
     }
