c3b2c6e188505999e8bbfbe98d76e1a4b25f9cef
ultraviolet10 <aritrac1998@hotmail.com>
2024-10-03 09:46:59 +0530

    remove fn, update docstring

--- packages/sdk-shared/lib/classes/BasePopupProvider.ts
+++ packages/sdk-shared/lib/classes/BasePopupProvider.ts
@@ -16,28 +16,42 @@ export type ResolveType<T extends EIP1193RequestMethods = EIP1193RequestMethods>
 ) => void
 
 /**
- * @abstract BasePopupProvider
+ * This class serves as a base for EIP-1193 providers that sometimes need to create popups to
+ * collect user approval. In particular, we have one such provider living on the app that relays
+ * requests to the iframe, and one such provider living in the iframe which is for requests
+ * initiated from within the iframe.
  *
- * This class serves as the base for providers that handle EIP-1193 requests.
- * It extends the {@link https://github.com/MetaMask/safe-event-emitter | SafeEventEmitter} to manage events and maintains a queue for
- * in-flight requests that may require popups for user interaction.
+ * Requests are made via the `request` method, which calls the `requiresApproval` method to
+ * determine whether an approval popup is needed. If so, a popup is created. The user approval or
+ * rejection is relayed by the popup to the iframe, which relays it to the provider in a way that
+ * triggers the `handleCompletedRequest` method. If no approval method is needed, the
+ * `handlePermissionlessRequest` method is called instead.
  *
- * The class provides mechanisms for handling these requests, managing popups,
- * and automatically checking if a popup was closed, which triggers request rejection
- * in such cases.
+ * The `requestPermissions` method is called to check if some permissions (beyond a simple user
+ * approval) are required for the request to proceed (so far this only happens when the app tries to
+ * make a request but isn't authenticated in the wallet or connected to it). The method should make
+ * the permisison request (which is allowed to be a recursive `request` call) and then return
+ * specifying whether the original request is be treated as permissionless or requires a further
+ * approval.
  *
- * Child classes must implement the `isConnected` and `request` methods to define
- * how the provider connects and handles requests.
+ * Note that `requestPermissions` is only called when `requiresApproval` returns true. That's
+ * because we allow permissionless access to the iframe provider, but suggesting things to the user
+ * (usually signing transactions) is subject to the initial wallet connection approval. Some
+ * requests `wallet_requestPermissions` can be be either permissionless (if the permission has
+ * already been granted) or require approval (if not), which is why this method needs to return a
+ * value to indicate this.
+ *
+ * The class includes logic that checks if a popup was closed instead of giving an explicit approval
+ * or rejection, which is then equated to a rejection.
+ *
+ * The class extends the {@link https://github.com/MetaMask/safe-event-emitter | SafeEventEmitter}
+ * to enable emitting events on instances of this class without having to handle the errors that
+ * can possibly be thrown by the handlers (these are thrown in a setTimeout instead).
  */
 export abstract class BasePopupProvider extends SafeEventEmitter {
     protected inFlightRequests = new Map<string, InFlightRequest>()
     protected timer: Timer | null = null
     protected POPUP_FEATURES = ["width=400", "height=800", "popup=true", "toolbar=0", "menubar=0"].join(",")
-
-    /**
-     * Abstract method to check if the provider is connected. Must be implemented by child classes.
-     */
-    abstract isConnected(): boolean
 
     /**
      * Abstract method to send requests. This method must be implemented by child classes.
