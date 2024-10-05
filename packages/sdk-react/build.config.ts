60c9a70e7df6013b0dbddb02fdc8032e71eb571e
Nicolas "Norswap" Laurent <norswap@gmail.com>
2024-10-05 12:40:34 +0800

    make build.config.ts, etc checkable with tsc & IDE

--- packages/sdk-react/build.config.ts
+++ packages/sdk-react/build.config.ts
@@ -1,23 +1,20 @@
-import { defineConfig } from "@happychain/scripts"
-
-import "./happydom.ts"
+import { type Config, defineConfig } from "@happychain/scripts"
 
 // import project so that hot reloading/watching works
+import "./happydom.ts"
 import "./lib/index.ts"
 
-export default defineConfig([
-    {
-        cleanOutDir: true,
-        tsConfig: "tsconfig.build.json",
-        apiExtractorConfig: "api-extractor.json",
-        bunConfig: {
-            entrypoints: ["./lib/index.ts"],
-            outdir: "./dist",
-            minify: true,
-            splitting: false,
-            external: ["react", "react-dom", "@happychain/js"],
-            sourcemap: "linked",
-            naming: "[dir]/[name].es.[ext]",
-        },
+export default defineConfig({
+    cleanOutDir: true,
+    tsConfig: "tsconfig.build.json",
+    apiExtractorConfig: "api-extractor.json",
+    bunConfig: {
+        entrypoints: ["./lib/index.ts"],
+        outdir: "./dist",
+        minify: true,
+        splitting: false,
+        external: ["react", "react-dom", "@happychain/js"],
+        sourcemap: "linked",
+        naming: "[dir]/[name].es.[ext]",
     },
-])
+}) as Config
