1a5798448572403f67dca0930808c9d8ee3d318e
not-reed <reed@happy.tech>
2024-10-08 08:31:15 -0700

    update iframe to use shared web3auth worker - 2

--- packages/iframe/vite.config.ts
+++ packages/iframe/vite.config.ts
@@ -1,51 +1,47 @@
 /// <reference types="vitest" />
+import { join, resolve } from "node:path"
+import { SharedWorkerPlugin } from "@happychain/worker"
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { nodePolyfills } from "vite-plugin-node-polyfills"
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+    optimizeDeps: {
+        exclude: ["@happychain/worker"],
+    },
     server: { port: 5160 },
     plugins: [
         nodePolyfills({ globals: { Buffer: true } }), // required for web3Auth
         TanStackRouterVite(),
-        react({
-            babel: { presets: ["jotai/babel/preset"] },
-        }),
+        react({ babel: { presets: ["jotai/babel/preset"] } }),
+        SharedWorkerPlugin(),
     ],
     resolve: {
         alias: {
-            crypto: "empty-module",
-            assert: "empty-module",
-            http: "empty-module",
-            https: "empty-module",
-            os: "empty-module",
-            url: "empty-module",
-            zlib: "empty-module",
-            stream: "empty-module",
-            _stream_duplex: "empty-module",
-            _stream_passthrough: "empty-module",
-            _stream_readable: "empty-module",
-            _stream_writable: "empty-module",
-            _stream_transform: "empty-module",
+            "@happychain/firebase-web3auth-strategy": resolve(
+                join(__dirname, "../sdk-firebase-web3auth-strategy/lib/index.ts"),
+            ),
         },
     },
     build: {
         rollupOptions: {
-            external: [
-                "react",
-                "viem",
-                "@web3auth/base",
-                "@web3auth/ethereum-mpc-provider",
-                "@web3auth/mpc-core-kit",
-                /\\.mocks$/,
-            ],
+            external:
+                mode === "production"
+                    ? []
+                    : [
+                          "react",
+                          "viem",
+                          "@web3auth/base",
+                          "@web3auth/ethereum-mpc-provider",
+                          "@web3auth/mpc-core-kit",
+                          /\\.mocks$/,
+                      ],
         },
     },
-    define: { global: "globalThis" }, // required for web3Auth
 
     test: {
         environment: "happy-dom",
     },
-})
+}))
