f227da553c60ed9bc7ff9de8663ccb15450d3fb2
Nicolas "Norswap" Laurent <norswap@gmail.com>
2024-10-12 01:39:18 +0800

    WIP remove hot reload handlers
    TODO: must remove OriginRequest

--- packages/iframe/src/utils/getDappOrigin.ts
+++ packages/iframe/src/utils/getDappOrigin.ts
@@ -1,28 +1,10 @@
 import type { HTTPString } from "@happychain/common"
-import { Msgs } from "@happychain/sdk-shared"
-import { appMessageBus } from "../services/eventBus"
-
-if (import.meta.hot) {
-    // disable hot reloading here, as when embedded in an iframe,
-    // the origin of the dapp after the refresh is always the iframe
-    // itself i.e. first load getDappOrigin === localhost:5173,
-    // after HMR localhost:5160
-    // This has repercussions on the permission system
-    import.meta.hot.accept(() => {
-        import.meta.hot?.invalidate()
-    })
-}
 
 export function getIframeOrigin(): HTTPString {
     return location.origin as HTTPString
 }
 
-let dappOrigin = (location.ancestorOrigins?.[0] ?? document.referrer) ? new URL(document.referrer).origin : null
-// if the iframe refreshes itself without the parent window refreshing,
-// dappOrigin and referrer information are lost, and the dappOrigin becomes the iframe
-appMessageBus.on(Msgs.OriginResponse, (newOrigin) => {
-    dappOrigin = newOrigin
-})
+const dappOrigin = new URL(location.ancestorOrigins?.[0] ?? document.referrer).origin
 
 export function getDappOrigin(): HTTPString {
     if (!dappOrigin) {
