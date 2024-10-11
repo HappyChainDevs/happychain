c69405553d4231b371bef66e37c49a8117815d04
Nicolas "Norswap" Laurent <norswap@gmail.com>
2024-10-12 02:07:45 +0800

    make demo background not tile (but "cover") + remove unstyled button

--- packages/demo-react/src/App.tsx
+++ packages/demo-react/src/App.tsx
@@ -53,7 +53,7 @@ function App() {
     }, [user])
 
     return (
-        <main className='flex min-h-dvh w-full flex-col items-center gap-4 bg-[url("/francesco-ungaro-Wn8JoB8FP70-unsplash.jpg")] bg-[100vw_auto] p-4'>
+        <main className='flex min-h-dvh w-full flex-col items-center gap-4 bg-[url("/francesco-ungaro-Wn8JoB8FP70-unsplash.jpg")] bg-cover p-4'>
             <h1 className="p-16 text-4xl font-bold text-white">HappyChain + TS + React + Viem</h1>
 
             <button
@@ -67,8 +67,6 @@ function App() {
             </button>
 
             <ConnectButton />
-
-            <ConnectButton disableStyles={true} />
 
             <div className="max-w-96 w-full overflow-auto bg-gray-200 p-4">
                 <p className="text-lg font-bold">User Details</p>
