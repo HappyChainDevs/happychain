2b357bb369b48633bfc72dc56cea41a8407a0961
Nicolas "Norswap" Laurent <norswap@gmail.com>
2024-10-11 22:55:49 +0800

    radically simplify the badge webcomponent code

--- packages/sdk-frontend-components/lib/components/Logo.tsx
+++
@@ -1,6 +0,0 @@
-/** @jsxImportSource preact */
-import type { EIP6963ProviderInfo } from "mipd"
-
-export function Logo({ info }: { info?: EIP6963ProviderInfo }) {
-    return <img src={info?.icon} alt={`${info?.name} logo`} className={"happychain-icon"} />
-}
