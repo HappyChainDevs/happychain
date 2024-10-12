d6207912e4f8bd450e8b9fad07def36738ed8348
Nicolas "Norswap" Laurent <norswap@gmail.com>
2024-10-13 05:10:00 +0800

    prettify codegen

--- packages/vite-plugin-shared-worker/src/plugin/utils.ts
+++ packages/vite-plugin-shared-worker/src/plugin/utils.ts
@@ -1,7 +1,7 @@
 import path from "node:path"
 import { findExports as mllyFindExports } from "mlly"
 
-// Determines if the transformer/plugin should handle the file or not
+/** Determines if the transformer/plugin should handle the file or not. */
 export const filter = (id: string) => {
     return id.includes(".sw.ts")
 }
