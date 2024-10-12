9ebf8981d42ec6e12b9e1338ae8468e160979ca2
Nicolas "Norswap" Laurent <norswap@gmail.com>
2024-10-13 05:10:00 +0800

    prettify codegen

--- packages/vite-plugin-shared-worker/src/plugin/DevelopmentPlugin.ts
+++ packages/vite-plugin-shared-worker/src/plugin/DevelopmentPlugin.ts
@@ -9,6 +9,8 @@ export function DevelopmentPlugin(): Plugin {
         enforce: "pre",
         transform(code: string, id: string) {
             if (!filter(id)) return
+            // In development, vite appends "?worker_file&type=module" to the worker files, which
+            // are paths passed to a web worker constructor like `SharedWorker`.
             const isClient = !id.includes("worker_file")
             return { code: isClient ? clientCodeGen(code, id) : workerCodeGen(code, id) }
         },
