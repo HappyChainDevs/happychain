f4a0ca29eb33e4b5f5b539af312f5280bfead678
naomiHauret <hauretnaomi@gmail.com>
2024-10-17 13:25:02 +0200

    types(iframe): fallback to current app url in user permission hook

--- packages/iframe/src/hooks/useHasPermissions.ts
+++ packages/iframe/src/hooks/useHasPermissions.ts
@@ -1,11 +1,12 @@
 import { useAtomValue } from "jotai"
 import { useMemo } from "react"
 import { type PermissionsRequest, atomForPermissionsCheck } from "../state/permissions"
+import { type AppURL, getAppURL } from "../utils/appURL"
 
 export function useHasPermissions(permissionsRequest: PermissionsRequest, appUrl?: string) {
     // This must be memoized to avoid an infinite render loop.
     const permissionsAtom = useMemo(
-        () => atomForPermissionsCheck(permissionsRequest, appUrl), //
+        () => atomForPermissionsCheck(permissionsRequest, (appUrl ?? getAppURL()) as AppURL), //
         [permissionsRequest, appUrl],
     )
     return useAtomValue(permissionsAtom)
