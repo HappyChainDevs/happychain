b2c4a37db0d6e2f40b67b2afeb235990698bc0b7
Gabriel Martinez Rodriguez <gabrielmartinezrdrgz@gmail.com>
2024-10-21 10:12:29 +0200

    fix(randomness-service): ci

--- packages/randomness-service/src/index.ts
+++ packages/randomness-service/src/index.ts
@@ -3,7 +3,7 @@ import { Transaction, TransactionManager } from "@happychain/transaction-manager
 import { http } from "viem"
 import { privateKeyToAccount } from "viem/accounts"
 import { anvil } from "viem/chains"
-import { ContractAlias, abis } from "./ABI/random.js"
+import { abis } from "./ABI/random.js"
 
 const client = await TransactionManager.create({
     account: privateKeyToAccount("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"),
@@ -13,14 +13,11 @@ const client = await TransactionManager.create({
     abis: abis,
 })
 
-let number = 0
-
 client.addTransactionCollector(() => {
     const transactionCount = 1
     const transactions = []
 
     for (let i = 0; i < transactionCount; i++) {
-        number += 1
         const transaction = new Transaction({
             chainId: anvil.id,
             address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
