80a837103e9783cc5e5904788544763bd0fa983a
ultraviolet10 <aritrac1998@hotmail.com>
2024-10-22 04:15:36 +0530

    fix: styles

--- packages/iframe/src/components/interface/home/ActionButtons.tsx
+++ packages/iframe/src/components/interface/home/ActionButtons.tsx
@@ -2,7 +2,7 @@ import { Link } from "@tanstack/react-router"
 
 const ActionButtons = () => {
     return (
-        <div className="flex flex-row w-full items-center justify-center space-x-6 px-2 py-4 border border-slate-600 border-opacity-50 rounded-lg">
+        <div className="flex flex-row w-full items-center justify-center space-x-6 px-2 py-2 border border-slate-600 border-opacity-50 rounded-lg">
             <Link
                 type="button"
                 className="flex items-center justify-center h-10 w-24 bg-cyan-600 text-black rounded-xl text-center"
