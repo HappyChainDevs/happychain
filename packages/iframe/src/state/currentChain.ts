bcbe610530de82fc4fa8dfccd1d1d9be57c93dac
ultraviolet10 <aritrac1998@hotmail.com>
2024-10-23 10:57:35 +0530

    chore: update file name, add docstring

--- packages/iframe/src/state/currentChain.ts
+++ packages/iframe/src/state/currentChain.ts
@@ -2,6 +2,10 @@ import { accessorsFromAtom } from "@happychain/common"
 import { type ChainParameters, getChainFromSearchParams } from "@happychain/sdk-shared"
 import { type Atom, atom } from "jotai"
 
+/**
+ * Atom to wrap around the `getChainFromSearchParams` function, to
+ * initialize the current chain details on page load for app wide access.
+ */
 export const currentChainAtom: Atom<ChainParameters> = atom(() => {
     return getChainFromSearchParams()
 })
