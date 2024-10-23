e794a631130861f229636687826488b1ad99c8b7
ultraviolet10 <aritrac1998@hotmail.com>
2024-10-23 17:28:21 +0530

    feat: introduce user balance format fn

--- packages/iframe/src/components/interface/home/HappyBalance.tsx
+++ packages/iframe/src/components/interface/home/HappyBalance.tsx
@@ -1,5 +1,5 @@
+import { formatUserBalance } from "@happychain/sdk-shared"
 import { useAtomValue } from "jotai"
-import { formatEther } from "viem"
 import { useBalance } from "wagmi"
 import { userAtom } from "../../../state/user"
 
@@ -8,15 +8,13 @@ const HappyBalance = () => {
 
     const { data: balance } = useBalance({ address: user?.address })
 
-    const truncatedBalance = balance
-        ? (Math.floor(Number.parseFloat(formatEther(balance.value)) * 1000) / 1000).toString()
-        : "0.0"
+    const formattedBalance = formatUserBalance(balance?.value)
 
     return (
         <div className="flex flex-row w-full items-center justify-between">
             <p className="text-lg">$HAPPY</p>
             <div className="flex flex-col items-center">
-                <p className="text-2xl">{truncatedBalance}</p>
+                <p className="text-2xl">{formattedBalance}</p>
             </div>
         </div>
     )
