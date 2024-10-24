595a0cacf478b08df7504d1271f991c070e72c93
Gabriel Martinez Rodriguez <gabrielmartinezrdrgz@gmail.com>
2024-10-24 14:55:24 +0200

    chore(randomnes-service): pr comments

--- packages/randomness-service/build.config.ts
+++ packages/randomness-service/build.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from "@happychain/scripts"
-import transactionManagerBuildConfig from "@happychain/transaction-manager/build.config"
+import { buildConfig } from "@happychain/transaction-manager"
 
 export default defineConfig([
     {
+        tsConfig: "tsconfig.json",
         bunConfig: {
             entrypoints: ["./src/index.ts"],
             minify: false,
             target: "node",
-            external: [...transactionManagerBuildConfig[0].bunConfig.external],
+            // The transaction-manager package depends on mikro-orm, which relies on dynamic imports like "pg" that are not
+            // actually installed. Therefore, when bundling the package, we need to instruct bun to ignore them.
+            external: [...buildConfig.default[0].bunConfig.external],
         },
     },
 ])
--- packages/transaction-manager/build.config.ts
+++ packages/transaction-manager/build.config.ts
@@ -4,9 +4,11 @@ export default defineConfig([
     {
         tsConfig: "tsconfig.json",
         bunConfig: {
-            entrypoints: ["./lib/index.ts", "./lib/mikro-orm.config.ts", "./build.config.ts"],
+            entrypoints: ["./lib/index.ts", "./lib/mikro-orm.config.ts"],
             minify: false,
             target: "node",
+            // These packages are not used by us, but mikro-orm depends on them because they can be imported dynamically.
+            // Therefore, when bundling the package, we need to instruct bun to ignore them since they are not actually installed.
             external: [
                 "sqlite3",
                 "mysql2",
