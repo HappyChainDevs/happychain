531b65857e72c3ae4f0b4bfbe5adc4fbf1b4289d
Gabriel Martinez Rodriguez <gabrielmartinezrdrgz@gmail.com>
2024-10-24 11:27:54 +0200

    chore(randomness-service): added trim to zod schemas

--- packages/common/lib/utils/zod.ts
+++ packages/common/lib/utils/zod.ts
@@ -3,6 +3,7 @@ import { z } from "zod"
 
 export const hexSchema = z
     .string()
+    .trim()
     .refine((s) => s.startsWith("0x"), {
         message: "Hex string must start with 0x",
     })
--- packages/randomness-service/src/env.ts
+++ packages/randomness-service/src/env.ts
@@ -4,9 +4,18 @@ import { z } from "zod"
 const envSchema = z.object({
     PRIVATE_KEY: hexSchema,
     RANDOM_CONTRACT_ADDRESS: hexSchema,
-    PRECOMMIT_DELAY: z.string().transform((s) => BigInt(s)),
-    POST_COMMIT_MARGIN: z.string().transform((s) => BigInt(s)),
-    TIME_BLOCK: z.string().transform((s) => BigInt(s)),
+    PRECOMMIT_DELAY: z
+        .string()
+        .trim()
+        .transform((s) => BigInt(s)),
+    POST_COMMIT_MARGIN: z
+        .string()
+        .trim()
+        .transform((s) => BigInt(s)),
+    TIME_BLOCK: z
+        .string()
+        .trim()
+        .transform((s) => BigInt(s)),
 })
 
 const parsedEnv = envSchema.safeParse(process.env)
