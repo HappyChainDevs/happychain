bd6b1a0c3922934a099690d2359cbbb1ec76f232
ultraviolet10 <aritrac1998@hotmail.com>
2024-10-24 13:13:28 +0530

    chore: type management in txHistory atoms

--- packages/iframe/src/components/interface/home/tabs/views/activity/TxLogEntry.tsx
+++ packages/iframe/src/components/interface/home/tabs/views/activity/TxLogEntry.tsx
@@ -2,43 +2,45 @@ import { shortenAddress } from "@happychain/sdk-shared"
 import { ArrowUpRight, CircleNotch } from "@phosphor-icons/react"
 import clsx from "clsx"
 import { formatEther } from "viem"
-import { getCurrentChainBlockExplorerUrl } from "../../../../../../state/currentChain"
-import type { ExtendedTransactionReceipt } from "../../../../../../state/txHistory"
+
+import { getCurrentChainBlockExplorerUrl } from "#src/state/currentChain.ts"
+import type { TxInfo } from "#src/state/txHistory.ts"
 
 interface TxLogEntryProps {
-    tx: ExtendedTransactionReceipt
+    tx: TxInfo
 }
 
 const TxLogEntry = ({ tx }: TxLogEntryProps) => {
     const blockExplorerUrl = getCurrentChainBlockExplorerUrl()
+    const { receipt, value } = tx
 
     return (
         <div className="flex flex-row items-center w-full justify-between px-3 py-4 border rounded-md border-slate-700">
             <div className="flex flex-row items-center justify-center space-x-1">
                 <ArrowUpRight
                     className={clsx({
-                        "bg-green-600 rounded-sm": tx.status === "success",
-                        "bg-red-600 rounded-sm": tx.status === "reverted",
+                        "bg-green-600 rounded-sm": receipt.status === "success",
+                        "bg-red-600 rounded-sm": receipt.status === "reverted",
                     })}
                 />
                 <div className="flex flex-col items-start justify-center">
                     <span className="text-black">Send</span>
                     <span>
                         <a
-                            href={`${blockExplorerUrl}/tx/${tx.transactionHash}`}
+                            href={`${blockExplorerUrl}/tx/${receipt.transactionHash}`}
                             target="_blank"
                             rel="noopener noreferrer"
                             className="text-blue-500 hover:text-purple-500 hover:underline"
                         >
-                            {shortenAddress(tx.transactionHash)}
+                            {shortenAddress(receipt.transactionHash)}
                         </a>
                     </span>
                 </div>
             </div>
 
             <div>
-                {tx.sendValue ? (
-                    <span className="text-black font-semibold">{`-${formatEther(tx.sendValue)} HAPPY`}</span>
+                {value !== 0n ? (
+                    <span className="text-black font-semibold">{`-${formatEther(value)} HAPPY`}</span>
                 ) : (
                     <div className="animate-spin">
                         <CircleNotch />
