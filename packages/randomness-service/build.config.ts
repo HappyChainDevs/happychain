892a1270f55ff89599d21880081dadd4d37ceb19
Nicolas "Norswap" Laurent <norswap@gmail.com>
2024-10-25 09:53:01 +0800

    remove array

--- packages/randomness-service/build.config.ts
+++ packages/randomness-service/build.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from "@happychain/scripts"
 import { buildConfig } from "@happychain/transaction-manager"
 
-export default defineConfig([
-    {
-        tsConfig: "tsconfig.json",
-        bunConfig: {
-            entrypoints: ["./src/index.ts"],
-            minify: false,
-            target: "node",
-            // The transaction-manager package depends on mikro-orm, which relies on dynamic imports like "pg" that are not
-            // actually installed. Therefore, when bundling the package, we need to instruct bun to ignore them.
-            external: [...buildConfig.default[0].bunConfig.external],
-        },
+export default defineConfig({
+    tsConfig: "tsconfig.json",
+    bunConfig: {
+        entrypoints: ["./src/index.ts"],
+        minify: false,
+        target: "node",
+        // The transaction-manager package depends on mikro-orm, which relies on dynamic imports like "pg" that are not
+        // actually installed. Therefore, when bundling the package, we need to instruct bun to ignore them.
+        external: [...buildConfig.default[0].bunConfig.external],
     },
-])
+})
