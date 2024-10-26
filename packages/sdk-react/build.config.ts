a3bdb3a74622c88d825ee93b25401c3180f75618
Nicolas "Norswap" Laurent <norswap@gmail.com>
2024-10-26 17:13:31 +0800

    update build configs

--- packages/sdk-react/build.config.ts
+++ packages/sdk-react/build.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "@happychain/build"
 
 export default defineConfig({
     bunConfig: {
-        entrypoints: ["./lib/index.ts"],
         external: ["react", "react-dom", "@happychain/js"],
     },
 })
--- packages/transaction-manager/build.config.ts
+++ packages/transaction-manager/build.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "@happychain/build"
 
 export default defineConfig({
     bunConfig: {
-        entrypoints: ["./lib/index.ts", "./lib/migrate.ts"],
         minify: false,
         target: "node",
         external: ["better-sqlite3"],
