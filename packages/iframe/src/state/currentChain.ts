49c5cc47fbd1c974476d50732d76f111edb67875
ultraviolet10 <aritrac1998@hotmail.com>
2024-10-27 13:27:28 +0530

    chore: redefine atom to hold fn

--- packages/iframe/src/state/currentChain.ts
+++ packages/iframe/src/state/currentChain.ts
@@ -3,21 +3,13 @@ import { type ChainParameters, getChainFromSearchParams } from "@happychain/sdk-
 import { type Atom, atom } from "jotai"
 
 /**
- * Atom to wrap around the `getChainFromSearchParams` function, to
- * initialize the current chain details on page load for app wide access.
+ * Atom to wrap around the {@link getChainFromSearchParams} function, to
+ * initialize the current chain details on page load for iframe wide access.
  */
-export const currentChainAtom: Atom<ChainParameters> = atom(() => {
-    return getChainFromSearchParams()
+export const baseChainAtom: Atom<{ getChain: () => ChainParameters }> = atom({
+    getChain: () => getChainFromSearchParams(),
 })
 
+export const currentChainAtom = atom((get) => get(baseChainAtom).getChain())
+
 export const { getValue: getCurrentChain } = accessorsFromAtom(currentChainAtom)
-
-// === State Accessors ========================================================================
-
-/**
- * Returns the Block Explorer URL, if present.
- */
-export function getCurrentChainBlockExplorerUrl(): string {
-    const currentChain = getCurrentChain()
-    return currentChain.blockExplorerUrls ? currentChain.blockExplorerUrls[0] : ""
-}
