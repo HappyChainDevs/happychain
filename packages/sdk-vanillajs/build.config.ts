2713156b9155f5a10385aa988cc74e9c2cc4e35d
not-reed <reed@happy.tech>
2024-10-29 11:23:55 -0700

    draggable-wallet

--- packages/sdk-vanillajs/build.config.ts
+++ packages/sdk-vanillajs/build.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, inlineCssPlugin } from "@happychain/scripts"
 
 export default defineConfig({
     bunConfig: {
+        minify: false,
         entrypoints: ["./lib/index.ts"],
         plugins: [inlineCssPlugin],
     },
