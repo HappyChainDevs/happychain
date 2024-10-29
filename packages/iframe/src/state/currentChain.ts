781c72d323713212ea706c0a73b148a508bce62d
ultraviolet10 <aritrac1998@hotmail.com>
2024-10-29 12:44:19 +0530

    chore: update atom definition to and track and update url state

--- packages/iframe/src/state/currentChain.ts
+++ packages/iframe/src/state/currentChain.ts
@@ -1,15 +1,32 @@
 import { accessorsFromAtom } from "@happychain/common"
 import { type ChainParameters, getChainFromSearchParams } from "@happychain/sdk-shared"
-import { type Atom, atom } from "jotai"
+import { type WritableAtom, atom } from "jotai"
+import type { AddEthereumChainParameter } from "viem"
 
 /**
- * Atom to wrap around the {@link getChainFromSearchParams} function, to
- * initialize the current chain details on page load for iframe wide access.
+ * This atom stores the current configuration of the chain that
+ * the iframe is connected to.
+ *
+ * The atom is initialised (and updated) with the ChainParameters
+ * object obtained from the current URL state.
+ *
+ * When `setCurrentChain` is called with a new chain payload, this atom:
+ *   - Updates the chain in the application state.
+ *   - Modifies the `chain` query parameter in the URL without reloading the page.
  */
-export const baseChainAtom: Atom<{ getChain: () => ChainParameters }> = atom({
-    getChain: () => getChainFromSearchParams(),
+export const currentChainAtom: WritableAtom<
+    ChainParameters,
+    [AddEthereumChainParameter | Readonly<AddEthereumChainParameter> | undefined],
+    void
+> = atom(getChainFromSearchParams(), (_get, set, newChain) => {
+    if ("URLSearchParams" in window && newChain) {
+        // Update the URL with the new chain value
+        const searchParams = new URLSearchParams(window.location.search)
+        searchParams.set("chain", JSON.stringify(newChain))
+        history.replaceState(history.state, "", `${location.origin}${location.pathname}?${searchParams.toString()}`)
+    }
+
+    set(currentChainAtom, getChainFromSearchParams())
 })
 
-export const currentChainAtom = atom((get) => get(baseChainAtom).getChain())
-
-export const { getValue: getCurrentChain } = accessorsFromAtom(currentChainAtom)
+export const { getValue: getCurrentChain, setValue: setCurrentChain } = accessorsFromAtom(currentChainAtom)
