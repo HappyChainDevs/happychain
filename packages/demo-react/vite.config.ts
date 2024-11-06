92a710904885cc6d804e588ca7e76e7d837330b7
Nicolas "Norswap" Laurent <norswap@gmail.com>
2024-11-06 17:40:31 +0700

    remove explicit vite targets

--- packages/demo-react/vite.config.ts
+++ packages/demo-react/vite.config.ts
@@ -4,15 +4,4 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    build: {
-        target: "esnext",
-    },
-    esbuild: {
-        target: "esnext",
-    },
-    optimizeDeps: {
-        esbuildOptions: {
-            target: "esnext",
-        },
-    },
 })
