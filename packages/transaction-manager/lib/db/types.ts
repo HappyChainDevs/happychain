b5af8941f01d8c2a97d149a1d1b2f28365e644a0
Gabriel Martinez Rodriguez <gabrielmartinezrdrgz@gmail.com>
2024-11-11 21:48:08 +0100

    chore(txm): fixes kysely

--- packages/transaction-manager/lib/db/types.ts
+++ packages/transaction-manager/lib/db/types.ts
@@ -1,19 +1,18 @@
 import type { UUID } from "@happychain/common"
-import type { JSONColumnType } from "kysely"
-import type { Address, ContractFunctionArgs } from "viem"
-import type { Attempt, TransactionStatus } from "../Transaction"
+import type { Address } from "viem"
+import type { TransactionStatus } from "../Transaction"
 
 export interface TransactionTable {
     intentId: UUID
     chainId: number
     address: Address
     functionName: string
-    args: JSONColumnType<ContractFunctionArgs>
+    args: string
     contractName: string
     deadline: number | undefined
     status: TransactionStatus
-    attempts: JSONColumnType<Attempt[]>
-    metadata: JSONColumnType<Record<string, unknown>> | undefined
+    attempts: string
+    metadata: string | undefined
 }
 
 export interface Database {
