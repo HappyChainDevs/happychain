a5f16995ad321e8d6d6ce4ceed21cfd0515ddda6
naomiHauret <hauretnaomi@gmail.com>
2024-11-11 16:22:20 +0100

    feat(iframe): create smart account

--- packages/iframe/src/actions/setUserWithProvider.ts
+++ packages/iframe/src/actions/setUserWithProvider.ts
@@ -2,25 +2,40 @@ import type { HappyUser } from "@happychain/sdk-shared"
 import { AuthState } from "@happychain/sdk-shared"
 import { getDefaultStore } from "jotai"
 import type { EIP1193Provider } from "viem"
-import { authStateAtom } from "../state/authState"
-import { clearAppPermissions } from "../state/permissions"
-import { providerAtom } from "../state/provider"
-import { userAtom } from "../state/user"
-import { getAppURL, isStandaloneIframe } from "../utils/appURL"
+import { authStateAtom } from "#src/state/authState"
+import { createKernelAccount } from "#src/state/kernelAccount"
+import { providerAtom } from "#src/state/provider"
+import { userAtom } from "#src/state/user"
 
 const store = getDefaultStore()
 
-export function setUserWithProvider(user: HappyUser | undefined, provider: EIP1193Provider | undefined) {
+export async function setUserWithProvider(user: HappyUser | undefined, provider: EIP1193Provider | undefined) {
     store.set(providerAtom, provider)
-    store.set(userAtom, user)
+    if (user) {
+        const kernelAccount = await createKernelAccount(user.address)
+        const smartAccountAddress = kernelAccount?.address
+
+        if (smartAccountAddress) {
+            const userWithSmartAccount: HappyUser = {
+                ...user,
+                controllingAddress: user.address,
+                address: smartAccountAddress,
+                addresses: [smartAccountAddress, user.address],
+                name: `${smartAccountAddress.slice(0, 6)}...${smartAccountAddress.slice(-4)}`,
+            }
+            store.set(userAtom, userWithSmartAccount)
+        } else {
+            console.error("Failed to create smart account")
+            store.set(userAtom, {
+                ...user,
+                address: undefined,
+                controllingAddress: user.address,
+            })
+        }
+    } else {
+        store.set(userAtom, undefined)
+    }
 
     // user auth state
     store.set(authStateAtom, () => (user ? AuthState.Connected : AuthState.Disconnected))
-
-    // Clear all dapp permissions on logout.
-    // TODO we actually don't want this, but it's very handy right now
-    //      let's remove this once we can manage permissions in-UI
-    if (!user && !isStandaloneIframe()) {
-        clearAppPermissions(getAppURL())
-    }
 }
