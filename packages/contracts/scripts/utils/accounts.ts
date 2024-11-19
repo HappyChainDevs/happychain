5414a3c3285fc0bbddc0c3423b28b641c8b17f21
Aryan Godara <aryangodara03@gmail.com>
2024-11-19 11:13:09 +0200

    fix linting [skip ci]

--- packages/contracts/scripts/utils/accounts.ts
+++ packages/contracts/scripts/utils/accounts.ts
@@ -3,9 +3,9 @@ import { entryPoint07Address } from "viem/account-abstraction"
 import { generatePrivateKey, privateKeyToAddress } from "viem/accounts"
 import { localhost } from "viem/chains"
 
+import { abis as mockAbis, deployment as mockDeployment } from "../../deployments/anvil/mockTokens/abis.ts"
 import { abis, deployment } from "../../deployments/anvil/testing/abis"
 import { account, publicClient, walletClient } from "./clients"
-import {abis as mockAbis, deployment as mockDeployment} from "../../deployments/anvil/mockTokens/abis.ts";
 
 const DEPOSIT = parseEther("100")
 
@@ -58,4 +58,4 @@ async function initializeTotalSupply(): Promise<string> {
     return receipt.status
 }
 
-export {  depositPaymaster, fundSmartAccount, getRandomAddress, initializeTotalSupply }
+export { depositPaymaster, fundSmartAccount, getRandomAddress, initializeTotalSupply }
