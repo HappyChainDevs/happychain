e2213bc547080c2d205287d0b44fe45eec955188
Gabriel Martinez Rodriguez <gabrielmartinezrdrgz@gmail.com>
2024-12-04 11:17:43 +0100

    feat(txm): use address instead of id to find transaction inside txm

--- packages/randomness-service/src/Factories/CommitmentTransactionFactory.ts
+++ packages/randomness-service/src/Factories/CommitmentTransactionFactory.ts
@@ -1,20 +1,19 @@
-import { Transaction } from "@happychain/transaction-manager"
+import type { Transaction, TransactionManager } from "@happychain/transaction-manager"
 import type { Address, Hex } from "viem"
 
 export class CommitmentTransactionFactory {
-    private readonly chainId: number
+    private readonly transactionManager: TransactionManager
     private readonly randomContractAddress: Address
     private readonly precommitDelay: bigint
 
-    constructor(chainId: number, randomContractAddress: Address, precommitDelay: bigint) {
-        this.chainId = chainId
+    constructor(transactionManager: TransactionManager, randomContractAddress: Address, precommitDelay: bigint) {
+        this.transactionManager = transactionManager
         this.randomContractAddress = randomContractAddress
         this.precommitDelay = precommitDelay
     }
 
     create(timestamp: bigint, commitment: Hex): Transaction {
-        return new Transaction({
-            chainId: this.chainId,
+        return this.transactionManager.createTransaction({
             address: this.randomContractAddress,
             functionName: "postCommitment",
             contractName: "Random",
