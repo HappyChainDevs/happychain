a520cec8e3741718ad4f99f52a3acc9c55df67ac
Gabriel Martinez Rodriguez <gabrielmartinezrdrgz@gmail.com>
2024-12-05 12:41:27 +0100

    chore(txm): added All to TxmHookType

--- packages/transaction-manager/lib/HookManager.ts
+++ packages/transaction-manager/lib/HookManager.ts
@@ -2,6 +2,7 @@ import { Topics, eventBus } from "./EventBus.js"
 import type { Transaction } from "./Transaction.js"
 
 export enum TxmHookType {
+    All = "All",
     TransactionStatusChanged = "TransactionStatusChanged",
 }
 
@@ -13,29 +14,27 @@ export type TxmHookPayload = {
 export type TxmHookHandler = (event: TxmHookPayload) => void
 
 export class HookManager {
-    private hooks: Record<TxmHookType | "All", TxmHookHandler[]>
+    private hooks: Record<TxmHookType, TxmHookHandler[]>
 
     constructor() {
         this.hooks = {
-            All: [],
-            TransactionStatusChanged: [],
+            [TxmHookType.All]: [],
+            [TxmHookType.TransactionStatusChanged]: [],
         }
         eventBus.on(Topics.TransactionStatusChanged, this.onTransactionStatusChanged.bind(this))
     }
 
-    public async addHook(handler: TxmHookHandler, type?: TxmHookType): Promise<void> {
-        const mapKey = type ?? "All"
-
-        if (!this.hooks[mapKey]) {
-            this.hooks[mapKey] = []
+    public async addHook(handler: TxmHookHandler, type: TxmHookType): Promise<void> {
+        if (!this.hooks[type]) {
+            this.hooks[type] = []
         }
-        this.hooks[mapKey].push(handler)
+        this.hooks[type].push(handler)
     }
 
     private async onTransactionStatusChanged(payload: {
         transaction: Transaction
     }): Promise<void> {
-        this.hooks[TxmHookType.TransactionStatusChanged].concat(this.hooks.All).map((h) =>
+        this.hooks[TxmHookType.TransactionStatusChanged].concat(this.hooks[TxmHookType.All]).map((h) =>
             h({
                 type: TxmHookType.TransactionStatusChanged,
                 transaction: payload.transaction,
