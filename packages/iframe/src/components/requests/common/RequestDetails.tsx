11fe7b781423feee56359814aecff2e75015ed2a
ultraviolet10 <aritrac1998@hotmail.com>
2024-12-06 18:56:05 +0530

    remove memo

--- packages/iframe/src/components/requests/common/RequestDetails.tsx
+++ packages/iframe/src/components/requests/common/RequestDetails.tsx
@@ -1,21 +1,19 @@
 import { CaretDown } from "@phosphor-icons/react"
-import { type PropsWithChildren, useMemo } from "react"
+import type { PropsWithChildren } from "react"
 import {
     recipeDisclosureContent,
     recipeDisclosureDetails,
     recipeDisclosureSummary,
-} from "#src/components/primitives/disclosure/variants.js"
+} from "#src/components/primitives/disclosure/variants"
 
 const RequestDetails = ({ children }: PropsWithChildren) => {
-    const memoizedChildren = useMemo(() => children, [children])
-
     return (
         <details className={recipeDisclosureDetails({ intent: "gradient" })}>
             <summary className={recipeDisclosureSummary()}>
                 View Request Details:
                 <CaretDown size="1.25em" />
             </summary>
-            <div className={recipeDisclosureContent({ intent: "gradient" })}>{memoizedChildren}</div>
+            <div className={recipeDisclosureContent({ intent: "gradient" })}>{children}</div>
         </details>
     )
 }
