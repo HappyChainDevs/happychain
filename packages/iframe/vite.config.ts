f3c3b627783577c9ee65ae3dc115abb0e757c716
naomiHauret <hauretnaomi@gmail.com>
2024-12-09 17:30:49 +0100

    chore(iframe): ensure vite config load env variables properly

--- packages/iframe/vite.config.ts
+++ packages/iframe/vite.config.ts
@@ -2,67 +2,68 @@
 import { SharedWorkerPlugin } from "@happychain/worker"
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import z from "zod"
 
-// Quick build time env checks
-
-const config = z
-    .object({
-        VITE_FIREBASE_API_KEY: z.string(),
-        VITE_FIREBASE_AUTH_DOMAIN: z.string(),
-        VITE_FIREBASE_PROJECT_ID: z.string(),
-        VITE_FIREBASE_STORAGE_BUCKET: z.string(),
-        VITE_FIREBASE_MESSAGE_SENDER_ID: z.string(),
-        VITE_FIREBASE_APP_ID: z.string(),
-        VITE_WEB3AUTH_CLIENT_ID: z.string(),
-        VITE_WEB3AUTH_NETWORK: z.string(),
-        VITE_WEB3AUTH_VERIFIER: z.string(),
-    })
-    .safeParse(import.meta.env ?? process.env) // import.meta.env is unavailable in tests
-if (config.error) {
-    console.log(config.error.errors)
-    process.exit(1)
-}
+const envConfigSchema = z.object({
+    VITE_BUNDLER_RPC_URL: z.string(),
+    VITE_FIREBASE_API_KEY: z.string(),
+    VITE_FIREBASE_AUTH_DOMAIN: z.string(),
+    VITE_FIREBASE_PROJECT_ID: z.string(),
+    VITE_FIREBASE_STORAGE_BUCKET: z.string(),
+    VITE_FIREBASE_MESSAGE_SENDER_ID: z.string(),
+    VITE_FIREBASE_APP_ID: z.string(),
+    VITE_WEB3AUTH_CLIENT_ID: z.string(),
+    VITE_WEB3AUTH_NETWORK: z.string(),
+    VITE_WEB3AUTH_VERIFIER: z.string(),
+})
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-    optimizeDeps: {
-        // Vite seems unable to properly optimize shared-workers with code-gen when switching
-        // between branches.
-        exclude: command === "serve" ? ["@happychain/worker"] : [],
-    },
-    server: { port: 5160 },
-    preview: { port: 4160 },
-    plugins: [
-        TanStackRouterVite(),
-        react({ babel: { presets: ["jotai/babel/preset"] } }),
-        SharedWorkerPlugin({
-            disabled: false,
-            prodChunks: sharedWorkerChunkStrategy(),
-        }),
-    ],
-    resolve: {
-        alias: {
-            // https://web3auth.io/docs/troubleshooting/vite-issues
-            crypto: "empty-module",
-            http: "empty-module",
-            https: "empty-module",
-            url: "empty-module",
-            zlib: "empty-module",
-            stream: "empty-module",
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), "")
+    const validateConfig = envConfigSchema.safeParse(env)
+    if (validateConfig.error) {
+        console.error(validateConfig.error.errors)
+        process.exit(1)
+    }
+    return {
+        optimizeDeps: {
+            // Vite seems unable to properly optimize shared-workers with code-gen when switching
+            // between branches.
+            exclude: command === "serve" ? ["@happychain/worker"] : [],
         },
-    },
+        server: { port: 5160 },
+        preview: { port: 4160 },
+        plugins: [
+            TanStackRouterVite(),
+            react({ babel: { presets: ["jotai/babel/preset"] } }),
+            SharedWorkerPlugin({
+                disabled: false,
+                prodChunks: sharedWorkerChunkStrategy(),
+            }),
+        ],
+        resolve: {
+            alias: {
+                // https://web3auth.io/docs/troubleshooting/vite-issues
+                crypto: "empty-module",
+                http: "empty-module",
+                https: "empty-module",
+                url: "empty-module",
+                zlib: "empty-module",
+                stream: "empty-module",
+            },
+        },
 
-    build: {
-        rollupOptions: {
-            external: [/\\.mocks$/],
+        build: {
+            rollupOptions: {
+                external: [/\\.mocks$/],
+            },
         },
-    },
-    test: {
-        environment: "happy-dom",
-    },
-}))
+        test: {
+            environment: "happy-dom",
+        },
+    }
+})
 
 /**
  * Chunking here is optional, but extracting the common runtime into a
