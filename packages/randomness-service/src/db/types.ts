f43a19f4fb5b380de61bc9454fd5bdd0e2b9fe32
Gabriel Martinez Rodriguez <gabrielmartinezrdrgz@gmail.com>
2024-12-09 17:27:21 +0100

    chore(randomnes-service): added comments into CommitmentInfoTable

--- packages/randomness-service/src/db/types.ts
+++ packages/randomness-service/src/db/types.ts
@@ -2,7 +2,9 @@ import type { Hex, UUID } from "@happychain/common"
 import type { CommitmentInfo } from "../CommitmentManager"
 
 export interface CommitmentInfoTable {
+    // The timestamp is stored as a number because Kisely automatically converts bigint to number
     timestamp: number
+    // The value is stored as a string because it can be a large number bigger than the max value of an SQLite integer
     value: string
     commitment: Hex
     transactionIntentId: UUID
