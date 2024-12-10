23e2d3021f6fb1abdf4c85d06bf8ee736a553967
naomiHauret <hauretnaomi@gmail.com>
2024-12-10 17:28:40 +0100

    refacto(iframe): wip - refactor to address comments

--- packages/iframe/src/requests/approved.ts
+++ packages/iframe/src/requests/approved.ts
@@ -46,7 +46,7 @@ export async function dispatchHandlers(request: PopupMsgs[Msgs.PopupApprove]) {
             const tx = request.payload.params[0]
             const smartAccountClient = await getSmartAccountClient()
             let hash: Hash
-
+            /*
             if (smartAccountClient?.account) {
                 const userOp = await convertTxToUserOp(
                     {
@@ -54,13 +54,16 @@ export async function dispatchHandlers(request: PopupMsgs[Msgs.PopupApprove]) {
                         data: tx.data,
                         value: tx.value ? hexToBigInt(tx.value) : 0n,
                     },
-                    smartAccountClient,
+                    smartAccountClient.account.address,
                 )
 
-                hash = (await smartAccountClient.sendUserOperation(userOp)) as Hash
+            hash = (await smartAccountClient.sendUserOperation(userOp)) as Hash
             } else {
                 hash = (await sendToWalletClient(request)) as Hash
             }
+            */
+
+            hash = (await sendToWalletClient(request)) as Hash
 
             // Track pending transaction
             const value = hexToBigInt(tx.value as Hex)
