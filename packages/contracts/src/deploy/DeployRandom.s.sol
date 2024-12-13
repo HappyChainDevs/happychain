c9e27ee3ec6b507f0bb37601304f1c3c36be6a9e
Gabriel Martinez Rodriguez <gabrielmartinezrdrgz@gmail.com>
2024-12-13 16:21:50 +0100

    feat(contracts): drand contract

--- packages/contracts/src/deploy/DeployRandom.s.sol
+++ packages/contracts/src/deploy/DeployRandom.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import {BaseDeployScript} from "./BaseDeployScript.sol";
-import {Random} from "../Random.sol";
+import {Random} from "../Randomness/Random.sol";
 
 /**
  * @dev Deploys the Randomness contract.
@@ -10,8 +10,21 @@ import {Random} from "../Random.sol";
 contract DeployL1 is BaseDeployScript {
     Random public random;
 
+    uint256[4] public DRAND_PUBLIC_KEY;
+    uint256 public constant DRAND_GENESIS_TIMESTAMP = 1727521075;
+    uint256 public constant DRAND_PERIOD = 3;
+
+    constructor() {
+        DRAND_PUBLIC_KEY = [
+            2416910118189096557713698606232949750075245832257361418817199221841198809231,
+            3565178688866727608783247307855519961161197286613423629330948765523825963906,
+            18766085122067595057703228467555884757373773082319035490740181099798629248523,
+            263980444642394177375858669180402387903005329333277938776544051059273779190
+        ];
+    }
+
     function deploy() internal override {
-        random = new Random();
+        random = new Random(DRAND_PUBLIC_KEY, DRAND_GENESIS_TIMESTAMP, DRAND_PERIOD);
         deployed("Random", address(random));
     }
 }
