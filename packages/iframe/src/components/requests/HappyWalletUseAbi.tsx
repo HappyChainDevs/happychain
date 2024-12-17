533059e70a40f535583e1320bdf4831894bf3956
ultraviolet10 <aritrac1998@hotmail.com>
2024-12-17 12:25:57 +0530

    refactor: use layout components for new req

--- packages/iframe/src/components/requests/HappyWalletUseAbi.tsx
+++ packages/iframe/src/components/requests/HappyWalletUseAbi.tsx
@@ -1,7 +1,10 @@
 import { WALLET_USE_ABI_RPC_METHOD } from "@happychain/common"
 import { useAtomValue } from "jotai"
-import { requestLabels } from "#src/constants/requestLabels"
 import { currentChainAtom } from "#src/state/chains"
+import { Button } from "../primitives/button/Button"
+import RawRequestDetails from "./common/RawRequestDetails"
+import RequestContent from "./common/RequestContent"
+import RequestLayout from "./common/RequestLayout"
 import type { RequestConfirmationProps } from "./props"
 
 export function HappyWalletUseAbi({
@@ -13,14 +16,9 @@ export function HappyWalletUseAbi({
     const chain = useAtomValue(currentChainAtom)
 
     return (
-        <main className="flex h-dvh flex-col items-start justify-between gap-4 bg-base-300 p-4">
-            <div className="flex w-full grow flex-col gap-4">
-                <div className="w-full rounded-lg bg-neutral p-4 font-bold">{window.location.origin}</div>
-                <div className="w-full rounded-lg bg-neutral p-4 font-bold">
-                    {requestLabels[method] ?? "Unknown Signature Type"}
-                </div>
-
-                <div className="flex grow flex-col gap-4 bg-zinc-100 rounded-xl p-4">
+        <RequestLayout method={method}>
+            <RequestContent>
+                <div className="flex flex-col w-full h-full gap-4 bg-zinc-100 rounded-xl p-4">
                     <div className="border-b border-zinc-300 pb-2 text-center text-sm font-bold text-blue-600">
                         Details
                     </div>
@@ -37,27 +35,27 @@ export function HappyWalletUseAbi({
                     </div>
                     <div className="flex flex-col grow gap-2">
                         <span className="text-sm font-bold text-gray-600">ABI:</span>
-                        <textarea
-                            className="grow w-full p-2 rounded-lg bg-neutral text-sm font-mono break-all overflow-auto resize-none"
-                            readOnly
-                            value={JSON.stringify(params?.abi, null, 2)}
-                        />
+                        <pre className="grow w-full p-2 rounded-lg bg-neutral text-sm font-mono break-all overflow-auto h-[300px]">
+                            <code>{JSON.stringify(params?.abi, null, 2)}</code>
+                        </pre>
                     </div>
-                </div>
-            </div>
 
-            <div className="flex w-full gap-4">
-                <button
-                    type="button"
-                    className="btn grow border-2 bg-success/75"
+                    <RawRequestDetails params={params} />
+                </div>
+            </RequestContent>
+
+            <div className="flex flex-col w-full gap-2">
+                <Button
+                    intent="primary"
+                    className="text-neutral-content justify-center"
                     onClick={() => accept({ method: WALLET_USE_ABI_RPC_METHOD, params })}
                 >
-                    Sign
-                </button>
-                <button type="button" className="btn border-2 border-red-300 bg-error/75" onClick={reject}>
+                    Add ABI
+                </Button>
+                <Button intent="outline-negative" className="text-base-content justify-center" onClick={reject}>
                     Reject
-                </button>
+                </Button>
             </div>
-        </main>
+        </RequestLayout>
     )
 }
