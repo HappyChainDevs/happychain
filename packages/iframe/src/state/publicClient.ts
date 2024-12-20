4e3a056ee06f70de98741ea6828ab9e380d8d30b
Nicolas "Norswap" Laurent <norswap@gmail.com>
2024-12-20 03:49:46 +0100

    add comment on transport selection in publicClientAtom

--- packages/iframe/src/state/publicClient.ts
+++ packages/iframe/src/state/publicClient.ts
@@ -9,6 +9,8 @@ import { transportAtom } from "./transport"
 
 export const publicClientAtom: Atom<PublicClient<CustomTransport | HttpTransport>> = atom((get) => {
     const chain = get(currentChainAtom)
+
+    // The fallback to the public RPC URL only triggers whenever the user is not logged in.
     const transport = get(transportAtom) ?? http(chain.rpcUrls[0], { batch: true })
 
     const publicClient = createPublicClient({ transport, chain: convertToViemChain(chain) })
