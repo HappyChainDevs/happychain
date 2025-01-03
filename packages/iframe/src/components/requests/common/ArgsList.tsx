d4e863615d881807256f27b7a6952a6e2e4d82e0
ultraviolet10 <aritrac1998@hotmail.com>
2025-01-03 17:27:45 +0530

    format

--- packages/iframe/src/components/requests/common/ArgsList.tsx
+++ packages/iframe/src/components/requests/common/ArgsList.tsx
@@ -5,7 +5,7 @@ interface ArgsListProps {
     fnInputs: readonly AbiParameter[]
 }
 
-function formatDisplayValue (arg: unknown) {
+function formatDisplayValue(arg: unknown) {
     const rawType = typeof arg
     return rawType === "bigint" ? (arg as bigint).toString() : String(arg)
 }
@@ -15,10 +15,8 @@ const ArgsList = ({ args, fnInputs }: ArgsListProps) => {
         <div className="flex flex-col gap-2 w-full mt-2">
             {args.map((arg, idx) => {
                 return (
-                    <div key={`arg-${(idx)}`} className="flex justify-between items-baseline gap-[1ex]">
-                        <span className="font-mono text-sm opacity-75">
-                            {fnInputs[idx].name}
-                        </span>
+                    <div key={"calldef"} className="flex justify-between items-baseline gap-[1ex]">
+                        <span className="font-mono text-sm opacity-75">{fnInputs[idx].name}</span>
                         <span className="font-mono text-sm truncate break-words hover:whitespace-normal hover:overflow-visible hover:text-overflow-clip hover:bg-neutral-100 hover:break rounded-lg p-1">
                             {formatDisplayValue(arg)} <span className="opacity-50">{fnInputs[idx].type}</span>
                         </span>
