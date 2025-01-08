389fb6f3658c32100095a3a32c83592ef73c9e4b
Aryan Godara <aryangodara03@gmail.com>
2025-01-08 13:34:40 +0530

    minor refactor, add comments on exported functions and constants

--- packages/contracts/scripts/utils/accounts.ts
+++ packages/contracts/scripts/utils/accounts.ts
@@ -7,15 +7,25 @@ import { abis as mockAbis, deployment as mockDeployment } from "../../deployment
 import { abis, deployment } from "../../deployments/anvil/testing/abis"
 import { account, publicClient, walletClient } from "./clients"
 
-/** Amount of ETH to mint/send in demo transactions for verification */
+// Amount of ETH to mint/send in userOps in the demo.
 export const AMOUNT = "0.01"
-/** Amount of ETH to deposit into paymasters/smart accounts for funding userOps */
+// Amount of ETH to deposit into paymasters/smart accounts for funding userOps (on Anvil).
 export const DEPOSIT = parseEther("100")
 
-export function getRandomAddress() {
+export function getRandomAddress(): Address {
     return privateKeyToAddress(generatePrivateKey()).toString() as Address
 }
 
+export function toHexDigits(number: bigint, size: number): string {
+    return numberToHex(number, { size }).slice(2)
+}
+
+/**
+ * Send Eth to the deterministic address of a SCA.
+ *
+ * @param {Address} accountAddress - The address to send Eth to.
+ * @return {Promise<"success" | "reverted">} - A promise that resolves with the transaction status.
+ */
 export async function fundSmartAccount(accountAddress: Address): Promise<"success" | "reverted"> {
     const txHash = await walletClient.sendTransaction({
         account: account,
@@ -32,6 +42,11 @@ export async function fundSmartAccount(accountAddress: Address): Promise<"succes
     return receipt.status
 }
 
+/**
+ * Deposit Eth to the EntryPoint contract on behalf of the HappyPaymaster, so it can sponsor userOps.
+ *
+ * @return {Promise<"success" | "reverted">} - A promise that resolves with the transaction status.
+ */
 export async function depositPaymaster(): Promise<"success" | "reverted"> {
     const txHash = await walletClient.writeContract({
         address: entryPoint07Address,
@@ -49,6 +64,12 @@ export async function depositPaymaster(): Promise<"success" | "reverted"> {
     return receipt.status
 }
 
+/**
+ * Mint mock tokens to initialize the MockToken contract's balanceOf[accountAddress] mapping's storage slot.
+ *
+ * @param {Address} accountAddress - The address to mint tokens for.
+ * @return {Promise<"success" | "reverted">} - A promise that resolves with the transaction status.
+ */
 export async function initializeTokenBalance(accountAddress: Address): Promise<"success" | "reverted"> {
     const hash = await walletClient.writeContract({
         address: mockDeployment.MockTokenA,
@@ -61,6 +82,12 @@ export async function initializeTokenBalance(accountAddress: Address): Promise<"
     return receipt.status
 }
 
+/**
+ * Encode the callData field for userOps, used in the demo.
+ *
+ * @param {Address} [to] - The address to mint tokens for.
+ * @return {UserOperationCall} - The encoded call data.
+ */
 export function createMintCall(to?: Address): UserOperationCall {
     return {
         to: mockDeployment.MockTokenA,
@@ -73,10 +100,12 @@ export function createMintCall(to?: Address): UserOperationCall {
     }
 }
 
-export function toHexDigits(number: bigint, size: number): string {
-    return numberToHex(number, { size }).slice(2)
-}
-
+/**
+ * Get the balance of an address in unit of Eth.
+ *
+ * @param {Address} receiver - The address to get the balance for.
+ * @return {Promise<string>} - A promise that resolves with the formatted balance.
+ */
 export async function getFormattedBalance(receiver: Address): Promise<string> {
     const balance = await publicClient.getBalance({
         address: receiver,
@@ -86,6 +115,12 @@ export async function getFormattedBalance(receiver: Address): Promise<string> {
     return formatEther(balance)
 }
 
+/**
+ * Get the token balance of an address in unit of Eth.
+ *
+ * @param {Address} address - The address to get the token balance for.
+ * @return {Promise<string>} - A promise that resolves with the formatted token balance.
+ */
 export async function getFormattedTokenBalance(address: Address): Promise<string> {
     const balance = await publicClient.readContract({
         address: mockDeployment.MockTokenA,
