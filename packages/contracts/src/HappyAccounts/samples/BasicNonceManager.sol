c874d3f5ea72d66f9d4452cc6c72e341d8ef55ea
Aryan Godara <aryangodara03@gmail.com>
2025-01-10 16:52:50 +0530

    update happyAccount to new spec [skip ci]

---
+++ packages/contracts/src/HappyAccounts/samples/BasicNonceManager.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity ^0.8.20;
+
+/**
+ * @title NonceManager
+ * @dev   Contract implementing nonce management for a HappyAccount
+ */
+contract BasicNonceManager {
+    event NonceUsed(uint256 indexed nonce);
+
+    uint256 private _nonce;
+
+    function getNonce() external view returns (uint256) {
+        return _nonce;
+    }
+
+    /**
+     * Validates and updates the nonce
+     * @param nonceAhead The difference between current nonce and happyTx.nonce
+     * @param isSimulation Wether the current transaction is a simulation (tx.origin == address(0))
+     * @return status The validation status of the nonce
+     */
+    function _validateAndUpdateNonce(int256 nonceAhead, bool isSimulation) internal returns (bool) {
+        if (nonceAhead < 0 || (!isSimulation && nonceAhead != 0)) {
+            return false;
+        }
+
+        emit NonceUsed(_nonce++);
+        return true;
+    }
+}
---
+++ packages/contracts/src/HappyAccounts/utils/ParallelNonceManager.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity ^0.8.20;
+
+/**
+ * @title ParallelNonceManager
+ * @dev   Contract implementing nonce management for a HappyAccount
+ */
+contract ParallelNonceManager {
+    /**
+     * @dev Emitted when a nonce is used in a track
+     * @param track The track number
+     * @param nonce The nonce that was used
+     */
+    event NonceUsed(uint64 indexed track, uint64 nonce);
+
+    /// @dev Mapping from track => nonce
+    mapping(uint64 => uint64) private _nonces;
+
+    function getNonce(uint64 track) public view returns (uint64) {
+        return _nonces[track];
+    }
+
+    /**
+     * @dev Internal function to validate and update nonce
+     * @param track The track number
+     * @param nonce The nonce to validate
+     * @return true if nonce was valid and updated
+     */
+    function _validateAndUpdateNonce(uint64 track, uint64 nonce) internal returns (bool) {
+        if (_nonces[track] != nonce) {
+            return false;
+        }
+
+        _nonces[track]++;
+        emit NonceUsed(track, nonce);
+        return true;
+    }
+}
