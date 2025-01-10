3e3426fc22a3dc4fb4f8313779ac2c43cdb59aec
Aryan Godara <aryangodara03@gmail.com>
2025-01-10 23:45:47 +0530

    update account factory code to new spec [skip ci]

--- packages/contracts/src/HappyAccounts/factories/ScrappyAccountFactory.sol
+++ packages/contracts/src/HappyAccounts/factories/ScrappyAccountFactory.sol
@@ -2,15 +2,18 @@
 pragma solidity ^0.8.20;
 
 import {LibClone} from "solady/utils/LibClone.sol";
+import {ScrappyAccount} from "../core/ScrappyAccount.sol";
+import {ScrappyAccount} from "../samples/ScrappyAccount.sol";
 
 /**
- * @title  HappyAccountFactory
- * @dev    Factory contract for deploying deterministic ERC1967 proxies for HappyAccounts
+ * @title  ScrappyAccountFactory
+ * @dev    Example factory contract for deploying deterministic ERC1967 proxies for {@link ScrappyAccount}.
  */
-contract HappyAccountFactory {
+contract ScrappyAccountFactory {
     error InitializeError();
+    error AlreadyDeployed();
 
-    /// @dev The implementation contract that all proxies will delegate to
+    /// @dev The implementation contract that all proxies will delegate to {@link ScrappyAccount}.
     address public immutable IMPLEMENTATION;
 
     /**
@@ -30,20 +33,20 @@ contract HappyAccountFactory {
      * @param salt A unique salt for deterministic deployment
      * @return The address of the created account
      */
-    function createAccount(bytes calldata initData, bytes32 salt) public payable returns (address) {
+    function createAccount(bytes calldata initData, address entryPoint, address owner)
+        public
+        payable
+        returns (address)
+    {
         bytes32 actualSalt = keccak256(abi.encodePacked(initData, salt));
-
         (bool alreadyDeployed, address account) =
             LibClone.createDeterministicERC1967(msg.value, IMPLEMENTATION, actualSalt);
 
-        if (!alreadyDeployed) {
-            // solhint-disable-next-line avoid-low-level-calls
-            (bool success,) = account.call(initData);
-            if (!success) {
-                revert InitializeError();
-            }
+        if (alreadyDeployed) {
+            revert AlreadyDeployed();
         }
 
+        ScrappyAccount(account).initialize(entryPoint, owner);
         emit HappyAccountCreated(account, salt);
         return account;
     }
