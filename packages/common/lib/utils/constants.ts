d6a64d350c7dc8c3d8d6a745a0eb2c4a40e08bda
naomiHauret <hauretnaomi@gmail.com>
2025-01-16 14:34:47 +0100

    feat(session-keys): define session keys types and storage references

--- packages/common/lib/utils/constants.ts
+++ packages/common/lib/utils/constants.ts
@@ -1,4 +1,5 @@
 export enum HappyMethodNames {
     HAPPY_USER_RPC_METHOD = "happy_user",
     WALLET_USE_ABI_RPC_METHOD = "happy_walletUseAbi",
+    REQUEST_SESSION_KEY = "happy_requestSessionKey",
 }
