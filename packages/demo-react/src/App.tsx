ba032cc393cd7bd162fb1d1be2c81df6aed5b5f8
not-reed <reed@happy.tech>
2025-01-20 13:46:47 -0800

    clean up demo styles

--- packages/demo-react/src/App.tsx
+++ packages/demo-react/src/App.tsx
@@ -121,86 +121,141 @@ function App() {
         <main className='flex min-h-dvh w-full flex-col items-center gap-4 bg-[url("/francesco-ungaro-Wn8JoB8FP70-unsplash.jpg")] bg-cover p-4'>
             <h1 className="p-16 text-4xl font-bold text-white">HappyChain + TS + React + Viem</h1>
 
-            <button
-                type="button"
-                onClick={() => {
-                    user ? disconnect() : connect()
-                }}
-                className="rounded-lg bg-sky-300 p-2 shadow-xl"
-            >
-                {user ? "Disconnect" : "Connect"}
-            </button>
+            <div className="flex flex-col md:grid grid-cols-2 gap-4 w-full md:max-w-screen-lg">
+                <div className="rounded-lg flex flex-col gap-4 p-4 backdrop-blur-sm bg-gray-200/35 col-span-2">
+                    <div className="text-lg font-bold">Connection Methods</div>
 
-            <ConnectButton />
+                    <div className="flex gap-4">
+                        <div className="flex flex-col h-full justify-between">
+                            <button
+                                type="button"
+                                onClick={() => (user ? disconnect() : connect())}
+                                className="rounded-lg bg-sky-300 p-2 shadow-xl"
+                            >
+                                {user ? "Disconnect" : "Connect"}
+                            </button>
+                            <small>Custom Button</small>
+                        </div>
 
-            <div className="max-w-96 w-full overflow-auto bg-gray-200 p-4">
-                <p className="text-lg font-bold">User Details</p>
-                <pre>{JSON.stringify(user, null, 2)}</pre>
+                        <span className="border border-gray-200/35" />
+
+                        <div className="flex flex-col h-full justify-between">
+                            <ConnectButton />
+                            <small>Default Badge</small>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="overflow-auto backdrop-blur-sm bg-gray-200/35 col-span-2 p-4 flex flex-col gap-4 rounded-lg">
+                    <p className="text-lg font-bold">User Details</p>
+                    <pre>{JSON.stringify(user, null, 2)}</pre>
+                </div>
+
+                <div className=" rounded-lg flex flex-col gap-4 p-4 backdrop-blur-sm bg-gray-200/35 col-span-2">
+                    <div className="text-lg font-bold">RPC Calls</div>
+                    <div className="flex gap-4">
+                        <div className="h-full">
+                            <button
+                                type="button"
+                                onClick={() => signMessage("Hello, World!")}
+                                className="rounded-lg bg-sky-300 p-2 shadow-xl whitespace-nowrap w-36"
+                            >
+                                Sign Message
+                            </button>
+                        </div>
+                        <div className="w-full overflow-auto">
+                            <div className="text-lg flex gap-2 whitespace-nowrap">
+                                <p className="font-bold">Raw Message: </p>
+                                <pre>Hello, World!</pre>
+                            </div>
+
+                            <div className="text-lg flex gap-2 whitespace-nowrap">
+                                <p className="font-bold">Signed Message:</p>
+                                <pre className="overflow-auto">{signatureResult}</pre>
+                            </div>
+                        </div>
+                    </div>
+                    <hr />
+
+                    <div className="flex gap-4 ">
+                        <div className="h-full">
+                            <button
+                                type="button"
+                                onClick={getBlock}
+                                className="rounded-lg bg-sky-300 p-2 shadow-xl whitespace-nowrap w-36"
+                            >
+                                Get Block
+                            </button>
+                        </div>
+
+                        <div className="w-full overflow-auto">
+                            <p className="text-lg font-bold">Results:</p>
+                            <pre className="max-h-48 overflow-auto w-full">
+                                {blockResult
+                                    ? JSON.stringify(
+                                          blockResult,
+                                          (_, v) => (typeof v === "bigint" ? v.toString() : v),
+                                          2,
+                                      )
+                                    : ""}
+                            </pre>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-2 gap-4 backdrop-blur-sm bg-gray-200/35 p-4 rounded-lg">
+                    <div className="text-lg font-bold col-span-2">Chain Switching</div>
+
+                    <button type="button" onClick={addChain} className="rounded-lg bg-sky-300 p-2 shadow-xl">
+                        Add Gnosis
+                    </button>
+
+                    <button
+                        type="button"
+                        onClick={addConflictedChain}
+                        className="rounded-lg bg-sky-300 p-2 shadow-xl whitespace-nowrap"
+                    >
+                        Add "Gnosis 2"
+                        <small>(creates conflict)</small>
+                    </button>
+
+                    <button
+                        type="button"
+                        onClick={() => switchChain(gnosis.id)}
+                        className="rounded-lg bg-sky-300 p-2 shadow-xl"
+                    >
+                        Switch to Gnosis
+                    </button>
+
+                    <button
+                        type="button"
+                        onClick={() => switchChain(happyChainSepolia.id)}
+                        className="rounded-lg bg-sky-300 p-2 shadow-xl"
+                    >
+                        Switch to HappyChain Sepolia
+                    </button>
+                </div>
+
+                <div className="grid grid-cols-2 gap-4 backdrop-blur-sm bg-gray-200/35 p-4 rounded-lg">
+                    <div className="text-lg font-bold col-span-2">Wallet Functionality</div>
+                    <button type="button" onClick={sendStub} className="rounded-lg bg-sky-300 p-2 shadow-xl">
+                        Show Send Screen
+                    </button>
+
+                    <div className="flex flex-row w-full items-center justify-center space-x-6">
+                        <button type="button" onClick={addNewToken} className="rounded-lg bg-sky-300 p-2 shadow-xl">
+                            Add Token
+                        </button>
+
+                        <button type="button" onClick={mintTokens} className="rounded-lg bg-sky-300 p-2 shadow-xl">
+                            Mint Token
+                        </button>
+                    </div>
+                    <button type="button" onClick={loadAbiStub} className="rounded-lg bg-sky-300 p-2 shadow-xl">
+                        Load ABI
+                    </button>
+                </div>
             </div>
-
-            <button
-                type="button"
-                onClick={() => signMessage("Hello, World!")}
-                className="rounded-lg bg-sky-300 p-2 shadow-xl"
-            >
-                Sign Message
-            </button>
-
-            <div className="max-w-96 w-full overflow-auto bg-gray-200 p-4">
-                <p className="text-lg font-bold">Results:</p>
-                <pre>{signatureResult}</pre>
-            </div>
-
-            <button type="button" onClick={getBlock} className="rounded-lg bg-sky-300 p-2 shadow-xl">
-                Get Block
-            </button>
-
-            <div className="max-w-96 w-full overflow-auto bg-gray-200 p-4">
-                <p className="text-lg font-bold">Results:</p>
-                <pre>{JSON.stringify(blockResult, (_, v) => (typeof v === "bigint" ? v.toString() : v), 2)}</pre>
-            </div>
-
-            <button type="button" onClick={addChain} className="rounded-lg bg-sky-300 p-2 shadow-xl">
-                Add Gnosis
-            </button>
-
-            <button type="button" onClick={addConflictedChain} className="rounded-lg bg-sky-300 p-2 shadow-xl">
-                Add "Gnosis 2 (creates conflict)"
-            </button>
-
-            <button
-                type="button"
-                onClick={() => switchChain(gnosis.id)}
-                className="rounded-lg bg-sky-300 p-2 shadow-xl"
-            >
-                Switch to Gnosis
-            </button>
-
-            <button
-                type="button"
-                onClick={() => switchChain(happyChainSepolia.id)}
-                className="rounded-lg bg-sky-300 p-2 shadow-xl"
-            >
-                Switch to HappyChain Sepolia
-            </button>
-
-            <button type="button" onClick={sendStub} className="rounded-lg bg-sky-300 p-2 shadow-xl">
-                Show Send Screen
-            </button>
-
-            <div className="flex flex-row w-full items-center justify-center space-x-6">
-                <button type="button" onClick={addNewToken} className="rounded-lg bg-sky-300 p-2 shadow-xl">
-                    Add Token
-                </button>
-
-                <button type="button" onClick={mintTokens} className="rounded-lg bg-sky-300 p-2 shadow-xl">
-                    Mint Token
-                </button>
-            </div>
-
-            <button type="button" onClick={loadAbiStub} className="rounded-lg bg-sky-300 p-2 shadow-xl">
-                Load ABI
-            </button>
         </main>
     )
 }
