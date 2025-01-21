f427e9cad5ccadd7d7968640e564287cd1d93181
ultraviolet10 <aritrac1998@hotmail.com>
2025-01-21 15:47:58 +0530

    deploy counter contract, update demo

--- packages/contracts/src/deploy/mocks/DeployCounter.s.sol
+++ packages/contracts/src/deploy/mocks/DeployCounter.s.sol
@@ -5,12 +5,12 @@ import {HappyCounter} from "../../mocks/HappyCounter.sol";
 import {BaseDeployScript} from "../BaseDeployScript.sol";
 
 contract DeployHappyCounter is BaseDeployScript {
-    bytes32 public constant DEPLOYMENT_SALT = bytes32(uint256(0));
+    bytes32 public constant DEPLOYMENT_SALT = bytes32(uint256(13));
 
     HappyCounter public counter;
 
     function deploy() internal override {
-        counter = new HappyCounter{salt: DEPLOYMENT_SALT}();
+        counter = new HappyCounter();
         deployed("++:)", "HappyCounter", address(counter));
     }
 }
