2fbb29d6267defba1702e69ecd0f5e7887464771
not-reed <reed@happy.tech>
2025-01-22 11:43:02 -0800

    getAccountAbstractionContracts chainDefinitions

--- packages/iframe/src/utils/getAccountAbstractionContracts.ts
+++ packages/iframe/src/utils/getAccountAbstractionContracts.ts
@@ -1,10 +1,10 @@
 import { type Deployment, deployment as contractsAddresses } from "@happychain/contracts/account-abstraction/sepolia"
-import { chains } from "@happychain/sdk-shared"
+import { chainDefinitions } from "@happychain/sdk-shared"
 
 export function getAccountAbstractionContracts(chainId: string): Deployment {
     let contracts = {}
     switch (chainId) {
-        case chains.happyChainSepolia.chainId:
+        case chainDefinitions.happyChainSepolia.chainId:
             contracts = contractsAddresses
             break
         default:
