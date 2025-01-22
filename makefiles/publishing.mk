cb0763f4dc573ab4a30b7f38d3d3aeb58ff596c4
not-reed <reed@happy.tech>
2025-01-22 15:38:33 -0800

    publish-prep

---
+++ makefiles/publishing.mk
@@ -0,0 +1,8 @@
+pack.dry-run: ## Dry Run package to view bundle size
+	bun pm pack --dry-run
+.PHONY: pack.dry-run
+
+publish.dry-run: ## Dry Run publish to npm
+	bun publish --dry-run
+.PHONY: publish.dry-run
+
