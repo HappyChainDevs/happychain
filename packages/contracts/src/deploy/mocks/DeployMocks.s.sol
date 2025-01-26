b5d53daf547c74a601d74c7b8803db6ebdbd2ae8
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-01-26 22:50:51 +0800

    make deploy-mocks use the new deployDeterministic

--- packages/contracts/src/deploy/mocks/DeployMocks.s.sol
+++ packages/contracts/src/deploy/mocks/DeployMocks.s.sol
@@ -8,22 +8,24 @@ import {BaseDeployScript} from "../BaseDeployScript.sol";
  * @dev Deploys mock contracts for testing purposes.
  */
 contract DeployMockERC20 is BaseDeployScript {
-    bytes32 public constant DEPLOYMENT_SALT_1 = bytes32(uint256(0));
-    bytes32 public constant DEPLOYMENT_SALT_2 = bytes32(uint256(1));
-    bytes32 public constant DEPLOYMENT_SALT_3 = bytes32(uint256(2));
+    // Every token uses the same contract, and so needs a different salt.
+    bytes32 public constant SALT_TOKEN_A = bytes32(uint256(0));
+    bytes32 public constant SALT_TOKEN_B = bytes32(uint256(1));
+    bytes32 public constant SALT_TOKEN_C = bytes32(uint256(2));
 
     MockERC20Token public mockTokenA;
     MockERC20Token public mockTokenB;
     MockERC20Token public mockTokenC;
 
     function deploy() internal override {
-        mockTokenA = new MockERC20Token{salt: DEPLOYMENT_SALT_1}("MockTokenA", "MTA", 18);
-        deployed("MockTokenA", "MockERC20Token", address(mockTokenA));
+        deployMockToken("MockTokenA", "MTA", SALT_TOKEN_A);
+        deployMockToken("MockTokenB", "MTB", SALT_TOKEN_B);
+        deployMockToken("MockTokenC", "MTC", SALT_TOKEN_C);
+    }
 
-        mockTokenB = new MockERC20Token{salt: DEPLOYMENT_SALT_2}("MockTokenB", "MTB", 18);
-        deployed("MockTokenB", "MockERC20Token", address(mockTokenB));
-
-        mockTokenC = new MockERC20Token{salt: DEPLOYMENT_SALT_3}("MockTokenC", "MTC", 18);
-        deployed("MockTokenC", "MockERC20Token", address(mockTokenC));
+    function deployMockToken(string memory name, string memory symbol, bytes32 salt) internal {
+        deployDeterministic(
+            name, "MockERC20Token", type(MockERC20Token).creationCode, abi.encode(name, symbol, uint8(18)), salt
+        );
     }
 }
