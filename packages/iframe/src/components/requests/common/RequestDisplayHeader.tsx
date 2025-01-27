2f274feab197c9f2238c2c90fa6d97d7797b0d23
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-01-27 20:42:15 +0800

    display correct app URL

--- packages/iframe/src/components/requests/common/RequestDisplayHeader.tsx
+++ packages/iframe/src/components/requests/common/RequestDisplayHeader.tsx
@@ -1,4 +1,5 @@
 import { requestLabels } from "#src/constants/requestLabels"
+import { getAppURL } from "#src/utils/appURL"
 
 interface RequestDisplayHeaderProps {
     method: keyof typeof requestLabels
@@ -9,7 +10,7 @@ const RequestDisplayHeader = ({ method }: RequestDisplayHeaderProps) => {
         // Fragment element since the parent `RequestLayout` aligns
         // these in a columnar fashion.
         <>
-            <div className="w-full rounded-lg bg-base-200 p-4 font-bold">{window.location.origin}</div>
+            <div className="w-full rounded-lg bg-base-200 p-4 font-bold">{getAppURL()}</div>
             <div className="w-full rounded-lg bg-base-200 p-4 font-bold">
                 {requestLabels[method] ?? "Unknown Signature Type"}
             </div>
