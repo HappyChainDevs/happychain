131698fada0038896c133499eb5f9589f7cf9850
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-01-28 07:57:20 +0800

    fix build watch mode

--- packages/happybuild/bin/happybuild.ts
+++ packages/happybuild/bin/happybuild.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env bun
 import { join } from "node:path"
+import { promiseWithResolvers } from "@happychain/common"
 import watcher from "@parcel/watcher"
+import { $ } from "bun"
 import { build } from "../lib/build"
 import { cliArgs } from "../lib/cli-args"
 import { getConfigs } from "../lib/config/getConfigs"
@@ -10,29 +12,42 @@ const { default: configs } = await import(join(process.cwd(), cliArgs.config))
 
 if (!cliArgs.watch) {
     await build({ configs, options: cliArgs })
-} else {
-    const _configs = getConfigs(configs, cliArgs)
-
-    const outDirs = Array.from(new Set(_configs.flatMap((c) => c.bunConfig.outdir))).concat([
-        "./node_modules",
-        "./*.tgz",
-    ])
-
-    const debounceBuild = debounceEvent(async () => build({ configs, options: cliArgs }), 250)
-
-    // Build once at start
-    await debounceBuild()
-
-    // rebuild (debounced)
-    const subscription = await watcher.subscribe(process.cwd(), (_err, _events) => debounceBuild(), {
-        ignore: outDirs,
-    })
-
-    process.on("SIGINT", () => {
-        void subscription.unsubscribe()
-        process.exit()
-    })
+    process.exit(0)
 }
+
+// === watch mode ===
+
+// hacky, but disables the spinner which otherwise prevents ctrl+c from working on some terminals
+process.env.CI = "true"
+
+const _configs = getConfigs(configs, cliArgs)
+const outDirs = Array.from(new Set(_configs.flatMap((c) => [c.bunConfig.outdir, c.exportDir]))).concat([
+    "./node_modules",
+    "./*.tgz",
+])
+
+let buildLock = promiseWithResolvers<void>()
+buildLock.resolve()
+
+const queueBuild = debounceEvent(async () => {
+    // wait for lock
+    const lock = buildLock
+    await buildLock.promise
+
+    if (lock !== buildLock) {
+        // another build took the lock
+        console.log("returning")
+        return
+    }
+
+    // take lock
+    buildLock = promiseWithResolvers<void>()
+
+    await build({ configs, options: cliArgs })
+
+    // release lock
+    buildLock.resolve()
+}, 1000)
 
 function debounceEvent(callback: (...args: unknown[]) => void, time: number) {
     let interval: Timer | undefined = undefined
@@ -44,3 +59,20 @@ function debounceEvent(callback: (...args: unknown[]) => void, time: number) {
         }, time)
     }
 }
+
+// build once at start
+await queueBuild()
+
+// rebuild (debounced)
+const subscription = await watcher.subscribe(process.cwd(), (_err, _events) => queueBuild(), {
+    ignore: outDirs,
+})
+
+let sigint = false
+process.prependListener("SIGINT", async () => {
+    if (sigint) return // run only once (somehow it runs twice otherwise)
+    sigint = true
+    console.log("cleanup")
+    await subscription.unsubscribe()
+    process.exit(1)
+})
