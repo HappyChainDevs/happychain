119d0f87b3ecda243c8c7e6be29b90ef0fad3d8b
not-reed <reed@happy.tech>
2025-01-27 12:14:38 -0800

    formatting-makefile-typo

--- makefiles/formatting.mk
+++ makefiles/formatting.mk
@@ -2,8 +2,8 @@
 
 check: ## Runs code quality & formatting checks
 	@biome check ./;
-.PHONT: check
+.PHONY: check
 
 format: ## Formats code and tries to fix code quality issues
 	@biome check ./ --write;
-.PHONT: format
+.PHONY: format
