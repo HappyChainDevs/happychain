18c72e67d72226cbcbedc8a22f499c1f3d02bfa5
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-01-28 01:12:40 +0800

    clean up DeployAA

--- packages/contracts/src/deploy/DeployAA.s.sol
+++ packages/contracts/src/deploy/DeployAA.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 pragma solidity ^0.8.0; // solhint-disable-line
 
-import {BaseDeployScript, console} from "./BaseDeployScript.sol";
+import {BaseDeployScript} from "./BaseDeployScript.sol";
 import {ENTRYPOINT_V7_CODE} from "./initcode/EntryPointV7Code.sol";
 import {ENTRYPOINT_SIMULATIONS_CODE} from "./initcode/EntryPointSimulationsCode.sol";
 
@@ -12,8 +12,6 @@ import {Kernel} from "kernel/Kernel.sol";
 import {KernelFactory} from "kernel/factory/KernelFactory.sol";
 import {FactoryStaker} from "kernel/factory/FactoryStaker.sol";
 import {ECDSAValidator} from "kernel/validator/ECDSAValidator.sol";
-import {IEntryPoint} from "kernel/interfaces/IEntryPoint.sol";
-import {stdJson} from "forge-std/StdJson.sol";
 
 // To ensure ABI generation.
 /* solhint-disable no-unused-import */
@@ -21,125 +19,151 @@ import {EntryPoint} from "account-abstraction/contracts/core/EntryPoint.sol";
 import {EntryPointSimulations} from "account-abstraction/contracts/core/EntryPointSimulations.sol";
 
 contract DeployAAContracts is BaseDeployScript {
-    using stdJson for string;
-
     bytes32 public constant DEPLOYMENT_SALT = bytes32(uint256(4));
     address public constant CREATE2_PROXY = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
     uint256 public constant PAYMASTER_DEPOSIT = 10 ether;
 
-    struct DeploymentAddresses {
-        address ecdsaValidator;
-        address entryPointSimulations;
-        address entryPointV7;
-        address factoryStaker;
-        address happyPaymaster;
-        address happyPaymasterImpl;
-        address kernel;
-        address kernelFactory;
-        address sessionKeyValidator;
-        address sessionKeyValidatorImpl;
-    }
-
-    // Expected addresses will be loaded from deployment.json
-    DeploymentAddresses private expected;
-    KernelFactory private factory;
-    FactoryStaker private staker;
-    HappyPaymaster private paymaster;
-
-    error EntryPointDeploymentFailed();
-    error EntryPointSimulationsDeploymentFailed();
-    error ConfigNotSet();
-    error InvalidConfig(string config);
-    error DeploymentJsonReadError();
-
-    function _loadExpectedAddresses() internal {
-        string memory config = vm.envOr("CONFIG", string(""));
-        if (bytes(config).length == 0) revert ConfigNotSet();
-
-        // TODO Use a library to predict the deployment address, and skip only if it is the same
-        //      as the expected address and has code.
-
-        string memory deploymentPath;
-        if (keccak256(bytes(config)) == keccak256(bytes("LOCAL"))) {
-            deploymentPath = "deployments/anvil/aa/deployment.json";
-        } else if (keccak256(bytes(config)) == keccak256(bytes("TEST"))) {
-            deploymentPath = "deployments/happy-sepolia/aa/deployment.json";
-        } else {
-            revert InvalidConfig(config);
-        }
-
-        string memory json = vm.readFile(deploymentPath);
-        bytes memory data = vm.parseJson(json);
-        expected = abi.decode(data, (DeploymentAddresses));
-    }
+    EntryPointSimulations public entryPointSimulations;
+    EntryPoint public entryPointV7;
+    ECDSAValidator public ecdsaValidator;
+    Kernel public kernel;
+    KernelFactory public kernelFactory;
+    FactoryStaker public factoryStaker;
+    HappyPaymaster public happyPaymasterImpl;
+    HappyPaymaster public happyPaymaster;
+    SessionKeyValidator public sessionKeyValidatorImpl;
+    SessionKeyValidator public sessionKeyValidator;
 
     function deploy() internal override {
-        _loadExpectedAddresses();
+        address simulationsExpected = 0xBbe8A301FbDb2a4CD58c4A37c262ecef8f889c47;
 
-        if (expected.entryPointSimulations.code.length == 0) {
+        if (simulationsExpected.code.length == 0) {
             // solhint-disable-next-line
             (bool success,) = CREATE2_PROXY.call(ENTRYPOINT_SIMULATIONS_CODE);
             if (!success) {
-                revert EntryPointSimulationsDeploymentFailed();
+                revert("EntryPointSimulations deployment failed");
             }
         }
+        deployed("EntryPointSimulations", simulationsExpected);
 
-        deployed("EntryPointSimulations", expected.entryPointSimulations);
+        //        (address payable _entryPointSimulations,) = deployDeterministicCalldata( //-
+        //            "EntrypointSimulations",
+        //            ENTRYPOINT_SIMULATIONS_CODE //-
+        //        );
+        //        entryPointSimulations = EntryPointSimulations(_entryPointSimulations);
 
-        if (expected.entryPointV7.code.length == 0) {
+        // -----------------------------------------------------------------------------------------
+
+        address entryPointExpected = 0x0000000071727De22E5E9d8BAf0edAc6f37da032;
+
+        if (entryPointExpected.code.length == 0) {
             // solhint-disable-next-line
             (bool success,) = CREATE2_PROXY.call(ENTRYPOINT_V7_CODE);
             if (!success) {
-                revert EntryPointDeploymentFailed();
+                revert("EntryPointV7 deployment failed");
             }
         }
+        address _entryPointV7 = entryPointExpected;
+        deployed("EntryPointV7", "EntryPoint", _entryPointV7);
 
-        deployed("EntryPointV7", "EntryPoint", expected.entryPointV7);
+        //        (address payable _entryPointV7,) = deployDeterministicCalldata( //-
+        //            "EntrypointV7",
+        //            ENTRYPOINT_V7_CODE //-
+        //        );
+        //        entryPointV7 = EntryPoint(_entryPointV7);
 
-        (expected.ecdsaValidator,) =
-            deployDeterministic("ECDSAValidator", type(ECDSAValidator).creationCode, abi.encode(), DEPLOYMENT_SALT);
+        // -----------------------------------------------------------------------------------------
 
-        (expected.kernel,) =
-            deployDeterministic("Kernel", type(Kernel).creationCode, abi.encode(expected.entryPointV7), DEPLOYMENT_SALT);
-
-        bool factoryDeployed;
-        (expected.kernelFactory, factoryDeployed) = deployDeterministic(
-            "KernelFactory", type(KernelFactory).creationCode, abi.encode(expected.kernel), DEPLOYMENT_SALT
+        (address _ecdsaValidator,) = deployDeterministic( //-
+            "ECDSAValidator",
+            type(ECDSAValidator).creationCode,
+            bytes(""),
+            DEPLOYMENT_SALT //-
         );
+        ecdsaValidator = ECDSAValidator(_ecdsaValidator);
 
-        bool stakerDeployed;
-        (expected.factoryStaker, stakerDeployed) = deployDeterministic(
-            "FactoryStaker", type(FactoryStaker).creationCode, abi.encode(msg.sender), DEPLOYMENT_SALT
+        // -----------------------------------------------------------------------------------------
+
+        (address payable _kernel,) = deployDeterministic( //-
+            "Kernel",
+            type(Kernel).creationCode,
+            abi.encode(_entryPointV7),
+            DEPLOYMENT_SALT //-
         );
-        if (stakerDeployed || factoryDeployed) {
-            FactoryStaker(expected.factoryStaker).approveFactory(KernelFactory(expected.kernelFactory), true);
+        kernel = Kernel(_kernel);
+
+        // -----------------------------------------------------------------------------------------
+
+        (address _kernelFactory, bool factoryDeployed) = deployDeterministic( //-
+            "KernelFactory",
+            type(KernelFactory).creationCode,
+            abi.encode(_kernel),
+            DEPLOYMENT_SALT //-
+        );
+        kernelFactory = KernelFactory(_kernelFactory);
+
+        // -----------------------------------------------------------------------------------------
+
+        (address _factoryStaker, bool stakerDeployed) = deployDeterministic( //-
+            "FactoryStaker",
+            type(FactoryStaker).creationCode,
+            abi.encode(msg.sender),
+            DEPLOYMENT_SALT //-
+        );
+        factoryStaker = FactoryStaker(_factoryStaker);
+
+        if (factoryDeployed || stakerDeployed) {
+            factoryStaker.approveFactory(kernelFactory, true);
         }
 
-        (expected.happyPaymasterImpl,) = deployDeterministic(
-            "HappyPaymasterImpl", "HappyPaymaster", type(HappyPaymaster).creationCode, abi.encode(), DEPLOYMENT_SALT
-        );
+        // -----------------------------------------------------------------------------------------
 
-        bool paymasterDeployed;
-        (expected.happyPaymaster, paymasterDeployed) = deployDeterministicProxy(
+        (address _happyPaymasterImpl,) = deployDeterministic( //-
+            "HappyPaymasterImpl",
             "HappyPaymaster",
-            expected.happyPaymasterImpl,
-            abi.encodeCall(HappyPaymaster.initialize, (expected.entryPointV7, msg.sender)),
-            DEPLOYMENT_SALT
+            type(HappyPaymaster).creationCode,
+            bytes(""),
+            DEPLOYMENT_SALT //-
         );
-        paymaster = HappyPaymaster(expected.happyPaymaster);
-        if (paymasterDeployed) {
-            paymaster.deposit{value: PAYMASTER_DEPOSIT}();
+        happyPaymasterImpl = HappyPaymaster(_happyPaymasterImpl);
+
+        // -----------------------------------------------------------------------------------------
+
+        (address _happyPaymaster, bool paymasterDeployed) = deployDeterministicProxy( //-
+            "HappyPaymaster",
+            _happyPaymasterImpl,
+            abi.encodeCall(happyPaymasterImpl.initialize, (_entryPointV7, msg.sender)),
+            DEPLOYMENT_SALT //-
+        );
+        happyPaymaster = HappyPaymaster(_happyPaymaster);
+
+        string memory config = vm.envOr("CONFIG", string(""));
+        if (keccak256(bytes(config)) == keccak256(bytes("LOCAL")) && paymasterDeployed) {
+            // In local mode, fund the paymaster with some gas tokens.
+            happyPaymaster.deposit{value: PAYMASTER_DEPOSIT}();
         }
 
-        (expected.sessionKeyValidatorImpl,) = deployDeterministic(
-            "SessionKeyValidatorImpl", "SessionKeyValidator", type(SessionKeyValidator).creationCode, abi.encode(), DEPLOYMENT_SALT
-        );
+        // -----------------------------------------------------------------------------------------
 
-        (expected.sessionKeyValidator,) = deployDeterministicProxy(
+        (address _sessionKeyValidatorImpl,) = deployDeterministic( //-
+            "SessionKeyValidatorImpl",
             "SessionKeyValidator",
-            expected.sessionKeyValidatorImpl,
-            abi.encodeCall(SessionKeyValidator.initialize, (msg.sender)),
-            DEPLOYMENT_SALT
+            type(SessionKeyValidator).creationCode,
+            bytes(""),
+            DEPLOYMENT_SALT //-
         );
+        sessionKeyValidatorImpl = SessionKeyValidator(_sessionKeyValidatorImpl);
+
+        // -----------------------------------------------------------------------------------------
+
+        (address _sessionKeyValidator,) = deployDeterministicProxy( //-
+            "SessionKeyValidator",
+            _sessionKeyValidatorImpl,
+            abi.encodeCall(sessionKeyValidatorImpl.initialize, (msg.sender)),
+            DEPLOYMENT_SALT //-
+        );
+        sessionKeyValidator = SessionKeyValidator(_sessionKeyValidator);
+
+        // -----------------------------------------------------------------------------------------
     }
 }
