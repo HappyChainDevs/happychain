055a74ca12056cdb1c0f2d9c75f051f47f969e26
not-reed <reed@happy.tech>
2025-01-28 12:11:58 -0800

    publishing makefiles

--- makefiles/publishing.mk
+++ makefiles/publishing.mk
@@ -1,3 +1,5 @@
+##@ Publishing
+
 pack.dry-run: ## Dry Run package to view bundle size
 	bun pm pack --dry-run
 .PHONY: pack.dry-run
@@ -6,3 +8,7 @@ publish.dry-run: ## Dry Run publish to npm
 	bun publish --dry-run
 .PHONY: publish.dry-run
 
+publish:
+	echo "DRY RUN, NOT PUBLISHING!"
+	bun publish --dry-run
+.PHONY: publish
