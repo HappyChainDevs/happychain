a0679d8efbc8c8fce4898bba2632c3c03996f851
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-02-01 20:49:16 +0800

    export missing type in sdk-react, re-export types from core wholesale

--- packages/react/lib/index.ts
+++ packages/react/lib/index.ts
@@ -1,16 +1,5 @@
-export { HappyWalletProvider, useHappyChain } from "./components/HappyWalletProvider"
+export { HappyWalletProvider, useHappyChain, type HappyWalletProviderProps } from "./components/HappyWalletProvider"
 export { ConnectButton } from "./components/Badge"
 
 export * from "@happy.tech/core"
-
-export type {
-    HappyUser,
-    Chain,
-    EIP1193ChainDisconnectedError,
-    EIP1193ChainNotRecognizedError,
-    EIP1193DisconnectedError,
-    EIP1193UnauthorizedError,
-    EIP1193UnsupportedMethodError,
-    EIP1193UserRejectedRequestError,
-    GenericProviderRpcError,
-} from "@happy.tech/core"
+export type * from "@happy.tech/core"
