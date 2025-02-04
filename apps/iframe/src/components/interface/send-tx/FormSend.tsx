219939ece2449a9a6b8f26a1572b13fba97dbcc9
naomiHauret <hauretnaomi@gmail.com>
2025-02-04 16:08:29 +0100

    refacto(iframe): cleanup send asset screen

---
+++ apps/iframe/src/components/interface/send-tx/FormSend.tsx
@@ -0,0 +1,129 @@
+import { useNavigate } from "@tanstack/react-router"
+import { cx } from "class-variance-authority"
+import { Button } from "#src/components/primitives/button/Button"
+import { FormField, FormFieldLabel } from "#src/components/primitives/form-field/FormField"
+import { Input } from "#src/components/primitives/input/Input"
+import { recipeInput } from "#src/components/primitives/input/variant"
+import { FieldFormSendAssets, useFormSendAssets } from "#src/hooks/useHappySend"
+
+export const FormSend = () => {
+    const navigate = useNavigate()
+    const {
+        formAtom: [formValue],
+        handleOnSubmit,
+        handleOnInput,
+        handleOnClickMaxNativeTokenBalance,
+        queryBalanceNativeToken,
+        formErrors,
+        mutationSendTransaction,
+        maximumValueNativeToken,
+    } = useFormSendAssets()
+
+    return (
+        <form onSubmit={handleOnSubmit} className="w-full grid gap-4">
+            <FormField>
+                <FormFieldLabel htmlFor={FieldFormSendAssets.Recipient}>To:</FormFieldLabel>
+                <div className="grid gap-1 [&:has(input:user-invalid)_.indicator]:not-sr-only">
+                    <Input
+                        required
+                        name={FieldFormSendAssets.Recipient}
+                        id={FieldFormSendAssets.Recipient}
+                        placeholder="0x..."
+                        readOnly={mutationSendTransaction.status === "pending"}
+                        onInput={handleOnInput}
+                        aria-describedby="help-recipient-address"
+                        aria-invalid={!!formErrors?.fieldErrors[FieldFormSendAssets.Recipient]}
+                        pattern="^$|^0x[a-fA-F0-9]{40}$"
+                        className=""
+                    />
+
+                    <p className="sr-only indicator w-full text-xs font-medium" id="help-recipient-address">
+                        {formErrors?.fieldErrors[FieldFormSendAssets.Recipient]}
+                    </p>
+                </div>
+            </FormField>
+            <FormField>
+                <div className="flex items-baseline justify-between gap-[1ex]">
+                    <FormFieldLabel htmlFor={FieldFormSendAssets.Amount}>Amount:</FormFieldLabel>
+                    <p
+                        className={cx(
+                            "text-[0.6925em] font-medium inline-flex gap-[1ex] items-baseline",
+                            queryBalanceNativeToken?.status === "pending" ? "animate-pulse" : "",
+                        )}
+                    >
+                        Your balance:{" "}
+                        {queryBalanceNativeToken?.status !== "success" ? "--.--" : maximumValueNativeToken}
+                    </p>
+                </div>
+                <div className="flex flex-col gap-1 [&:has(input:user-invalid)_.indicator]:not-sr-only">
+                    <div
+                        className={recipeInput({
+                            class: "relative flex justify-between gap-1",
+                        })}
+                    >
+                        <input
+                            required
+                            readOnly={
+                                queryBalanceNativeToken.status !== "success" ||
+                                mutationSendTransaction.status === "pending"
+                            }
+                            name={FieldFormSendAssets.Amount}
+                            id={FieldFormSendAssets.Amount}
+                            className="w-full focus:outline-none h-full bg-transparent"
+                            placeholder="0.0"
+                            step="any"
+                            inputMode="decimal"
+                            type="number"
+                            min="0"
+                            value={formValue[FieldFormSendAssets.Amount]}
+                            max={maximumValueNativeToken}
+                            onInput={handleOnInput}
+                            aria-describedby="help-send-amount"
+                            aria-invalid={!!formErrors?.fieldErrors[FieldFormSendAssets.Amount]}
+                        />
+                        <Button
+                            intent="ghost"
+                            aria-disabled={
+                                mutationSendTransaction.status === "pending" ||
+                                queryBalanceNativeToken.status === "pending" ||
+                                maximumValueNativeToken === 0
+                            }
+                            className="text-xs"
+                            type="button"
+                            onClick={handleOnClickMaxNativeTokenBalance}
+                        >
+                            Max
+                        </Button>
+                    </div>
+                    <div className="grid">
+                        <p className="sr-only indicator w-full text-xs font-medium" id="help-send-amount">
+                            {formErrors?.fieldErrors[FieldFormSendAssets.Amount]}
+                        </p>
+                    </div>
+                </div>
+            </FormField>
+            <div className="grid gap-4">
+                <Button
+                    className="justify-center"
+                    intent="primary"
+                    aria-disabled={mutationSendTransaction.status === "pending" || !!formErrors}
+                    isLoading={mutationSendTransaction.status === "pending"}
+                    type="submit"
+                >
+                    Send
+                </Button>
+                <Button
+                    aria-disabled={mutationSendTransaction.status === "pending"}
+                    onClick={() => {
+                        if (mutationSendTransaction.status === "pending") return
+                        navigate({ to: "/embed" })
+                    }}
+                    intent="ghost-negative"
+                    className="justify-center"
+                >
+                    Go back
+                </Button>
+            </div>
+        </form>
+    )
+}
--- apps/iframe/src/components/primitives/input/variant.ts
+++ apps/iframe/src/components/primitives/input/variant.ts
@@ -9,11 +9,11 @@ const recipeInput = cva(
         "w-full",
         "text-start",
         // User activity: focus
-        "focus:outline-none focus:ring-2",
+        "focus:outline-none [&:is(:focus,:focus-within)]:ring-2",
         // Behaviour: disabled
-        "[&:is([aria-disabled=true],:disabled,:readonly)]:opacity-60 [&:is([aria-disabled=true],:disabled,:readonly)]:cursor-not-allowed",
+        "[&:is([aria-disabled=true],:disabled,:readonly),:has([aria-disabled=true],:disabled,:readonly))]:opacity-60 [&:is([aria-disabled=true],:disabled,:readonly),:has([aria-disabled=true],:disabled,:readonly))]:cursor-not-allowed",
         // State: error
-        "dark:[&:is(:user-invalid,[aria-invalid=true])]:bg-error/5 [&:is(:user-invalid,[aria-invalid=true])]:bg-error/15 [&:is(:user-invalid,[aria-invalid=true])]:border-error/60 dark:[&:is(:user-invalid,[aria-invalid=true])]:border-error/30",
+        "dark:[&:is(:has(:user-invalid[aria-invalid]),:user-invalid[aria-invalid])]:bg-error/5 [&:is(:has(:user-invalid[aria-invalid]),:user-invalid[aria-invalid])]:bg-error/15 [&:is(:has(:user-invalid[aria-invalid]),:user-invalid[aria-invalid])]:border-error/60 dark:[&:is(:has(:user-invalid[aria-invalid]),:user-invalid[aria-invalid])]:border-error/30",
     ],
     {
         variants: {
@@ -21,8 +21,6 @@ const recipeInput = cva(
                 default: [
                     // Input
                     "bg-base-100 border-neutral/25 dark:border-neutral/80",
-                    // Part: placeholder
-                    "placeholder:text-neutral/50",
                 ],
             },
             scale: {
