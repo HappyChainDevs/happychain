f66360ea418083134b4673df3293995fae1cf24e
Gabriel Martinez Rodriguez <gabrielmartinezrdrgz@gmail.com>
2025-02-07 17:09:41 +0100

    chore(randomness): changed monitoring logs

--- packages/txm/lib/TxMonitor.ts
+++ packages/txm/lib/TxMonitor.ts
@@ -78,7 +78,7 @@ export class TxMonitor {
         const promises = transactions.map(async (transaction) => {
             const inAirAttempts = transaction.getInAirAttempts()
 
-            console.log("In air attempts", inAirAttempts.map((a) => a.hash))
+            console.log("In air attempts", inAirAttempts.map((a) => a.nonce))
 
             // This could happen if, on the first try, the attempt to submit the transaction fails before flush
             if (inAirAttempts.length === 0) {
