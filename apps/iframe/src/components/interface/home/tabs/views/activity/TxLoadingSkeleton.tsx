66480000c27a633b3a692324ff8ec6af051a44f4
naomiHauret <hauretnaomi@gmail.com>
2025-02-10 20:11:32 +0100

    refacto(iframe): update loading indicator

--- apps/iframe/src/components/interface/home/tabs/views/activity/TxLoadingSkeleton.tsx
+++ apps/iframe/src/components/interface/home/tabs/views/activity/TxLoadingSkeleton.tsx
@@ -15,28 +15,29 @@ interface TxLoadingSkeletonProps {
 const TxLoadingSkeleton = ({ tx }: TxLoadingSkeletonProps) => {
     const currentChain = useAtomValue(currentChainAtom)
     const blockExplorerUrl = currentChain.blockExplorerUrls ? currentChain.blockExplorerUrls : ""
-
     return (
-        <div className="flex flex-col items-start w-full justify-between px-3 py-4 border rounded-md border-slate-700">
-            <div className="flex flex-row w-full items-center justify-between">
-                <a
-                    href={`${blockExplorerUrl}/op/${tx}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-primary hover:text-primary/50 hover:underline"
-                >
-                    {shortenAddress(tx)}
-                </a>
-
+        <article className="focus-within:bg-primary/10 hover:bg-primary/5 p-2 rounded-md grid gap-1.5 relative">
+            <div className="flex items-center text-xs gap-[1ex]">
                 <div className="animate-spin">
-                    <CircleNotch />
+                    <CircleNotch weight="bold" size="0.795em" />
+                </div>
+                <h1 className="font-medium text-base-content/80">Pending transaction</h1>
+                <div className="ms-auto font-semibold text-base-content/70 dark:text-base-content/50">
+                    {shortenAddress(tx)}
                 </div>
             </div>
+            <span className="block h-[1.25ex] rounded w-[9ch] bg-base-content/20" />
 
-            <span className="text-[12px] text-warning hover:text-warning/80 hover:underline px-2 py-1 bg-warning/40 rounded-lg">
-                Pending
-            </span>
-        </div>
+            <a
+                href={`${blockExplorerUrl}/op/${tx}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                title="View on explorer"
+                className="absolute size-full z-10 inset opacity-0"
+            >
+                {shortenAddress(tx)}
+            </a>
+        </article>
     )
 }
 
