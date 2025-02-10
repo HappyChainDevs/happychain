2c9bf416ab40fb51a3bf592211a905bcaf7ccc53
Gabriel Martinez Rodriguez <gabrielmartinezrdrgz@gmail.com>
2025-02-10 21:34:34 +0100

    chore(txm): export AttemptSubmissionErrorCause

--- packages/txm/lib/index.ts
+++ packages/txm/lib/index.ts
@@ -8,3 +8,4 @@ export {
 } from "./GasEstimator.js"
 export type { LatestBlock } from "./BlockMonitor.js"
 export { TxmHookType, type TxmHookHandler } from "./HookManager.js"
+export type { AttemptSubmissionErrorCause } from "./TransactionSubmitter.js"
