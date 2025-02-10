e17b843f67b272d0ad7423efcbe86462b7f6e2f0
Aryan Godara <aryangodara03@gmail.com>
2025-02-10 23:44:17 +0530

    update natspec doc comments to /** [skip ci]

--- contracts/src/HappyAccounts/utils/Common.sol
+++ contracts/src/HappyAccounts/utils/Common.sol
@@ -4,21 +4,21 @@ pragma solidity ^0.8.20;
 /// @notice Each of these values is shared between at least two or more of IHappyPaymaster,
 ///         IHappyAccount, and HappyEntryPoint contracts.
 
-/*
- * Selector returned by {@link IHappyAccount.validate} in simulation mode if the
+/**
+ * Selector returned by {IHappyAccount.validate} in simulation mode if the
  * nonce can be valid in the future but is not the current nonce (and so the happyTx
  * would fail if submitted before the ones matching missing nonces).
- * 
+ *
  * This error is used during simulation to indicate that a transaction will be valid
  * once its nonce matches, allowing for gas estimation while preventing premature
- * submission. Outside of simulation, the function should return {@link InvalidNonce}
+ * submission. Outside of simulation, the function should return InvalidNonce}
  * instead.
  */
 error FutureNonceDuringSimulation();
 
-/*
- * Selector returned by {@link IHappyAccount.validate} and
- * {@link IHappyPaymaster.payout} in simulation mode if the validity of the happyTx
+/**
+ * Selector returned by {IHappyAccount.validate} and
+ * {IHappyPaymaster.payout} in simulation mode if the validity of the happyTx
  * cannot be ascertained during simulation.
  *
  * e.g. we can't verify a signature over the gas limit during simulation,
@@ -26,13 +26,13 @@ error FutureNonceDuringSimulation();
  */
 error UnknownDuringSimulation();
 
-/*
+/**
  * Functions that are supposed to be called from the EntryPoint contract but are not
  * should *revert* with this error.
  */
 error NotFromEntryPoint();
 
-/*
- * Selector returned by {@link IHappyAccount.validte} when the happyTx's signature is invalid.
+/**
+ * Selector returned by {IHappyAccount.validte} when the happyTx's signature is invalid.
  */
 error InvalidOwnerSignature();
