6acf8a60471ecbff3af75bc0f548a57be00f5459
naomiHauret <hauretnaomi@gmail.com>
2025-02-11 16:16:01 +0100

    feat(iframe): differentiate operations in activity tab

--- apps/iframe/src/components/interface/home/tabs/views/activity/TxLoadingSkeleton.tsx
+++ apps/iframe/src/components/interface/home/tabs/views/activity/TxLoadingSkeleton.tsx
@@ -15,19 +15,18 @@ interface TxLoadingSkeletonProps {
 const TxLoadingSkeleton = ({ tx }: TxLoadingSkeletonProps) => {
     const currentChain = useAtomValue(currentChainAtom)
     const blockExplorerUrl = currentChain.blockExplorerUrls ? currentChain.blockExplorerUrls : ""
+
     return (
         <article className="focus-within:bg-primary/10 hover:bg-primary/5 p-2 rounded-md grid gap-1.5 relative">
-            <div className="flex items-center text-xs gap-[1ex]">
-                <div className="animate-spin">
-                    <CircleNotch weight="bold" size="0.795em" />
+            <div className="flex items-baseline text-xs gap-3">
+                <div className="animate-spin size-5 flex items-center justify-center">
+                    <CircleNotch weight="bold" size="0.95em" />
                 </div>
                 <h1 className="font-medium text-base-content/80">Pending transaction</h1>
                 <div className="ms-auto font-semibold text-base-content/70 dark:text-base-content/50">
                     {shortenAddress(tx)}
                 </div>
             </div>
-            <span className="block h-[1.25ex] rounded w-[9ch] bg-base-content/20" />
-
             <a
                 href={`${blockExplorerUrl}/op/${tx}`}
                 target="_blank"
