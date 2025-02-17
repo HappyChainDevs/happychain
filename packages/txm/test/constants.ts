7c15e133c7619e276de9ef81df46328791c74c21
Gabriel Martinez Rodriguez <gabrielmartinezrdrgz@gmail.com>
2025-02-17 13:34:43 +0100

    test: txm

---
+++ packages/txm/test/constants.ts
@@ -0,0 +1,3 @@
+export const CHAIN_ID = 31337;
+export const PRIVATE_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+export const RPC_URL = "http://localhost:8545";
---
+++ packages/txm/vitest.setup.ts
@@ -0,0 +1,1 @@
+process.env.TXM_DB_PATH = "./test/txm.sqlite";
