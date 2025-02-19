bc2b1baa53a73f96cb001129081e45a98294b403
ultraviolet10 <aritrac1998@hotmail.com>
2025-02-19 12:23:08 +0530

    missed fmt

--- demos/react/src/demo-components/WalletFunctionalityDemo.tsx
+++ demos/react/src/demo-components/WalletFunctionalityDemo.tsx
@@ -57,7 +57,9 @@ const WalletFunctionalityDemo = () => {
 
     async function loadAbiStub() {
         await loadAbi(deployment.HappyCounter, abis.HappyCounter)
-        toast.success(`ABI loaded for ${deployment.HappyCounter}! Click on the Mint Token button to see the ABI in use within the request popup.`)
+        toast.success(
+            `ABI loaded for ${deployment.HappyCounter}! Click on the Mint Token button to see the ABI in use within the request popup.`,
+        )
     }
 
     return (
