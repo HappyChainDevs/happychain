5689ff09385eb939cff956d3dfdf720d41c37f22
not-reed <reed@happy.tech>
2025-02-21 10:57:57 -0800

    add 'deploy account' route

---
+++ apps/submitter/src/deployments.ts
@@ -0,0 +1,15 @@
+import { abis as happyAAAbis, deployment as happyAADeployment } from "@happy.tech/contracts/happy-aa/anvil"
+import { abis as mockAbis, deployment as mockDeployment } from "@happy.tech/contracts/mocks/anvil"
+
+// Import and re-export contracts here for a simple way to switch between deployments throughout the app
+export const deployment = {
+    ...happyAADeployment,
+    ...mockDeployment,
+}
+
+export const abis = {
+    ...happyAAAbis,
+    ...mockAbis,
+}
+
+export default { abis, deployment }
--- apps/submitter/src/env.ts
+++ apps/submitter/src/env.ts
@@ -1,10 +1,16 @@
 import { z } from "zod"
+import { isHexString } from "./zod/isHexString"
 
 // Define the schema as an object with all of the env
 // variables and their types
 const envSchema = z.object({
+    PRIVATE_KEY_LOCAL: z.string().refine(isHexString),
+    PRIVATE_KEY_ACCOUNT_DEPLOYER: z
+        .string()
+        .refine(isHexString)
+        .default(process.env.PRIVATE_KEY_LOCAL as `0x${string}`), // risky, but this is validated above
     APP_PORT: z.coerce.number().default(3002),
-    NODE_ENV: z.enum(["production", "development", "test"]),
+    NODE_ENV: z.enum(["production", "development", "test", "cli"]),
     LOG_LEVEL: z.enum(["debug", "info", "warn", "error"]).default("info"),
 })
 
