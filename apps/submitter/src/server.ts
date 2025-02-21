a2009a699bdc405cbdf8735b12f477d2eab2b092
not-reed <reed@happy.tech>
2025-02-21 11:00:02 -0800

    submitter basic middleware

--- apps/submitter/src/server.ts
+++ apps/submitter/src/server.ts
@@ -1,8 +1,32 @@
 import { Hono } from "hono"
+import { every, except } from "hono/combine"
+import { logger as loggerMiddleware } from "hono/logger"
+import { prettyJSON as prettyJSONMiddleware } from "hono/pretty-json"
+import { requestId as requestIdMiddleware } from "hono/request-id"
+import { timeout as timeoutMiddleware } from "hono/timeout"
+import { timing as timingMiddleware } from "hono/timing"
+import env from "./env"
 import { logger } from "./logger"
 import accountsApi from "./routes/api/accounts"
 
-export const app = new Hono().route("/api/v1/accounts", accountsApi)
+export const app = new Hono()
+    // middleware
+    .use(
+        except(
+            // don't run these during testing
+            () => env.NODE_ENV === "test",
+            every(
+                timingMiddleware(), // measure response times
+                loggerMiddleware(), // log all calls to the console
+                prettyJSONMiddleware(), // add '?pretty' to any json endpoint to prettyprint
+            ),
+        ),
+    )
+    .use(timeoutMiddleware(10_000))
+    .use(requestIdMiddleware())
+
+    // Routes
+    .route("/api/v1/accounts", accountsApi)
 
 app.notFound((c) => c.text("These aren't the droids you're looking for", 404))
 app.onError((err, c) => {
