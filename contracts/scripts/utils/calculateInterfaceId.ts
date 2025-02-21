6dc040d225fe49fc23ee6848df6708888703e1da
Aryan Godara <aryangodara03@gmail.com>
2025-02-21 13:59:09 +0530

    update interface ID [skip ci]

--- contracts/scripts/utils/calculateInterfaceId.ts
+++ contracts/scripts/utils/calculateInterfaceId.ts
@@ -6,9 +6,12 @@ function calculateInterfaceId(functionSignatures: string[]): string {
     let interfaceId = 0n
 
     for (const signature of functionSignatures) {
+        console.log(signature)
         // Take first 4 bytes of keccak256
         const selector = BigInt(toFunctionSelector(signature))
+        console.log(selector.toString(16), "\n")
         interfaceId ^= selector
+        console.log("interfaceId:", interfaceId.toString(16), "\n")
     }
 
     return "0x" + interfaceId.toString(16).padStart(8, "0")
@@ -16,7 +19,7 @@ function calculateInterfaceId(functionSignatures: string[]): string {
 
 // HappyTx struct tuple type for function signatures
 const happyTxType =
-    "tuple(address account,uint32 gasLimit,uint32 executeGasLimit,address dest,address paymaster,uint256 value,uint192 nonceTrack,uint64 nonceValue,uint256 maxFeePerGas,int256 submitterFee,bytes callData,bytes paymasterData,bytes validatorData)"
+    "(address account,uint32 gasLimit,uint32 executeGasLimit,address dest,address paymaster,uint256 value,uint192 nonceTrack,uint64 nonceValue,uint256 maxFeePerGas,int256 submitterFee,bytes callData,bytes paymasterData,bytes validatorData)"
 
 // IHappyAccount interface functions
 const iHappyAccountFunctions = [
@@ -32,5 +35,5 @@ const iHappyPaymasterFunctions = [`payout(${happyTxType})`]
 console.log("IHappyAccount interface ID:", calculateInterfaceId(iHappyAccountFunctions))
 console.log("IHappyPaymaster interface ID:", calculateInterfaceId(iHappyPaymasterFunctions))
 
-// IHappyAccount interface ID: 0x90ab9eae
-// IHappyPaymaster interface ID: 0xa79b0c0c
+// IHappyAccount interface ID: 0x909c11f4
+// IHappyPaymaster interface ID: 0x9c7b367f
