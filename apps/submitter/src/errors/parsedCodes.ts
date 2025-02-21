ccab032f0b71e4e0f90eba1fef31d1806cf5371a
not-reed <reed@happy.tech>
2025-02-21 13:49:36 -0800

    add error utils and classes

---
+++ apps/submitter/src/errors/parsedCodes.ts
@@ -0,0 +1,38 @@
+import { getAbiItem, parseAbi, toFunctionSelector } from "viem/utils"
+
+/**
+ * Used to calculate the selectors for the following _errors_
+ *
+ * i.e.
+ * this definition is from the contract
+ * `error InvalidNonce()` => `0x756688fe`
+ *
+ * and here would be how to convert between the two
+ * (notice to parse the ABI error, replace 'error' with 'function'):
+ * ```ts
+ * getAbiItem({ abi: abi, name: '0x756688fe' })?.name //=> 'InvalidNonce'
+ * toFunctionSelector("function InvalidNonce()") //=> '0x756688fe'
+ * ```
+ */
+const abi = parseAbi([
+    // IHappyAccount.sol ErrorSelectors
+    "function InvalidNonce()",
+    "function GasPriceTooHigh()",
+    "function WrongAccount()",
+    "function FutureNonceDuringSimulation()",
+    // Common.sol ErrorSelectors
+    "function FutureNonceDuringSimulation()",
+    "function UnknownDuringSimulation()",
+    "function NotFromEntryPoint()",
+    "function InvalidOwnerSignature()",
+    // IHappyPaymaster.sol ErrorSelectors
+    "function WrongTarget()",
+    "function InvalidOwnerSignature()",
+])
+
+export const getErrorNameFromSelector = (selector: `0x${string}`) => getAbiItem({ abi: abi, name: selector })?.name
+
+export const getSelectorFromErrorName = (name: string) => {
+    const item = abi.find((a) => a.name === name)
+    return item ? toFunctionSelector(item) : undefined
+}
