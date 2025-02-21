8f59c8f969777515293c82d6d5c34812343b8818
Aryan Godara <aryangodara03@gmail.com>
2025-02-21 16:05:14 +0530

    update natspec comments

--- contracts/src/happy-accounts/factories/ScrappyAccountFactory.sol
+++ contracts/src/happy-accounts/factories/ScrappyAccountFactory.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {ScrappyAccount} from "../samples/ScrappyAccount.sol";
 
-/// @notice Sample factory contract for deploying deterministic ERC1967 proxies for {ScrappyAccount}.
+/// Sample factory contract for deploying deterministic ERC1967 proxies for {ScrappyAccount}.
 contract ScrappyAccountFactory {
     /// Error thrown when account initialization fails
     error InitializeError();
