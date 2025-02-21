3f007e9040b256e7d1f1a6b486c4f218799666d8
not-reed <reed@happy.tech>
2025-02-21 10:32:05 -0800

    add basic hono & env validation

---
+++ apps/submitter/src/env.ts
@@ -0,0 +1,17 @@
+import { z } from "zod"
+
+// Define the schema as an object with all of the env
+// variables and their types
+const envSchema = z.object({
+    APP_PORT: z.coerce.number().default(3001),
+    NODE_ENV: z.enum(["production", "development", "test"]),
+})
+
+// Validate `process.env` against our schema
+// and return the result
+const env = envSchema.parse(process.env)
+
+// Export the result so we can use it in the project
+export default env
+
+export type Environment = z.infer<typeof envSchema>
