f4af1e1306bc2d94f19f9aaa67a8ac3eceaa5b58
not-reed <reed@happy.tech>
2025-02-21 14:03:19 -0800

    add nonce queue manager

---
+++ apps/submitter/src/utils/getHappyTxHash.ts
@@ -0,0 +1,23 @@
+import { keccak256 } from "viem/utils"
+import type { HappyTx } from "#src/tmp/interface/HappyTx"
+import { encodeHappyTx } from "./encodeHappyTx"
+
+// with paymaster, don't include gas values in the signature!
+const paymasterGasData = {
+    executeGasLimit: 0n,
+    gasLimit: 0n,
+    maxFeePerGas: 0n,
+    submitterFee: 0n,
+} as const
+
+export function computeHappyTxHash(happyTx: HappyTx): `0x${string}` {
+    // Don't include validator data in the signature so that pre & post signing are the same
+
+    const isSelfSigning = happyTx.paymaster === happyTx.account
+
+    const hashData: HappyTx = isSelfSigning
+        ? { ...happyTx, validatorData: "0x" }
+        : { ...happyTx, validatorData: "0x", ...paymasterGasData }
+
+    return keccak256(encodeHappyTx(hashData))
+}
