59379303ca05669259a29b482b719f1f81521ae2
Gabriel Martinez Rodriguez <gabrielmartinezrdrgz@gmail.com>
2025-02-24 17:02:47 +0100

    chore(txm): pr review

---
+++ packages/txm/test/utils/customAsserts.ts
@@ -0,0 +1,17 @@
+import type { Address, TransactionReceipt } from "viem"
+import { expect } from "vitest"
+
+function assertReceipt(to: Address, from: Address, status: "success" | "reverted", receipt?: TransactionReceipt) {
+    expect(receipt).toBeDefined()
+    expect(receipt?.status).toBe(status)
+    expect(receipt?.to?.toLocaleLowerCase()).toBe(to.toLowerCase())
+    expect(receipt?.from.toLowerCase()).toBe(from.toLowerCase())
+}
+
+export function assertReceiptSuccess(to: Address, from: Address, receipt?: TransactionReceipt): void {
+    assertReceipt(to, from, "success", receipt)
+}
+
+export function assertReceiptReverted(to: Address, from: Address, receipt?: TransactionReceipt): void {
+    assertReceipt(to, from, "reverted", receipt)
+}
