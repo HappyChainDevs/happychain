441a45b3165da3f8494bedeabdebfd84ec69d78c
aodhgan <gawnieg@gmail.com>
2025-02-26 10:51:56 -0800

    init coverage reporter for txm

--- packages/txm/vitest.config.ts
+++ packages/txm/vitest.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "vitest/config"
 
 export default defineConfig({
     test: {
+        coverage: {
+            provider: 'v8', // or 'v8' for native V8 coverage
+            reporter: ['html', 'lcov'], // 'text', 'json', 'html', etc.
+            include: ['lib/'], // Target files for coverage
+            exclude: ['node_modules', 'test', 'dist'], // Exclude files
+          },
         globals: true,
         environment: "node",
         setupFiles: "./vitest.setup.ts",
