2f881b2d6a8e1a404cebee89f3a9288318365743
Gabriel Martinez Rodriguez <gabrielmartinezrdrgz@gmail.com>
2025-02-26 15:07:46 +0100

    pr: review

--- packages/txm/test/utils/db.ts
+++ packages/txm/test/utils/db.ts
@@ -1,4 +1,6 @@
+import type { UUID } from "@happy.tech/common"
 import { db } from "../../lib/db/driver"
+import type { TransactionTable } from "../../lib/db/types"
 
 export async function cleanDB() {
     const tables = await db
@@ -16,4 +18,8 @@ export async function cleanDB() {
     for (const table of tables) {
         await db.schema.dropTable(table.name).execute()
     }
+}
+
+export async function getPersistedTransaction(intentId: UUID): Promise<TransactionTable | undefined> {
+    return await db.selectFrom("transaction").where("intentId", "=", intentId).selectAll().executeTakeFirst()
 }
