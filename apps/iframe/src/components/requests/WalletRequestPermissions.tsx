1d6d470b8a172e93e04fc5ed9ddd41014ebcbba1
naomiHauret <hauretnaomi@gmail.com>
2025-02-27 12:12:19 +0100

    chore(iframe): cleanup removed tabs from popup requests layout common components

--- apps/iframe/src/components/requests/WalletRequestPermissions.tsx
+++ apps/iframe/src/components/requests/WalletRequestPermissions.tsx
@@ -3,14 +3,11 @@ import { getAppURL } from "#src/utils/appURL"
 import {
     FormattedDetailsLine,
     Layout,
-    RequestTabsValues,
     SectionBlock,
     SectionTitle,
     SubsectionBlock,
     SubsectionContent,
     SubsectionTitle,
-    TabContent,
-    Tabs,
 } from "./common/Layout"
 import type { RequestConfirmationProps } from "./props"
 
@@ -47,35 +44,33 @@ export const WalletRequestPermissions = ({
                 },
             }}
         >
-            <Tabs defaultValue={RequestTabsValues.Details}>
-                <TabContent value={RequestTabsValues.Details}>
-                    <SectionBlock>
-                        <SectionTitle>Permissions</SectionTitle>
-                        <SubsectionBlock>
-                            <SubsectionContent>
-                                {params.map((param) => {
-                                    const [[name]] = Object.entries(param)
-                                    return (
-                                        <>
-                                            <SubsectionTitle key={`scope-${name}-title`}>{name}</SubsectionTitle>
-                                            <FormattedDetailsLine key={`scope-${name}-description`}>
-                                                {permissionDescriptions[name as PermissionDescriptionIndex]}
-                                            </FormattedDetailsLine>
-                                        </>
-                                    )
-                                })}
-                            </SubsectionContent>
-                        </SubsectionBlock>
-                    </SectionBlock>
-                </TabContent>
-                <TabContent className="break-words" value={RequestTabsValues.Raw}>
-                    <SectionBlock>
-                        <SubsectionBlock>
-                            <FormattedDetailsLine isCode>{JSON.stringify(params, null, 2)}</FormattedDetailsLine>
-                        </SubsectionBlock>
-                    </SectionBlock>
-                </TabContent>
-            </Tabs>
+            <SectionBlock>
+                <SectionTitle>Permissions</SectionTitle>
+                <SubsectionBlock>
+                    <SubsectionContent>
+                        {params.map((param) => {
+                            const [[name]] = Object.entries(param)
+                            return (
+                                <>
+                                    <SubsectionTitle key={`scope-${name}-title`}>{name}</SubsectionTitle>
+                                    <FormattedDetailsLine key={`scope-${name}-description`}>
+                                        {permissionDescriptions[name as PermissionDescriptionIndex]}
+                                    </FormattedDetailsLine>
+                                </>
+                            )
+                        })}
+                    </SubsectionContent>
+                </SubsectionBlock>
+            </SectionBlock>
+            <SectionBlock>
+                <SubsectionBlock>
+                    <SubsectionContent>
+                        <SubsectionTitle>Data</SubsectionTitle>
+
+                        <FormattedDetailsLine isCode>{JSON.stringify(params, null, 2)}</FormattedDetailsLine>
+                    </SubsectionContent>
+                </SubsectionBlock>
+            </SectionBlock>
             <SectionBlock>
                 <p className="font-bold pb-8 text-center text-sm">
                     You can revoke granted permissions from the wallet.
