40fe92c0ff441d0416a152c9720ae288ffb4e73c
naomiHauret <hauretnaomi@gmail.com>
2025-02-27 12:11:48 +0100

    refacto(iframe): remove tabs from request popups

--- apps/iframe/src/components/requests/WalletAddEthereumChain.tsx
+++ apps/iframe/src/components/requests/WalletAddEthereumChain.tsx
@@ -4,11 +4,10 @@ import { Input } from "../primitives/input/Input"
 import {
     FormattedDetailsLine,
     Layout,
-    RequestTabsValues,
     SectionBlock,
     SubsectionBlock,
-    TabContent,
-    Tabs,
+    SubsectionContent,
+    SubsectionTitle,
 } from "./common/Layout"
 import type { RequestConfirmationProps } from "./props"
 
@@ -34,88 +33,86 @@ export const WalletAddEthereumChain = ({
                 },
             }}
         >
-            <Tabs defaultValue={RequestTabsValues.Details}>
-                <TabContent value={RequestTabsValues.Details}>
-                    <SectionBlock>
-                        <form>
-                            <fieldset className="grid gap-4">
-                                <legend className="pb-2 text-xs font-semibold">Customize chain information</legend>
-                                <FormField>
-                                    <FormFieldLabel htmlFor="custom-chain-name">Name</FormFieldLabel>
-                                    <Input
-                                        required
-                                        onChange={(e) => {
-                                            setChain((old) => ({ ...old, chainName: e.target.value }))
-                                        }}
-                                        type="text"
-                                        id="custom-chain-name"
-                                        name="custom-chain-name"
-                                        value={chain.chainName}
-                                    />
-                                </FormField>
-                                <FormField>
-                                    <FormFieldLabel htmlFor="custom-chain-rpc-url">RPC URL</FormFieldLabel>
-                                    <Input
-                                        onChange={(e) => {
-                                            setChain((old) => ({ ...old, rpcUrls: [e.target.value] }))
-                                        }}
-                                        required
-                                        type="url"
-                                        value={chain.rpcUrls[0] ?? ""}
-                                        id="custom-chain-rpc-url"
-                                        name="custom-chain-rpc-url"
-                                    />
-                                </FormField>
-                                <FormField>
-                                    <FormFieldLabel htmlFor="custom-chain-currency">Currency symbol</FormFieldLabel>
-                                    <Input
-                                        type="text"
-                                        onChange={(e) => {
-                                            setChain((old) => ({
-                                                ...old,
-                                                nativeCurrency: {
-                                                    name: e.target.value ?? "",
-                                                    symbol: e.target.value,
-                                                    decimals: 18,
-                                                },
-                                            }))
-                                        }}
-                                        required
-                                        value={chain.nativeCurrency?.symbol ?? ""}
-                                        id="custom-chain-currency"
-                                        name="custom-chain-currency"
-                                    />
-                                </FormField>
+            <SectionBlock>
+                <form>
+                    <fieldset className="grid gap-4">
+                        <legend className="pb-2 text-xs font-semibold">Customize chain information</legend>
+                        <FormField>
+                            <FormFieldLabel htmlFor="custom-chain-name">Name</FormFieldLabel>
+                            <Input
+                                required
+                                onChange={(e) => {
+                                    setChain((old) => ({ ...old, chainName: e.target.value }))
+                                }}
+                                type="text"
+                                id="custom-chain-name"
+                                name="custom-chain-name"
+                                value={chain.chainName}
+                            />
+                        </FormField>
+                        <FormField>
+                            <FormFieldLabel htmlFor="custom-chain-rpc-url">RPC URL</FormFieldLabel>
+                            <Input
+                                onChange={(e) => {
+                                    setChain((old) => ({ ...old, rpcUrls: [e.target.value] }))
+                                }}
+                                required
+                                type="url"
+                                value={chain.rpcUrls[0] ?? ""}
+                                id="custom-chain-rpc-url"
+                                name="custom-chain-rpc-url"
+                            />
+                        </FormField>
+                        <FormField>
+                            <FormFieldLabel htmlFor="custom-chain-currency">Currency symbol</FormFieldLabel>
+                            <Input
+                                type="text"
+                                onChange={(e) => {
+                                    setChain((old) => ({
+                                        ...old,
+                                        nativeCurrency: {
+                                            name: e.target.value ?? "",
+                                            symbol: e.target.value,
+                                            decimals: 18,
+                                        },
+                                    }))
+                                }}
+                                required
+                                value={chain.nativeCurrency?.symbol ?? ""}
+                                id="custom-chain-currency"
+                                name="custom-chain-currency"
+                            />
+                        </FormField>
 
-                                <FormField>
-                                    <FormFieldLabel isOptional htmlFor="custom-chain-block-explorer">
-                                        Block explorer
-                                    </FormFieldLabel>
-                                    <Input
-                                        type="url"
-                                        onChange={(e) => {
-                                            setChain((old) => ({
-                                                ...old,
-                                                blockExplorerUrls: e.target.value ? [e.target.value] : undefined,
-                                            }))
-                                        }}
-                                        value={chain.blockExplorerUrls?.[0] ?? ""}
-                                        id="custom-chain-block-explorer"
-                                        name="custom-chain-block-explorer"
-                                    />
-                                </FormField>
-                            </fieldset>
-                        </form>
-                    </SectionBlock>
-                </TabContent>
-                <TabContent value={RequestTabsValues.Raw}>
-                    <SectionBlock>
-                        <SubsectionBlock>
-                            <FormattedDetailsLine isCode>{JSON.stringify(params, null, 2)}</FormattedDetailsLine>
-                        </SubsectionBlock>
-                    </SectionBlock>
-                </TabContent>
-            </Tabs>
+                        <FormField>
+                            <FormFieldLabel isOptional htmlFor="custom-chain-block-explorer">
+                                Block explorer
+                            </FormFieldLabel>
+                            <Input
+                                type="url"
+                                onChange={(e) => {
+                                    setChain((old) => ({
+                                        ...old,
+                                        blockExplorerUrls: e.target.value ? [e.target.value] : undefined,
+                                    }))
+                                }}
+                                value={chain.blockExplorerUrls?.[0] ?? ""}
+                                id="custom-chain-block-explorer"
+                                name="custom-chain-block-explorer"
+                            />
+                        </FormField>
+                    </fieldset>
+                </form>
+            </SectionBlock>
+            <SectionBlock>
+                <SubsectionBlock>
+                    <SubsectionContent>
+                        <SubsectionTitle>Data</SubsectionTitle>
+
+                        <FormattedDetailsLine isCode>{JSON.stringify(params, null, 2)}</FormattedDetailsLine>
+                    </SubsectionContent>
+                </SubsectionBlock>
+            </SectionBlock>
         </Layout>
     )
 }
