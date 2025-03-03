f95127925db308c385ef555bb18ebce160153eb5
Aryan Godara <aryangodara03@gmail.com>
2025-03-03 13:52:58 +0530

    setup for all the unit tests [skip ci]

---
+++ contracts/src/test/happy-accounts/Utils.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
+import {HappyTx} from "../../happy-accounts/core/HappyTx.sol";
+import {HappyTxLib} from "../../happy-accounts/libs/HappyTxLib.sol";
+import {ScrappyAccount} from "../../happy-accounts/samples/ScrappyAccount.sol";
+import {MockERC20Token} from "../../mocks/MockERC20.sol";
+
+/// Common utility functions for HappyAccounts unit tests
+contract HappyTxTestUtils is Test {
+    using HappyTxLib for HappyTx;
+    using MessageHashUtils for bytes32;
+
+    uint256 private constant TOKEN_MINT_AMOUNT = 10;
+    uint192 private constant DEFAULT_NONCETRACK = 0;
+
+    function createSignedHappyTx(
+        address account,
+        address paymaster,
+        address dest,
+        uint256 privKey,
+        address smartAccount
+    ) public view returns (HappyTx memory) {
+        HappyTx memory happyTx = getStubHappyTx(dest, TOKEN_MINT_AMOUNT);
+
+        happyTx.account = account;
+        happyTx.paymaster = paymaster;
+        happyTx.nonceTrack = 0;
+        happyTx.nonceValue = getNonce(smartAccount, DEFAULT_NONCETRACK);
+
+        // Store original values
+        uint32 origGasLimit;
+        uint32 origExecuteGasLimit;
+        uint256 origMaxFeePerGas;
+        int256 origSubmitterFee;
+
+        if (paymaster != account) {
+            origGasLimit = happyTx.gasLimit;
+            origExecuteGasLimit = happyTx.executeGasLimit;
+            origMaxFeePerGas = happyTx.maxFeePerGas;
+            origSubmitterFee = happyTx.submitterFee;
+
+            // Temporarily zero them for signing
+            happyTx.gasLimit = 0;
+            happyTx.executeGasLimit = 0;
+            happyTx.maxFeePerGas = 0;
+            happyTx.submitterFee = 0;
+        }
+
+        happyTx.validatorData = signHappyTx(happyTx, privKey);
+
+        if (paymaster != account) {
+            // Restore original values after signing
+            happyTx.gasLimit = origGasLimit;
+            happyTx.executeGasLimit = origExecuteGasLimit;
+            happyTx.maxFeePerGas = origMaxFeePerGas;
+            happyTx.submitterFee = origSubmitterFee;
+        }
+
+        return happyTx;
+    }
+
+    function getStubHappyTx(address dest, uint256 mintAmount) public pure returns (HappyTx memory) {
+        return HappyTx({
+            account: address(0), // Stub value
+            gasLimit: 4000000000, // 0xEE6B2800
+            executeGasLimit: 4000000000, // 0xEE6B2800
+            dest: dest,
+            paymaster: address(0), // Stub value
+            value: 0,
+            nonceTrack: 0,
+            nonceValue: 0,
+            maxFeePerGas: 1200000000, // 0x47868C00
+            submitterFee: 100, // 0x64
+            callData: getMintCallData(dest, mintAmount),
+            paymasterData: hex"",
+            validatorData: hex"",
+            extraData: hex""
+        });
+    }
+
+    function signHappyTx(HappyTx memory happyTx, uint256 privKey) public pure returns (bytes memory signature) {
+        bytes32 hash = keccak256(happyTx.encode()).toEthSignedMessageHash();
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privKey, hash);
+        signature = abi.encodePacked(r, s, v);
+    }
+
+    function getMintCallData(address target, uint256 amount) public pure returns (bytes memory) {
+        return abi.encodeCall(MockERC20Token.mint, (target, amount));
+    }
+
+    function getNonce(address smartAccount, uint192 nonceTrack) public view returns (uint64) {
+        return uint64(ScrappyAccount(payable(smartAccount)).getNonce(nonceTrack));
+    }
+}
