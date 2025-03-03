76f3f72d912043bd132af6b248eeab4cc175c518
Aryan Godara <aryangodara03@gmail.com>
2025-03-03 13:05:10 +0530

    temp commit

---
+++ contracts/src/test/happy-accounts/core/HappyEntryPoint.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {HappyEntryPoint} from "../../../happy-accounts/core/HappyEntryPoint.sol";
+import {ScrappyAccount} from "../../../happy-accounts/samples/ScrappyAccount.sol";
+import {ScrappyPaymaster} from "../../../happy-accounts/samples/ScrappyPaymaster.sol";
+import {ScrappyAccountFactory} from "../../../happy-accounts/factories/ScrappyAccountFactory.sol";
+
+contract HappyEntryPointTest is Test {
+
+    function setUp() public {
+    }
+}
