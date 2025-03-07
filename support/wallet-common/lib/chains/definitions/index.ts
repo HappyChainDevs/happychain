7f222916c08b8dd3242d2bf9d0298ac752c27ef3
ultraviolet10 <aritrac1998@hotmail.com>
2025-03-07 19:42:21 +0530

    fix: cleanup unused chain definitions

--- support/wallet-common/lib/chains/definitions/index.ts
+++ support/wallet-common/lib/chains/definitions/index.ts
@@ -1,10 +1,3 @@
-// Unused, for testing use only
-export { baseSepolia } from "./baseSepolia"
-export { base } from "./base"
-export { op } from "./op"
-export { opSepolia } from "./opSepolia"
-export { ethereumSepolia } from "./ethereumSepolia"
-
 // Internal use only
 export { ethereum } from "./ethereum"
 export { devnet } from "./devnet"
