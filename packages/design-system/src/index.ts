2ec76d38e6752d3e67c8ad26a68181e0cc1cf24b
naomiHauret <hauretnaomi@gmail.com>
2025-03-12 15:25:14 +0100

    refacto(design system): remove barrel files

--- packages/design-system/src/index.ts
+++ packages/design-system/src/index.ts
@@ -1,12 +1,32 @@
-export * from "./panel"
-export * from "./button"
-export * from "./collapsible"
-export * from "./expandable"
-export * from "./form-field"
-export * from "./menu"
-export * from "./native-select"
-export * from "./table"
-export * from "./user-input"
-//@ todo
-// export * from './surface'
-// export * from './material'
+// Button
+export * from "./button/recipeGuiButton"
+export * from "./button/recipeSkeuoButton"
+
+// Input
+export * from "./user-input/recipeGuiUserInput"
+
+// Expandable
+export * from "./expandable/recipeGuiExpandable"
+
+// Collapsible
+export * from "./collapsible/recipeGuiCollapsible"
+export * from "./form-field/recipeGuiFormField"
+
+// Panel
+export * from "./panel/recipeGuiPanel"
+
+// Menu
+export * from "./menu/recipeGuiMenu"
+
+// Select (native input)
+export * from "./native-select/recipeGuiNativeSelect"
+
+// Table (dataview)
+export * from "./table/recipeGuiTable"
+
+// Slider (range input)
+export * from "./slider/recipeSkeuoSlider"
+
+// @todo
+// export * from './material/'
+// export * from './surface/recipeSkeuoSurface'
