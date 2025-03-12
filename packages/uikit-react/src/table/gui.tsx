e0c3a7eae0eac27a56d9bb41c77ad5964b0ecb31
naomiHauret <hauretnaomi@gmail.com>
2025-03-12 11:22:22 +0100

    refacto(design system): improve table primitive ; include advanced examples

--- packages/uikit-react/src/table/gui.tsx
+++ packages/uikit-react/src/table/gui.tsx
@@ -1,114 +1,120 @@
-import { forwardRef, type HTMLAttributes, type TdHTMLAttributes, type ThHTMLAttributes } from "react";
-import { flexRender } from "@tanstack/react-table";
-import { cx } from "cva";
-import { recipeGuiTable, type GuiTableBodyVariantsProps, type GuiTableHeaderCellVariantsProps, type GuiTableHeaderVariantsProps, type GuiTableRootVariantsProps, type GuiTableVariantsProps } from "@happy.tech/design-system";
-import { useDataviewTable } from "./use-dataview-table";
+import {
+    type GuiTableBodyCellVariantsProps,
+    type GuiTableBodyVariantsProps,
+    type GuiTableHeaderCellVariantsProps,
+    type GuiTableHeaderVariantsProps,
+    type GuiTableRootVariantsProps,
+    type GuiTableRowVariantsProps,
+    recipeGuiTable,
+} from "@happy.tech/design-system"
+import { flexRender } from "@tanstack/react-table"
+import { cx } from "cva"
+import { type HTMLAttributes, type TdHTMLAttributes, type ThHTMLAttributes, forwardRef } from "react"
+import { useDataviewTable } from "./use-dataview-table"
 
-interface GuiTableProps extends HTMLAttributes<HTMLTableElement> , GuiTableRootVariantsProps {}
+interface GuiTableProps extends HTMLAttributes<HTMLTableElement>, GuiTableRootVariantsProps {}
 export const GuiTable = forwardRef<HTMLTableElement, GuiTableProps>(
-  ({ className = '', columnsSizing, intent, scale, ...props }, ref) => {
-    return (
-      <table
-        ref={ref}
-        className={cx(recipeGuiTable.table({ columnsSizing, intent, scale }), className)}
-        {...props}
-      />
-    );
-  }
-);
+    ({ className = "", columnsSizing, intent, scale, ...props }, ref) => {
+        return (
+            <table
+                ref={ref}
+                data-part="hds-dataview-table"
+                className={cx(recipeGuiTable.table({ columnsSizing, intent, scale }), className)}
+                {...props}
+            />
+        )
+    },
+)
 
-interface GuiTableHeaderProps extends HTMLAttributes<HTMLTableSectionElement>, GuiTableHeaderVariantsProps{}
+interface GuiTableHeaderProps extends HTMLAttributes<HTMLTableSectionElement>, GuiTableHeaderVariantsProps {}
 export const GuiTableHeader = forwardRef<HTMLTableSectionElement, GuiTableHeaderProps>(
-  ({ className = '', scale, intent, ...props }, ref) => {
-    return (
-      <thead
-        ref={ref}
-        className={cx(recipeGuiTable.header({ scale, intent }), className)}
-        {...props}
-      />
-    );
-  }
-);
+    ({ className = "", scale, intent, ...props }, ref) => {
+        return (
+            <thead
+                ref={ref}
+                data-part="hds-dataview-table-header"
+                className={cx(recipeGuiTable.header({ scale, intent }), className)}
+                {...props}
+            />
+        )
+    },
+)
 
-interface GuiTableRowProps extends HTMLAttributes<HTMLTableRowElement> {}
+interface GuiTableRowProps extends HTMLAttributes<HTMLTableRowElement>, GuiTableRowVariantsProps {}
 export const GuiTableRow = forwardRef<HTMLTableRowElement, GuiTableRowProps>(
-  (props, ref) => {
-    return (
-      <tr
-        ref={ref}
-        {...props}
-      />
-    );
-  }
-);
+    ({ scale, type, className, ...props }, ref) => {
+        return <tr ref={ref} className={cx(recipeGuiTable.row({ scale, type }), className)} {...props} />
+    },
+)
 
-interface GuiTableCellProps extends TdHTMLAttributes<HTMLTableCellElement> {}
+interface GuiTableCellProps extends TdHTMLAttributes<HTMLTableCellElement>, GuiTableBodyCellVariantsProps {}
 export const GuiTableCell = forwardRef<HTMLTableCellElement, GuiTableCellProps>(
-  (props, ref) => {
-    return (
-      <td
-        ref={ref}
-        {...props}
-      />
-    );
-  }
-);
+    ({ scale, className, ...props }, ref) => {
+        return (
+            <td
+                data-part="hds-dataview-table-body-cell"
+                ref={ref}
+                className={cx(recipeGuiTable.headerCell({ scale }), className)}
+                {...props}
+            />
+        )
+    },
+)
 
 interface GuiTableHeaderCellProps extends ThHTMLAttributes<HTMLTableCellElement>, GuiTableHeaderCellVariantsProps {}
 export const GuiTableHeaderCell = forwardRef<HTMLTableCellElement, GuiTableHeaderCellProps>(
-  ({ className = '', scale, ...props }, ref) => {
-    return (
-      <th
-        ref={ref}
-        className={cx(recipeGuiTable.headerCell({ scale }), className)}
-        {...props}
-      />
-    );
-  }
-);
+    ({ className = "", scale, ...props }, ref) => {
+        return (
+            <th
+                ref={ref}
+                data-part="hds-dataview-table-header-cell"
+                className={cx(recipeGuiTable.headerCell({ scale }), className)}
+                {...props}
+            />
+        )
+    },
+)
 
 interface GuiTableBodyProps extends HTMLAttributes<HTMLTableSectionElement>, GuiTableBodyVariantsProps {}
 export const GuiTableBody = forwardRef<HTMLTableSectionElement, GuiTableBodyProps>(
-  ({ className = '', intent, scale, ...props}, ref) => {
-    return (
-      <tbody
-        ref={ref}
-        className={cx(recipeGuiTable.body({ scale }), className)}
-        {...props}
-      />
-    );
-  }
-);
+    ({ className = "", scale, ...props }, ref) => {
+        return (
+            <tbody
+                ref={ref}
+                data-part="hds-dataview-table-body"
+                className={cx(recipeGuiTable.body({ scale }), className)}
+                {...props}
+            />
+        )
+    },
+)
 
 export const GuiTableRows = () => {
-  const table = useDataviewTable();
-  return (
-    <>
-      {table.getRowModel().rows.map(row => (
-        <GuiTableRow key={row.id}>
-          {row.getVisibleCells().map(cell => (
-            <GuiTableCell key={cell.id}>
-              {flexRender(cell.column.columnDef.cell, cell.getContext())}
-            </GuiTableCell>
-          ))}
-        </GuiTableRow>
-      ))}
-    </>
-  );
-};
+    const table = useDataviewTable()
+    return (
+        <>
+            {table.getRowModel().rows.map((row) => (
+                <GuiTableRow data-part="hds-dataview-table-body-row" type="body" key={row.id}>
+                    {row.getVisibleCells().map((cell) => (
+                        <GuiTableCell key={cell.id}>
+                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                        </GuiTableCell>
+                    ))}
+                </GuiTableRow>
+            ))}
+        </>
+    )
+}
 
 export const GuiTableHeaders = () => {
-  const table = useDataviewTable();
-  return (
-    <GuiTableRow>
-      {table.getHeaderGroups()[0].headers.map(header => (
-        <GuiTableHeaderCell key={header.id}>
-          {flexRender(
-            header.column.columnDef.header,
-            header.getContext()
-          )}
-        </GuiTableHeaderCell>
-      ))}
-    </GuiTableRow>
-  );
-};
+    const table = useDataviewTable()
+    return (
+        <GuiTableRow data-part="hds-dataview-table-header-row" type="header">
+            {table.getHeaderGroups()[0].headers.map((header) => (
+                <GuiTableHeaderCell key={header.id}>
+                    {flexRender(header.column.columnDef.header, header.getContext())}
+                </GuiTableHeaderCell>
+            ))}
+        </GuiTableRow>
+    )
+}
