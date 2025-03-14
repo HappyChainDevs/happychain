0a16d8210cd307b148fd8f38185e97e66a4b1731
ultraviolet10 <aritrac1998@hotmail.com>
2025-03-14 13:05:07 +0530

    fix: import reuse + format

--- apps/iframe/src/utils/createHappyUserFromWallet.ts
+++ apps/iframe/src/utils/createHappyUserFromWallet.ts
@@ -17,7 +17,7 @@ export async function createHappyUserFromWallet(providerId: string, address: Add
         uid: address,
 
         // web3 details
-        address: address,
+        address: accountAddress,
         controllingAddress: address,
     }
 }
