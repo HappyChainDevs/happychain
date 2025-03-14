b95844d3fa5ef167b591b5dfbd1dd7b89ddaab40
ultraviolet10 <aritrac1998@hotmail.com>
2025-03-14 12:42:52 +0530

    feat: app demo scaffold

--- demos/react/src/demo-components/CallBatchDemo.tsx
+++ demos/react/src/demo-components/CallBatchDemo.tsx
@@ -34,18 +34,14 @@ const CallBatchDemo = () => {
         <div className="grid grid-cols-2 gap-4 backdrop-blur-sm bg-gray-200/35 p-4 rounded-lg">
             <div className="text-lg font-bold col-span-2">EIP-5792</div>
 
-            <button
-                type="button"
-                onClick={getCapabilities}
-                className="rounded-lg bg-sky-300 p-2 shadow-xl font-mono truncate"
-            >
+            <button type="button" onClick={getCapabilities} className="rounded-lg bg-sky-300 p-2 shadow-xl font-mono">
                 wallet_getCapabilities
             </button>
 
             <button
                 type="button"
                 onClick={sendBatch}
-                className="rounded-lg bg-sky-300 p-2 shadow-xl text-center font-mono truncate"
+                className="rounded-lg bg-sky-300 p-2 shadow-xl text-center font-mono"
             >
                 wallet_sendCalls
             </button>
