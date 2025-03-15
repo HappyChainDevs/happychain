509949df4563ec141e537ecba826b71f4edc28fd
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-03-15 13:00:51 +0100

    add some spacing in RequestSessionKey

--- apps/iframe/src/components/requests/HappyRequestSessionKey.tsx
+++ apps/iframe/src/components/requests/HappyRequestSessionKey.tsx
@@ -46,19 +46,21 @@ export const HappyRequestSessionKey = ({
             headline={<>Enable automatic approvals</>}
             description={
                 <>
-                    <p className="mb-2">
-                        The app will be able to send transactions to{" "}
-                        <a
-                            href={`${blockExplorerUrl}/address/${targetAddress}?tab=contract`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-primary border-b border-dashed hover:bg-primary/40"
-                        >
-                            {shortenAddress(targetAddress)}
-                        </a>{" "}
-                        without approval.
-                    </p>
-                    <p>You can revoke automatic approvals from the wallet.</p>
+                    <div className="mb-4">
+                        <p className="mb-2">
+                            The app will be able to send transactions to{" "}
+                            <a
+                                href={`${blockExplorerUrl}/address/${targetAddress}?tab=contract`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-primary border-b border-dashed hover:bg-primary/40"
+                            >
+                                {shortenAddress(targetAddress)}
+                            </a>{" "}
+                            without approval.
+                        </p>
+                        <p>You can revoke automatic approvals from the wallet.</p>
+                    </div>
                     <SectionBlock>
                         <div className="grid bg-warning/40 border-warning text-warning-content/90 dark:bg-warning/5 dark:border-warning/20 dark:text-warning gap-2 text-sm border py-[1em] px-[1.25em] rounded-lg w-full">
                             <p>You could lose tokens the contract can access.</p>
