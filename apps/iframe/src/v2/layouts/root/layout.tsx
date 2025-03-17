1c8d0fb64e1e80efdde2e5d37f03872363839da8
naomiHauret <hauretnaomi@gmail.com>
2025-03-17 01:01:12 +0100

    feat(iframe): redesign - app layout

---
+++ apps/iframe/src/v2/layouts/root/layout.tsx
@@ -0,0 +1,48 @@
+import type { PropsWithChildren } from "react"
+import { Device } from "./device"
+import { Root } from "./provider"
+import { Screen } from "./screen"
+
+export const Layout = {
+    Root,
+    Device,
+    Screen,
+}
+
+/**
+ * Layout common to all pages.
+ * Uses an "islands" architecture, where distinct interactive areas are visually and functionally 
+ * separated from the main content.
+ * 
+ * We have 2 key islands in our layout :
+ * 
+ * 1. Device.ActionsIsland:
+ *    - Located inside <Device /> but outside its <Screen /> 
+ *    - Contains contextual actions that change based on current screen
+ * 
+ * 2. Screen.View.BottomNavbarIsland:
+ *    - Represents software navigation within the digital interface
+ *    - Located inside <Screen.View /> (part of the digital display)
+ *    - Appears and behaves like a traditional mobile app navbar
+
+ * @note Nesting order matters - Root (state) > Device (hardware) > Screen (display) > View (page/screen)
+ * @note Children are rendered within `<Screen.View />`, not directly in Device or Screen
+ * @note Page-specific controls should be handled by exporting custom components 
+ *       that ActionsIsland and BottomNavbarIsland can render conditionally
+ */
+export const RootLayout = ({ children }: PropsWithChildren) => {
+    return (
+        <Layout.Root>
+            <Layout.Device>
+                <Layout.Screen>
+                    <Layout.Screen.View>
+                        {children}
+                        <Layout.Screen.View.BottomNavbarIsland />
+                    </Layout.Screen.View>
+                </Layout.Screen>
+                <Layout.Device.NavSlider />
+                <Layout.Device.ActionsIsland />
+            </Layout.Device>
+        </Layout.Root>
+    )
+}
