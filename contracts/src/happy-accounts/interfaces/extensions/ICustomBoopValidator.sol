d7efff4651ff6dc9dd8ed243e0d1eda44194ed01
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-03-23 00:36:20 +0100

    improve doc for extension interfaces

--- contracts/src/happy-accounts/interfaces/extensions/ICustomBoopValidator.sol
+++ contracts/src/happy-accounts/interfaces/extensions/ICustomBoopValidator.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.8.20;
 
 import {HappyTx} from "../../core/HappyTx.sol";
 
-/// Interface for custom boop validators.
+/// Interface for custom validators that can be registered with Boop accounts implementing
+/// {IExtensibleBoopAccount}, with extension type {ExtensionType.Validator}.
 interface ICustomBoopValidator {
+    /// Same interface and specification as {IBoopAccount.validate}.
     function validate(HappyTx memory happyTx) external returns (bytes4);
 }
