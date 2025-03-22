6143efb39c9293abd863a4bb1f5e383e56bb9f44
naomiHauret <hauretnaomi@gmail.com>
2025-03-22 01:26:51 +0100

    feat(design system): implement combobox primitive

--- packages/uikit-react/src/index.ts
+++ packages/uikit-react/src/index.ts
@@ -1,10 +1,16 @@
+// Styled UI Kit
 export * from "./alert/alert"
 export * from "./button/button"
 export * from "./collapsible/collapsible"
 export * from "./dialog/dialog"
+export * from "./combobox/combobox"
 export * from "./field/field"
 export * from "./input/input"
 export * from "./menu/menu"
 export * from "./table/table"
 export * from "./slider/slider"
 export { useDataviewTable } from "./table/use-dataview-table"
+
+// Unstyled Ark primitives
+export * from "@ark-ui/react/format"
+export * from "@ark-ui/react/factory"
