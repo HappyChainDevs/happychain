24d28189c61ad05f91b5030a7b11000801023cd9
naomiHauret <hauretnaomi@gmail.com>
2025-03-22 01:28:42 +0100

    feat(iframe): redesign send screen

--- apps/iframe/src/hooks/useFormSendAssets.ts
+++ apps/iframe/src/hooks/useFormSendAssets.ts
@@ -1,38 +1,54 @@
+import { createListCollection } from "@happy.tech/uikit-react"
+import { useForm } from "@tanstack/react-form"
 import { useNavigate } from "@tanstack/react-router"
-import { atom, useAtom, useAtomValue } from "jotai"
-import { useCallback, useEffect, useMemo } from "react"
-import { type Address, formatEther, isAddress, parseEther } from "viem"
-import { useBalance, useSendTransaction, useWaitForTransactionReceipt } from "wagmi"
-import { getBalanceQueryKey } from "wagmi/query"
-import { coerce, object, string } from "zod"
+import { useAtomValue } from "jotai"
+import { useCallback, useEffect, useMemo, useState } from "react"
+import { type Address, type Hash, erc20Abi, isAddress, parseEther, parseUnits } from "viem"
+import { useSendTransaction, useWaitForTransactionReceipt, useWriteContract } from "wagmi"
+import { getBalanceQueryKey, useMutation } from "wagmi/query"
+import { coerce, number, object, string } from "zod"
 import { userAtom } from "#src/state/user"
+import { watchedAssetsAtom } from "#src/state/watchedAssets"
 import { queryClient } from "#src/tanstack-query/config"
+import { useTokenBalance } from "./useTokenBalance"
 
 /**
  * Form field names values
  */
 export enum FieldFormSendAssets {
+    Token = "send-asset-token-address",
+    TokenDecimals = "send-asset-token-decimals",
     Recipient = "send-asset-recipient",
     Amount = "send-asset-amount",
 }
 
-const DEFAULT_FORM_STATE = {
-    [FieldFormSendAssets.Recipient]: undefined,
-    [FieldFormSendAssets.Amount]: 0,
+type DataFormSendToken = {
+    [FieldFormSendAssets.Amount]: number
+    [FieldFormSendAssets.TokenDecimals]: number
+    [FieldFormSendAssets.Token]?: Address | ""
+    [FieldFormSendAssets.Recipient]: Address
 }
 
-/**
- * Form inputs values
- */
-const formSendAssetsAtom = atom<{
-    [FieldFormSendAssets.Recipient]?: Address
-    [FieldFormSendAssets.Amount]?: number
-}>(DEFAULT_FORM_STATE)
+export type TokensComboboxItem = {
+    type: "ERC20" | "native"
+    address: Address | ""
+    decimals: number
+    label: string
+    value: Address | ""
+}
 
 /**
  * Send asset form validation schema
  */
-const schemaFormSendAsset = object({
+export const schemaFormSendAsset = object({
+    [FieldFormSendAssets.Token]: string()
+        .length(0)
+        .or(
+            string().refine((val) => isAddress(val), {
+                message: "Ensure you select a token.",
+            }),
+        ),
+    [FieldFormSendAssets.TokenDecimals]: number().positive().default(18),
     [FieldFormSendAssets.Recipient]: string({
         message: "Ensure you provide a valid Ethereum address for the recipient.",
     }).refine((val) => isAddress(val), {
@@ -40,129 +56,194 @@ const schemaFormSendAsset = object({
     }),
     [FieldFormSendAssets.Amount]: coerce
         .number({
-            message: "Ensure you enter a positive amount number.",
+            message: "Ensure you enter a positive number for the amount.",
         })
-        .positive({ message: "Ensure you enter a positive amount number." }),
+        .positive({ message: "Ensure you enter a positive number for the amount." }),
 })
 
 /**
  * Custom hook for managing send assets form state and its operations
  */
-export function useFormSendAssets(asset?: Address) {
+export function useFormSendAssets() {
     const navigate = useNavigate()
     const user = useAtomValue(userAtom)
-    const formAtom = useAtom(formSendAssetsAtom)
-    const [form, setForm] = formAtom
 
-    /**
-     * Get user's native token balance.
-     * Only enabled when user is connected and no asset address is provided
-     */
-    const queryBalanceNativeToken = useBalance({
-        address: user?.address,
-        query: {
-            enabled: user?.address && isAddress(user?.address) && !isAddress(`${asset}`),
+    // Tokens combobox
+    const watchedAssets = useAtomValue(watchedAssetsAtom)
+    const userAssets = watchedAssets?.[user!.address] ?? []
+    const [initialTokensList] = useState([
+        {
+            address: "",
+            symbol: "HAPPY",
+            label: "HAPPY",
+            type: "native",
+            decimals: 24,
+            value: "",
+        },
+        {
+            type: "ERC20",
+            address: "0xc80629fE33747288AaFb97684F86f7eD2D1aBF69",
+            decimals: 18,
+            label: "MTA",
+            value: "0xc80629fE33747288AaFb97684F86f7eD2D1aBF69",
+        },
+        {
+            type: "ERC20",
+            address: "0xF2Cd1312bcE60Edcd17406962aB3d76730bf4873",
+            decimals: 18,
+            label: "MTB",
+            value: "0xF2Cd1312bcE60Edcd17406962aB3d76730bf4873",
+        },
+        {
+            type: "ERC20",
+            address: "0x09420341423aE6D9c14C26052AF66A948BE4982c",
+            decimals: 18,
+            label: "MTC",
+            value: "0x09420341423aE6D9c14C26052AF66A948BE4982c",
+        },
+        ...userAssets.map((asset) => ({
+            ...asset.options,
+            type: asset.type,
+            label: asset.options.symbol,
+            value: asset.options.address,
+        })),
+    ])
+    const [defaultSelectedToken] = useState(initialTokensList[0])
+    const [tokens, setTokens] = useState(initialTokensList)
+    const collectionTokens = useMemo(() => createListCollection({ items: tokens }), [tokens])
+
+    // Form config
+    const form = useForm({
+        defaultValues: {
+            [FieldFormSendAssets.Recipient]: "",
+            [FieldFormSendAssets.Token]: defaultSelectedToken.value,
+            [FieldFormSendAssets.TokenDecimals]: 18,
+            [FieldFormSendAssets.Amount]: 0,
+        },
+        validators: {
+            //@ts-expect-error - onChange clashing with the validator for some reason
+            onChange: schemaFormSendAsset.refine((values) => {
+                if (
+                    values[FieldFormSendAssets.Token] === "" ||
+                    userAssets.find((token) => token.options.address === values[FieldFormSendAssets.Token])
+                )
+                    return {
+                        message: "Select a valid token.",
+                    }
+            }),
+        },
+        onSubmit: ({ value }) => {
+            if (mutationSendToken.status === "pending") return
+            mutationSendToken.mutate(value as DataFormSendToken)
         },
     })
 
-    const mutationSendTransaction = useSendTransaction({
-        mutation: {
-            onSettled() {
-                queryClient.invalidateQueries({
-                    queryKey: getBalanceQueryKey({
-                        address: user?.address,
-                    }),
+    // Balance
+    const {
+        queryBalanceNativeToken,
+        queryBalanceERC20Token,
+        configQueryToken: [, setQueryTokenBalance],
+        balance,
+    } = useTokenBalance({
+        userAddress: user?.address as Address,
+        tokenAddress: defaultSelectedToken.address as Address,
+    })
+
+    // Combobox handlers
+    function handleTokensComboboxInputChange(details: { inputValue: string }) {
+        const matchValue = details.inputValue.trim().toLowerCase()
+        const filtered = tokens.filter(
+            (item) => item.value.toLowerCase().includes(matchValue) || item.label.toLowerCase().includes(matchValue),
+        )
+        setTokens(filtered.length === 0 || matchValue.length === 0 ? initialTokensList : filtered)
+    }
+
+    function handleTokensComboboxValueChange(item: TokensComboboxItem) {
+        form.setFieldValue(FieldFormSendAssets.Token, item.value)
+        form.setFieldValue(FieldFormSendAssets.TokenDecimals, item?.decimals ?? 18)
+        setQueryTokenBalance(item.value as Address)
+    }
+
+    // Form mutations & queries
+    const mutationSendNativeToken = useSendTransaction()
+    const mutationTransferERC20 = useWriteContract()
+
+    const mutationSendToken = useMutation({
+        mutationFn: async (values: DataFormSendToken) => {
+            mutationSendNativeToken.reset()
+            mutationTransferERC20.reset()
+
+            const from = user?.address as Address
+            const to = values[FieldFormSendAssets.Recipient] as Address
+            const tokenAddress = values[FieldFormSendAssets.Token] as Address
+            if (isAddress(tokenAddress)) {
+                // ERC20: `transfer`
+                const amount = parseUnits(
+                    `${values[FieldFormSendAssets.Amount]}`,
+                    values[FieldFormSendAssets.TokenDecimals] as number,
+                )
+                return await mutationTransferERC20.writeContractAsync({
+                    abi: erc20Abi,
+                    address: tokenAddress as Address,
+                    functionName: "transferFrom",
+                    args: [from, to, amount],
                 })
-            },
+            } else {
+                // Native token - `sendTransaction`
+                return await mutationSendNativeToken.sendTransaction({
+                    to: values[FieldFormSendAssets.Recipient] as Address,
+                    value: parseEther(`${values[FieldFormSendAssets.Amount]}`),
+                })
+            }
+        },
+        onSettled() {
+            queryClient.invalidateQueries({
+                queryKey: getBalanceQueryKey({
+                    address: user?.address,
+                }),
+            })
         },
     })
 
     const queryWaitForTransactionReceipt = useWaitForTransactionReceipt({
-        hash: mutationSendTransaction?.data,
+        hash: mutationSendToken?.data as Hash,
         query: {
-            enabled: !!mutationSendTransaction.data,
+            enabled: !!mutationSendToken.data,
         },
     })
 
-    const maximumValueNativeToken = useMemo(() => {
-        if (queryBalanceNativeToken?.data?.value) return +formatEther(queryBalanceNativeToken?.data?.value)
-        return 0
-    }, [queryBalanceNativeToken?.data?.value])
-
-    /**
-     * Run custom validation on form fields to refine form errors
-     */
-    const formErrors = useMemo(() => {
-        const validationSchema = schemaFormSendAsset.refine(
-            (vals) => {
-                return vals[FieldFormSendAssets.Amount] <= maximumValueNativeToken
-            },
-            {
-                message: "The amount exceeds your available balance",
-                path: [FieldFormSendAssets.Amount],
-            },
-        )
-
-        const result = validationSchema.safeParse(form)
-        if (!result.success) {
-            return result.error.flatten()
-        }
-
-        return null
-    }, [form, maximumValueNativeToken])
-
-    /**
-     * Sets the native token amount to the maximum available balance
-     */
-    function handleOnClickMaxNativeTokenBalance() {
-        if (mutationSendTransaction.status === "pending" || maximumValueNativeToken === 0) return
-        setForm({
-            ...form,
-            [FieldFormSendAssets.Amount]: maximumValueNativeToken,
-        })
-    }
-
-    /**
-     * Ensures form data is valid befor initiating transaction
-     */
-    function handleOnSubmit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault()
-        if (mutationSendTransaction.status === "pending" || formErrors) return
-        mutationSendTransaction.sendTransaction({
-            to: form[FieldFormSendAssets.Recipient] as Address,
-            value: parseEther(`${form[FieldFormSendAssets.Amount]}`),
-        })
-    }
-
-    /**
-     * Updates form state when input value changes
-     */
-    function handleOnInput(e: React.ChangeEvent<HTMLInputElement>) {
-        setForm({
-            ...form,
-            [e.currentTarget.name]: e.currentTarget.value,
-        })
-    }
-
+    // Side effects
     const onTransactionCompleted = useCallback(() => {
-        mutationSendTransaction.reset()
-        setForm(DEFAULT_FORM_STATE)
+        mutationSendToken.reset()
+        //setForm(DEFAULT_FORM_STATE)
         navigate({ to: "/embed" })
-    }, [setForm, navigate, mutationSendTransaction])
+    }, [navigate, mutationSendToken])
 
     useEffect(() => {
         if (queryWaitForTransactionReceipt.isSuccess) onTransactionCompleted()
     }, [queryWaitForTransactionReceipt.isSuccess, onTransactionCompleted])
 
     return {
-        formAtom,
-        formErrors,
-        handleOnClickMaxNativeTokenBalance,
-        handleOnInput,
-        handleOnSubmit,
-        maximumValueNativeToken,
-        mutationSendTransaction,
-        queryBalanceNativeToken,
+        // Form state, mutations, queries
+        form,
+        mutationSendTransaction: mutationSendToken,
         queryWaitForTransactionReceipt,
+
+        // Token combobox
+        tokens: {
+            defaultSelection: defaultSelectedToken,
+            items: tokens,
+            collection: collectionTokens,
+        },
+        handleTokensComboboxInputChange,
+        handleTokensComboboxValueChange,
+
+        // Max token amount
+        maxTokenAmount: {
+            status: isAddress(form.getFieldValue(FieldFormSendAssets.Token))
+                ? queryBalanceERC20Token.status
+                : queryBalanceNativeToken.status,
+            amount: balance,
+        },
     }
 }
