2a940ee7e5d790fd8eb24c6665e59e67ba19f0db
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-03-23 00:36:20 +0100

    improve doc for extension interfaces

--- contracts/src/happy-accounts/interfaces/extensions/ICustomBoopExecutor.sol
+++ contracts/src/happy-accounts/interfaces/extensions/ICustomBoopExecutor.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.20;
 import {HappyTx} from "../../core/HappyTx.sol";
 import {ExecutionOutput} from "../../interfaces/IHappyAccount.sol";
 
-/// Interface for custom boop executors.
+/// Interface for custom validators that can be registered with Boop accounts implementing
+/// {IExtensibleBoopAccount}, with extension type {ExtensionType.Executor}.
 interface ICustomBoopExecutor {
+    /// Same interface and specification as {IBoopAccount.execute}.
     function execute(HappyTx memory happyTx) external returns (ExecutionOutput memory output);
 }
