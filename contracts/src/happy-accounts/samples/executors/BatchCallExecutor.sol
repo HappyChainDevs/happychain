dbb62f8d9962a09576ff4d43706b187d7cfcd8b5
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-03-23 03:36:27 +0100

    Execution > CallInfo + add comments

--- contracts/src/happy-accounts/samples/executors/BatchCallExecutor.sol
+++ contracts/src/happy-accounts/samples/executors/BatchCallExecutor.sol
@@ -13,28 +13,31 @@ import {HappyTx} from "../../core/HappyTx.sol";
 import {HappyTxLib} from "../../libs/HappyTxLib.sol";
 
 /**
- * @dev Key used in {HappyTx.extraData} for call information (array of {BatchCallExecutor.Execution}),
+ * @dev Key used in {HappyTx.extraData} for call information (array of {CallInfo}),
  * to be looked up by {BatchCallExecutor.execute}.
  */
-bytes3 constant BATCH_CALL_KEY = 0x000100;
+bytes3 constant BATCH_CALL_INFO_KEY = 0x000100;
 
-/// @dev The execution data of a call in a batch
-struct Execution {
+/**
+ * @dev Selector returned by {BatchCallExecutor.execute} when no call information is found in the
+ * extraData.
+ */
+error MissingBatchCallInfo();
+
+/// @dev Information (destination, value and calldata) for a batched call.
+struct CallInfo {
     address dest;
     uint256 value;
     bytes callData;
 }
 
+/**
+ * This executor executes multiples calls in an atomic way (all run, or all revert).
+ *
+ * Each call specified is specified in a {CallInfo} struct, which are together stored in an
+ * ABI-encoded array in {HappyTx.extraData}, keyed on {BATCH_CALL_INFO_KEY}.
+ */
 contract BatchCallExecutor is ICustomBoopExecutor, ReentrancyGuardTransient, OwnableUpgradeable, UUPSUpgradeable {
-    // ====================================================================================================
-    // ERRORS
-
-    /**
-     * @dev Selector returned by {BatchCallExecutor.execute} when no batch call data is found in the extraData.
-     *      This indicates that the transaction failed to include the required batch call information.
-     */
-    error MissingBatchCallData();
-
     // ====================================================================================================
     // CONSTANTS
 
@@ -61,11 +64,11 @@ contract BatchCallExecutor is ICustomBoopExecutor, ReentrancyGuardTransient, Own
 
     function execute(HappyTx memory happyTx) external returns (ExecutionOutput memory output) {
         // 1. Parse the extraData with a key, to retrieve the calls
-        (bool found, bytes memory calls) = HappyTxLib.getExtraDataValue(happyTx.extraData, BATCH_CALL_KEY);
-        if (!found) revert MissingBatchCallData();
+        (bool found, bytes memory calls) = HappyTxLib.getExtraDataValue(happyTx.extraData, BATCH_CALL_INFO_KEY);
+        if (!found) revert MissingBatchCallInfo();
 
         // 2. decodeBatch the bytes memory -> bytes memory[]
-        Execution[] memory executionBatch = abi.decode(calls, (Execution[]));
+        CallInfo[] memory executionBatch = abi.decode(calls, (CallInfo[]));
 
         // 3. call _executeBatch -> executes each call individually
         return _executeBatch(executionBatch);
@@ -75,7 +78,7 @@ contract BatchCallExecutor is ICustomBoopExecutor, ReentrancyGuardTransient, Own
     // INTERNAL FUNCTIONS
 
     /// @dev Executes a batch of calls, returns the total gas used
-    function _executeBatch(Execution[] memory executionBatch) internal returns (ExecutionOutput memory output) {
+    function _executeBatch(CallInfo[] memory executionBatch) internal returns (ExecutionOutput memory output) {
         // For each call, execute it and add the gas used to the total
         // If any call reverts, the entire batch reverts, return that call's output.revertData
         for (uint256 i = 0; i < executionBatch.length; i++) {
@@ -91,7 +94,7 @@ contract BatchCallExecutor is ICustomBoopExecutor, ReentrancyGuardTransient, Own
     }
 
     /// @dev Executes a single call, returns the gas used
-    function _execute(Execution memory execution) internal returns (ExecutionOutput memory output) {
+    function _execute(CallInfo memory execution) internal returns (ExecutionOutput memory output) {
         uint256 startGas = gasleft();
         (bool success, bytes memory returnData) = execution.dest.call{value: execution.value}(execution.callData);
         if (!success) {
