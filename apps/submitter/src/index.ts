20c264ded0caa0f8b8886bcb6b89037044bbcfc3
not-reed <reed@happy.tech>
2025-03-24 11:56:48 -0700

    create submitter app from package

---
+++ apps/submitter/src/index.ts
@@ -0,0 +1,29 @@
+import { app, env } from "@happy.tech/submitter"
+import pkg from "@happy.tech/submitter/package.json"
+import { injectOpenAPIDocs } from "./docs"
+
+// Enable API Documentation page
+injectOpenAPIDocs(app, {
+    documentation: {
+        info: {
+            title: "Boop",
+            version: pkg.version,
+            description: "Happy Account Submitter",
+        },
+        servers: [
+            {
+                url: "http://localhost:3001",
+                description: "Local server",
+            },
+            {
+                url: "https://boop.happy.tech",
+                description: "Local server",
+            },
+        ],
+    },
+})
+
+export default {
+    port: env.APP_PORT,
+    fetch: app.fetch,
+}
