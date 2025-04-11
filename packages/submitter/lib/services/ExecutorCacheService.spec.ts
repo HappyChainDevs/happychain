75c4b272264c0e34ef11c9f9c736c26802e6b8ba
not-reed <reed@happy.tech>
2025-04-11 13:23:11 -0700

    Support multiple executor keys with load balancing

---
+++ packages/submitter/lib/services/ExecutorCacheService.spec.ts
@@ -0,0 +1,125 @@
+import { beforeEach, describe, expect, it } from "bun:test"
+import { type Account, generatePrivateKey, privateKeyToAccount } from "viem/accounts"
+import { ExecutorCacheService } from "./ExecutorCacheService"
+
+describe("ExecutorCacheService", () => {
+    let executorService: ExecutorCacheService
+    let testUserAccount: Account
+    let accounts: Account[]
+
+    beforeEach(() => {
+        executorService = new ExecutorCacheService(100)
+        testUserAccount = privateKeyToAccount(generatePrivateKey())
+
+        const defaultAccount = privateKeyToAccount(generatePrivateKey())
+        const accountOne = privateKeyToAccount(generatePrivateKey())
+        accounts = [defaultAccount, accountOne]
+
+        for (const account of accounts) {
+            executorService.registerExecutor(account)
+        }
+    })
+    it("returns an executor", () => {
+        const account = executorService.get("0x1", testUserAccount.address, 1n)
+        expect(account).toBeDefined()
+        expect(account.address).toBe(accounts[0].address)
+    })
+
+    it("returns the same executor for multiple calls with the same user account and nonceTrack", () => {
+        const expected = accounts[0].address
+        expect(executorService.get("0x1", testUserAccount.address, 1n).address).toBe(expected)
+        expect(executorService.get("0x2", testUserAccount.address, 1n).address).toBe(expected)
+        expect(executorService.get("0x3", testUserAccount.address, 1n).address).toBe(expected)
+        expect(executorService.get("0x4", testUserAccount.address, 1n).address).toBe(expected)
+        expect(executorService.get("0x5", testUserAccount.address, 1n).address).toBe(expected)
+    })
+
+    it("returns the different executors for multiple calls with the same user account and different nonceTrack", () => {
+        const first = accounts[0].address
+        const second = accounts[1].address
+        expect(executorService.get("0x1", testUserAccount.address, 1n).address).toBe(first)
+        expect(executorService.get("0x2", testUserAccount.address, 2n).address).toBe(second)
+        expect(executorService.get("0x3", testUserAccount.address, 3n).address).toBe(second)
+        expect(executorService.get("0x4", testUserAccount.address, 4n).address).toBe(first)
+    })
+
+    it("returns allows hashes to expire", async () => {
+        const first = accounts[0].address
+        const second = accounts[1].address
+        expect(executorService.get("0x1", testUserAccount.address, 1n).address).toBe(first)
+        expect(executorService.get("0x2", testUserAccount.address, 2n).address).toBe(second)
+        await new Promise((resolve) => setTimeout(resolve, 100)) // allows the timeout to expire
+        expect(executorService.get("0x3", testUserAccount.address, 3n).address).toBe(first)
+        expect(executorService.get("0x4", testUserAccount.address, 4n).address).toBe(second)
+    })
+
+    it("maintains consistency through ttl expirys", async () => {
+        const first = accounts[0].address
+        const second = accounts[1].address
+        expect(executorService.get("0x1", testUserAccount.address, 1n).address).toBe(first)
+        expect(executorService.get("0x2", testUserAccount.address, 2n).address).toBe(second)
+        await new Promise((resolve) => setTimeout(resolve, 50))
+        expect(executorService.get("0x3", testUserAccount.address, 2n).address).toBe(second)
+        await new Promise((resolve) => setTimeout(resolve, 50))
+        expect(executorService.get("0x4", testUserAccount.address, 2n).address).toBe(second)
+        await new Promise((resolve) => setTimeout(resolve, 50))
+        expect(executorService.get("0x5", testUserAccount.address, 2n).address).toBe(second)
+        await new Promise((resolve) => setTimeout(resolve, 50))
+        expect(executorService.get("0x6", testUserAccount.address, 2n).address).toBe(second)
+        await new Promise((resolve) => setTimeout(resolve, 50))
+
+        // new entry (3n), first has expired, second has not
+        expect(executorService.get("0x7", testUserAccount.address, 2n).address).toBe(second)
+        expect(executorService.get("0x8", testUserAccount.address, 3n).address).toBe(first)
+        expect(executorService.get("0x9", testUserAccount.address, 4n).address).toBe(first)
+        expect(executorService.get("0x10", testUserAccount.address, 5n).address).toBe(first)
+    })
+
+    it("expires and refreshes", async () => {
+        const first = accounts[0].address
+        const second = accounts[1].address
+        expect(executorService.get("0x1", testUserAccount.address, 1n).address).toBe(first)
+        expect(executorService.get("0x1", testUserAccount.address, 2n).address).toBe(second)
+        expect(executorService.stats()).toStrictEqual([
+            { address: second, count: 1 },
+            { address: first, count: 1 },
+        ])
+        await new Promise((resolve) => setTimeout(resolve, 50))
+        expect(executorService.get("0x2", testUserAccount.address, 2n).address).toBe(second)
+        expect(executorService.get("0x2", testUserAccount.address, 3n).address).toBe(first)
+        expect(executorService.stats()).toStrictEqual([
+            { address: first, count: 2 },
+            { address: second, count: 2 },
+        ])
+        await new Promise((resolve) => setTimeout(resolve, 50))
+        expect(executorService.get("0x3", testUserAccount.address, 2n).address).toBe(second)
+        expect(executorService.get("0x3", testUserAccount.address, 3n).address).toBe(first)
+        expect(executorService.get("0x3", testUserAccount.address, 4n).address).toBe(first)
+        expect(executorService.stats()).toStrictEqual([
+            { address: second, count: 2 },
+            { address: first, count: 3 },
+        ])
+        await new Promise((resolve) => setTimeout(resolve, 50))
+        expect(executorService.get("0x4", testUserAccount.address, 4n).address).toBe(first)
+        expect(executorService.stats()).toStrictEqual([
+            { address: second, count: 1 },
+            { address: first, count: 3 },
+        ])
+        await new Promise((resolve) => setTimeout(resolve, 50))
+        expect(executorService.get("0x5", testUserAccount.address, 4n).address).toBe(first)
+        expect(executorService.stats()).toStrictEqual([
+            { address: second, count: 0 },
+            { address: first, count: 2 },
+        ])
+        await new Promise((resolve) => setTimeout(resolve, 50))
+        expect(executorService.stats()).toStrictEqual([
+            { address: second, count: 0 },
+            { address: first, count: 1 },
+        ])
+        await new Promise((resolve) => setTimeout(resolve, 50))
+        expect(executorService.stats()).toStrictEqual([
+            { address: second, count: 0 },
+            { address: first, count: 0 },
+        ])
+    })
+})
---
+++ packages/submitter/lib/services/ExecutorCacheService.ts
@@ -0,0 +1,110 @@
+import type { Account } from "viem/accounts"
+import { ExecutorHeap } from "#lib/utils/ExecutorHeap.ts"
+
+interface Expiry {
+    executor: { account: Account }
+    hashExpirations: Map<`0x${string}`, NodeJS.Timeout>
+}
+
+export class ExecutorCacheService {
+    constructor(
+        /**
+         * The time-to-live (TTL) for the cache in milliseconds. Default is 30 seconds.
+         * Once an executorAccount is fetched for a given userAccount+nonceTrack, this
+         * will remain the 'executor' for that combination until the TTL expires.
+         * 30 seconds should provide ample time for a transaction to be submitted.
+         *
+         * The TTL is refreshed every time the executor is fetched, which occurs during 'submit'
+         * so as long as the account submits a transaction within 30 seconds, the cache will always
+         * return the same executor for this account+nonceTrack
+         */
+        private ttl = 30_000,
+        public heap = new ExecutorHeap(),
+        private expiryMap = new Map<string, Expiry>(),
+    ) {}
+
+    /**
+     * Useful for debugging.
+     */
+    stats() {
+        return this.heap.values.map((h) => ({ address: h.account.address, count: h.jobCount }))
+    }
+
+    /**
+     * Registers a new Executor wallet onto the heap
+     */
+    registerExecutor(executor: Account): void {
+        this.heap.add(executor)
+    }
+
+    /**
+     * Returns the cached Executor for this account+nonceTrack combo. If there is no executor,
+     * this will find the least used executor and add it to the cache mapping. If there already is
+     * an executor it will return it, but first cancel the previous ttl, and create a new ttl
+     * starting now, resulting in a rolling ttl per-hash
+     */
+    get(hash: `0x${string}`, account: string, nonceTrack: bigint): Account {
+        const key = `${account}-${nonceTrack}`
+        const expiry = this.expiryMap.get(key)
+
+        if (!expiry) {
+            const executor = this.createExpiry(hash, key)
+            return executor
+        }
+
+        const executor = this.resetExpiry(hash, key, expiry)
+        return executor
+    }
+
+    private createExpiry(hash: `0x${string}`, key: string): Account {
+        // get the least used account
+        const executor = this.heap.peek()
+        // console.log({ executor })
+        if (!executor) throw new Error("No available executors")
+        this.heap.increment(executor.account.address)
+        const hashExpirations = new Map([
+            [
+                hash,
+                setTimeout(() => {
+                    this.heap.decrement(executor.account.address)
+                    this.expiryMap.get(key)?.hashExpirations.delete(hash)
+                    if (!this.expiryMap.get(key)?.hashExpirations.size) {
+                        this.expiryMap.delete(key)
+                    }
+                }, this.ttl),
+            ],
+        ])
+
+        this.expiryMap.set(key, {
+            executor: { account: executor.account },
+            hashExpirations,
+        })
+
+        return executor.account
+    }
+
+    private resetExpiry(hash: `0x${string}`, key: string, expiry: Expiry): Account {
+        // Check if the hash exists and reset its associated timeout
+        if (expiry.hashExpirations.has(hash)) {
+            const oldTimeoutId = expiry.hashExpirations.get(hash)!
+            clearTimeout(oldTimeoutId)
+        } else {
+            // If this hash is new, increment the job count
+            this.heap.increment(expiry.executor.account.address)
+        }
+
+        // Set a new timeout for this hash
+        const newTimeoutId = setTimeout(() => {
+            this.heap.decrement(expiry.executor.account.address)
+            expiry.hashExpirations.delete(hash)
+            if (!expiry.hashExpirations.size) {
+                this.expiryMap.delete(key)
+            }
+        }, this.ttl)
+
+        // Update the expiration entry for this hash
+        expiry.hashExpirations.set(hash, newTimeoutId)
+
+        return expiry.executor.account
+    }
+}
