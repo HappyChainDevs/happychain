786a1519581a42e55354354036d1ca540657e427
ultraviolet10 <aritrac1998@hotmail.com>
2025-04-16 18:33:42 +0530

    fix: fix abi decoded data display layout

--- apps/iframe/src/components/primitives/disclosure/variants.ts
+++ apps/iframe/src/components/primitives/disclosure/variants.ts
@@ -2,13 +2,13 @@ import { cva } from "class-variance-authority"
 
 /** Defines the container for a `<details>` element. */
 const recipeDisclosureDetails = cva(
-    "p-0 group cursor-pointer w-full focus-within:ring-2 rounded-lg items-center [&_svg]:open:-rotate-180 [&_svg]:transition",
+    "group w-full focus-within:ring-2 rounded-md items-center [&_svg]:open:-rotate-180 [&_svg]:transition border border-neutral/10",
     {
         variants: {
             intent: {
                 default: "bg-base-100",
-                gradient: "bg-gradient-to-br from-[rgb(56,189,248)] to-[rgb(109,40,217)] text-white shadow-lg",
-                neutral: "bg-base-content text-neutral shadow border border-neutral rounded-lg",
+                gradient: "bg-gradient-to-br from-[rgb(56,189,248)] to-[rgb(109,40,217)] text-white",
+                neutral: "bg-base-300/90 dark:bg-base-content text-neutral rounded-lg",
                 developerInfo: "",
             },
         },
@@ -19,7 +19,7 @@ const recipeDisclosureDetails = cva(
 )
 
 /** Defines the styles for the summary line for a `<details>` element. */
-const recipeDisclosureSummary = cva("list-none text-sm p-4 font-semibold flex justify-between cursor-pointer", {
+const recipeDisclosureSummary = cva("list-none max-w-prose text-sm py-2 px-2.5 flex justify-between mx-auto", {
     variants: {
         intent: {
             default: "",
@@ -32,12 +32,12 @@ const recipeDisclosureSummary = cva("list-none text-sm p-4 font-semibold flex ju
 })
 
 /** Defines the content area of a collapsible `<details>` element. */
-const recipeDisclosureContent = cva("px-4 py-2 text-xs overflow-x-auto group-open:overflow-x-auto", {
+const recipeDisclosureContent = cva("text-xs overflow-x-auto group-open:overflow-x-auto", {
     variants: {
         intent: {
             default: "",
             gradient: "",
-            neutral: "bg-base-content text-neutral border-t border-neutral shadow-sm rounded-b-lg",
+            neutral: "text-neutral rounded-b-lg",
         },
     },
     defaultVariants: {
