4b58b36d961eb23c3bbd3a874e209de9d20a12d0
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-04-21 05:16:36 +0200

    silence pesky annotations from ox+rollup

--- demos/js/vite.config.ts
+++ demos/js/vite.config.ts
@@ -4,4 +4,16 @@ import { defineConfig } from "vite"
 export default defineConfig({
     server: { port: 6001, strictPort: true },
     preview: { port: 6001, strictPort: true },
+    build: {
+        rollupOptions: {
+            onwarn(warning, defaultHandler) {
+                if (
+                    warning.code === "INVALID_ANNOTATION" &&
+                    warning.message.includes("contains an annotation that Rollup cannot interpret")
+                )
+                    return // silence pesky annotations from wevm/ox
+                defaultHandler(warning)
+            },
+        },
+    },
 })
--- demos/react/vite.config.ts
+++ demos/react/vite.config.ts
@@ -6,4 +6,16 @@ export default defineConfig({
     server: { port: 6002, strictPort: true },
     preview: { port: 6002, strictPort: true },
     plugins: [react()],
+    build: {
+        rollupOptions: {
+            onwarn(warning, defaultHandler) {
+                if (
+                    warning.code === "INVALID_ANNOTATION" &&
+                    warning.message.includes("contains an annotation that Rollup cannot interpret")
+                )
+                    return // silence pesky annotations from wevm/ox
+                defaultHandler(warning)
+            },
+        },
+    },
 })
