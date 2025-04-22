3f5d45ff4a54040650e4753c15d1688270d53039
ultraviolet10 <aritrac1998@hotmail.com>
2025-04-23 00:25:23 +0530

    access new icons from iframe deploy

---
+++ .changeset/lazy-shrimps-relate.md
@@ -0,0 +1,10 @@
+---
+"@happy.tech/common": patch
+"@happy.tech/core": patch
+"@happy.tech/iframe": patch
+"demo-react": patch
+"demo-vue": patch
+"demo-js": patch
+---
+
+Add new happychain icon
--- packages/core/lib/wallet/WalletFrame.tsx
+++ packages/core/lib/wallet/WalletFrame.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource preact */
-import { icon64x64 } from "@happy.tech/common"
 import { AuthState } from "@happy.tech/wallet-common"
+import { config } from "../config"
 import { useAnimatedStateTransitions } from "./hooks/useAnimatedStateTransitions"
 import { useAuthState } from "./hooks/useAuthState"
 import { useHappyUser } from "./hooks/useHappyUser"
@@ -41,7 +41,12 @@ export const WalletFrame = ({ dragging }: WalletFrameProps) => {
         >
             {showSpinner && <LoadingSpinner />}
             {/* Base64 to avoid any bundle issues and network requests */}
-            <img src={icon64x64} alt="HappyChain Logo" className="wallet-logo" inert={true} />
+            <img
+                src={`${config.iframePath}/images/happyIcon.png`}
+                alt="HappyChain Logo"
+                className="wallet-logo"
+                inert={true}
+            />
 
             <div className="wallet-iframe-wrapper" inert={!isOpen}>
                 <div ref={iframe} style="width: 100%; height: 100%;">
