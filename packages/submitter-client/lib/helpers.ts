185add4c416ff13ccaede7c994ee2a47f0c9ff5f
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-04-22 04:11:27 +0200

    eliminate helpers.ts

--- packages/submitter-client/lib/helpers.ts
+++
@@ -1,5 +0,0 @@
-import { computeBoopHash } from "@happy.tech/submitter/client"
-
-// == Utilities ====================================================================================
-
-export { computeBoopHash }
