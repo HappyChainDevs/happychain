410acb2fe2ececc436065849e675543329660ef7
not-reed <reed@happy.tech>
2025-04-23 13:25:20 -0700

    Add BatchCallExecutor and SessionKeyValidator to boop deployment

--- packages/submitter/lib/tests/setup.ts
+++ packages/submitter/lib/tests/setup.ts
@@ -1,6 +1,5 @@
 import { afterAll, beforeAll } from "bun:test"
 import "./env-bootstrap"
-import { sleep } from "bun"
 import { migrator } from "#lib/database/utils/migrator"
 import { anvil } from "./utils/anvil"
 import { contracts } from "./utils/contracts"
@@ -9,10 +8,14 @@ beforeAll(async () => {
     const { error } = await migrator.migrateToLatest()
     if (error) throw new Error("[Submitter] Failed to run test migrations", { cause: error })
 
+    /**
+     * This works great for running the tests, however
+     * it breaks when using --watch mode. to run watch mode successfully
+     * you need to run anvil as a separate service and deploy the
+     * contracts manually.
+     */
     await anvil.start()
-    await sleep(2_000)
     await contracts.deploy()
-    await sleep(2_000)
 })
 
 afterAll(async () => {
--- packages/submitter/lib/tests/utils/contracts.ts
+++ packages/submitter/lib/tests/utils/contracts.ts
@@ -1,37 +1,35 @@
 import { $ } from "bun"
-import { waitBlocks } from "./anvil"
+
+function parseContractAddresses(results: string): string {
+    return results
+        .split("\n")
+        .filter((a) => a.startsWith('  "'))
+        .map((a) => a.replace(/,$/g, ""))
+        .map((a) => {
+            const [name, address] = a.split(": ")
+            const nameStr = `${name.replace(/"/g, "").trim()}:`
+            return `    \x1b[36m${nameStr.padEnd(32, " ")}\x1b[0m ${address.replace(/"/g, "")}`
+        })
+        .join("\n")
+}
 
 async function deployBoopContracts(): Promise<void> {
-    console.log("Deploying Boop Contracts...")
+    console.log("🚧 Deploying Boop Contracts...")
     try {
         const results = await $`make -C ../../contracts deploy-boop`.text()
-        console.log(
-            results
-                .split("\n")
-                .filter((a) => a.startsWith('  "'))
-                .map((a) => a.replace(/,$/g, ""))
-                .join("\n"),
-        )
-        await waitBlocks(1)
-        console.log("Boop Contracts Deployed")
+        console.log(parseContractAddresses(results))
+        console.log("🚀 Boop Contracts Deployed")
     } catch (e) {
         throw new Error("Error deploying Boop contracts", { cause: e })
     }
 }
 
 async function deployMockContracts(): Promise<void> {
-    console.log("Deploying Mock Contracts...")
+    console.log("🚧 Deploying Mock Contracts...")
     try {
         const results = await $`make -C ../../contracts deploy-mocks`.text()
-        console.log(
-            results
-                .split("\n")
-                .filter((a) => a.startsWith('  "'))
-                .map((a) => a.replace(/,$/g, ""))
-                .join("\n"),
-        )
-        await waitBlocks(1)
-        console.log("Mock Contracts Deployed")
+        console.log(parseContractAddresses(results))
+        console.log("🚀 Mock Contracts Deployed")
     } catch (e) {
         throw new Error("Error deploying Mock contracts", { cause: e })
     }
@@ -39,7 +37,10 @@ async function deployMockContracts(): Promise<void> {
 
 export const contracts = {
     deploy: async () => {
+        console.log("")
         await deployBoopContracts()
+        console.log("")
         await deployMockContracts()
+        console.log("")
     },
 }
