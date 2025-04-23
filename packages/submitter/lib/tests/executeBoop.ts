aadb200ddc1add81a8f6dafc664a03b09d2679e2
aodhgan <gawnieg@gmail.com>
2025-04-23 15:51:39 -0700

    refactor

--- packages/submitter/lib/tests/executeBoop.ts
+++ packages/submitter/lib/tests/executeBoop.ts
@@ -1,23 +1,16 @@
-import { createMockTokenAMintHappyTx, signTx } from "./utils";
-import {privateKeyToAccount} from "viem/accounts"
+import { privateKeyToAccount } from "viem/accounts"
 import { createPublicClient, http, zeroAddress } from "viem";
 import { happychainTestnet } from "viem/chains";
-import {createAccount, execute, type Boop, computeBoopHash} from "@happy.tech/submitter-client"
-import { serializeBigInt } from "@happy.tech/submitter/client" 
-
+import { createAccount, execute, computeBoopHash } from "@happy.tech/submitter-client"
 import { abis, deployment } from "@happy.tech/contracts/boop/sepolia";
 
-const SUBMITTER_STAGING_URL = "https://submitter.happy.tech"
-
 const testAccount = privateKeyToAccount("0x8c5a8f60027c4a4654742cca624c7370599b0699dc142d44c9759e3040e201e3")
-
-
-const nonceTrack = 0n
 
 const publicClient = createPublicClient({
     chain: happychainTestnet,
     transport: http(),
 })
+
 async function getNonce(account: `0x${string}`, nonceTrack = 0n): Promise<bigint> {
     return await publicClient.readContract({
         address: deployment.EntryPoint,
@@ -26,35 +19,6 @@ async function getNonce(account: `0x${string}`, nonceTrack = 0n): Promise<bigint
         args: [account, nonceTrack],
     })
 }
-
-async function _createAccount(){
-    const res = await createAccount({
-        owner: testAccount.address,
-        salt: "0x01",
-    })
-    console.log("createAccount", res)
-    if (res.isOk()) {
-        return res.value.address
-    } 
-}
-
-async function _execute(tx: Boop){
-    const res = await execute({
-        tx
-    })
-    console.log("execute", res)
-}
-async function run(){
-    const account = await _createAccount()
-    const tx = await createAndSignMintTx(account!)
-    console.log("signed tx", tx)
-    const res = await _execute(tx)
-    console.log("execute", res)
-}
-run().then(() => {
-    console.log("done")
-})
-
 
 async function createAndSignMintTx(account: `0x${string}`){
     const unsignedTx = {
@@ -74,70 +38,30 @@ async function createAndSignMintTx(account: `0x${string}`){
         validatorData: "0x" as `0x${string}`,
         extraData: "0x" as `0x${string}`,
     }
-    console.log("unsignedTx", unsignedTx)
+    
     const boopHash = computeBoopHash(216n, unsignedTx)
-    console.log("boopHash", boopHash)
     const validatorData = await testAccount.signMessage({ message: { raw: boopHash } })
-    const signedTx = { ...unsignedTx, validatorData }
-    console.log("signedTx", signedTx)
-    return signedTx
+    return { ...unsignedTx, validatorData }
 }
-async function executeBoop(){
-    // create account by calling /api/v1/accounts/create
-    const accountCreationResult = await fetch(`${SUBMITTER_STAGING_URL}/api/v1/accounts/create`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ owner: testAccount.address, salt: "0x01" }),
-    })
-    const accountAddress = (await accountCreationResult.json()).address
-    console.log({accountAddress})
-    console.log("testAccount", testAccount.address)
-     
-    
-    for(let i = 0; i < 1; i++){
-        console.log("nonce value ", await getNonce(accountAddress))
 
-        const unsignedTx = await createMockTokenAMintHappyTx(accountAddress,
-            await getNonce(accountAddress), nonceTrack)
-        unsignedTx.dest = "0x02206faC6469B2f59FC2Bb9d3BC181Fbe703F8B7" // token A on testnet
-        unsignedTx.account = accountAddress
-        console.log("unsignedTx", unsignedTx)
-        console.log("calldata ", unsignedTx.callData)
-        
-        const signedTx = await signTx(testAccount, unsignedTx)
-        console.log("signedTx", signedTx)   
-        
-        
-        console.time("submitter")
-        const result = await fetch(`${SUBMITTER_STAGING_URL}/api/v1/submitter/execute`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ tx: serializeBigInt(signedTx) }),
+async function run(){
+    const createAccountResult = await createAccount({
+        owner: testAccount.address,
+        salt: "0x01",
+    })
+    if (createAccountResult.isOk()) {
+        const tx = await createAndSignMintTx(createAccountResult.value.address)
+        const executeRes = await execute({
+            tx
         })
-        console.timeEnd("submitter")
-        // console.log(result)
-        // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-        const response = (await result.json()) as any
-        console.log("result", response.status)
-        // console.log(response.state.receipt.happyTxHash)
-        // console.time("receipt")
-        // // get receipt
-        // const receipt = await fetch(`https://submitter.happy.tech/api/v1/submitter/receipt/${response.state.receipt.happyTxHash}`, {
-        //     method: "GET",
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //     },
-        // })
-        // const receiptJson = await receipt.json()
-        // console.log(receiptJson)
-        // console.timeEnd("receipt")
-    }
-    
+        if (executeRes.isOk()) {
+            console.log("txHash", executeRes.value)
+        } else {
+            throw new Error(executeRes.error.message)
+        }
+    }     
 }
-// executeBoop().then(() => {
-//     console.log("done")
-// })
+
+run().then(() => {
+    console.log("done")
+})
