b3c3d34f1c6a6cbbd370d789a5c8eef11c3b01d9
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-04-23 06:57:12 +0200

    setup a request logger

---
+++ apps/iframe/src/logger.ts
@@ -0,0 +1,18 @@
+import { LogLevel, Logger, logLevel } from "@happy.tech/common"
+
+/**
+ * Default at which all logger's log level are set by default if not changed programmatically.
+ * Initialized to the env var VITE_LOG_LEVEL if set, otherwise {@link LogLevel.WARN}.
+ */
+export const defaultLogLevel = logLevel(import.meta.env.VITE_LOG_LEVEL)
+
+/**
+ * Default logger instance. Also accessible in the browser console via `window.happyLogger`.
+ */
+export const logger = Logger.instance
+logger.setLogLevel(defaultLogLevel)
+
+/**
+ * Logger facade for tracing requests.
+ */
+export const reqLogger = Logger.create("requests")
