e40e5e903a5634a42bcd4fa1808fc1f24ece6918
aodhgan <gawnieg@gmail.com>
2025-04-23 17:10:12 -0700

    rm funded pk

--- packages/submitter-client/test/executeBoop.ts
+++ packages/submitter-client/test/executeBoop.ts
@@ -1,16 +1,11 @@
 import { abis, deployment } from "@happy.tech/contracts/boop/sepolia"
 import { deployment as mockDeployments } from "@happy.tech/contracts/mocks/sepolia"
 import { http, createPublicClient, zeroAddress } from "viem"
-import { privateKeyToAccount } from "viem/accounts"
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
 import { happychainTestnet } from "viem/chains"
 import { computeBoopHash, createAccount, execute } from "../lib/index"
 
-const pk = process.env.SUBMITTER_CRON_PK
-
-if (!pk) {
-    throw new Error("SUBMITTER_CRON_PK is not set – did you add the secret to the workflow?")
-}
-
+const pk = generatePrivateKey()
 const testAccount = privateKeyToAccount(pk as `0x${string}`)
 
 const publicClient = createPublicClient({
@@ -42,7 +37,7 @@ async function createAndSignMintTx(account: `0x${string}`) {
         maxFeePerGas: 1200000000n,
         submitterFee: 100n,
         callData:
-            "0x40c10f19000000000000000000000000d224f746ed779fd492ccadae5cd377e58ee811810000000000000000000000000000000000000000000000000de0b6b3a7640000" as `0x${string}`, // mint some number of tokens to 0xd224f746ed779fd492ccadae5cd377e58ee81181
+            "0x40c10f19000000000000000000000000d224f746ed779fd492ccadae5cd377e58ee811810000000000000000000000000000000000000000000000000de0b6b3a7640000" as `0x${string}`, // mint 1 token to 0xd224f746ed779fd492ccadae5cd377e58ee81181
         validatorData: "0x" as `0x${string}`,
         extraData: "0x" as `0x${string}`,
     }
