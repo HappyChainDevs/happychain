9d3dc3ac8aca269df930bf83c21f0ec904657ced
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-04-26 01:47:42 +0200

    fix build

--- apps/iframe/src/connections/boopAccount.ts
+++ apps/iframe/src/connections/boopAccount.ts
@@ -1,11 +1,11 @@
-import { createAccount } from "@happy.tech/submitter-client/lib"
 import type { Address } from "viem"
+import { boopClient } from "#src/state/boopClient"
 
 export async function getBoopAccountAddress(owner: Address): Promise<Address> {
     const salt = "0x1"
     try {
         // TODO is this fast enough for login?
-        const result = await createAccount({
+        const result = await boopClient.createAccount({
             // already verifies if the account is created under the hood
             owner,
             salt,
