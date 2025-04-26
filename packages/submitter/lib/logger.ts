8417727583041f9bdac0746b3a3219fe3f4c0258
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-04-27 00:12:58 +0200

    submitter pkg: use regular log level vars, improve .env usage

--- packages/submitter/lib/logger.ts
+++ packages/submitter/lib/logger.ts
@@ -1,15 +1,4 @@
-import { LogLevel, Logger } from "@happy.tech/common"
+import { LogLevel, Logger, logLevel } from "@happy.tech/common"
 import { env } from "./env"
 
-const logLevel: LogLevel =
-    env.NODE_ENV === "test"
-        ? LogLevel.WARN
-        : {
-              off: LogLevel.OFF,
-              trace: LogLevel.TRACE,
-              info: LogLevel.INFO,
-              warn: LogLevel.WARN,
-              error: LogLevel.ERROR,
-          }[env.LOG_LEVEL]
-
-export const logger = Logger.create("Submitter", logLevel)
+export const logger = Logger.create("Submitter", env.NODE_ENV === "test" ? LogLevel.WARN : logLevel(env.NODE_ENV))
