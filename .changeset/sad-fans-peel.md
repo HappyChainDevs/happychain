09f0482c35bf73ced35dd730bb5cf69925c18c23
ultraviolet10 <aritrac1998@hotmail.com>
2025-04-27 20:25:55 +0530

    pr updates

--- .changeset/sad-fans-peel.md
+++
@@ -1,7 +0,0 @@
----
-"@happy.tech/submitter": patch
-"@happy.tech/boop-sdk": patch
-"@happy.tech/common": patch
----
-
-Move bigint helper from submitter-client to common package.
--- support/common/lib/utils/bigint.ts
+++ support/common/lib/utils/bigint.ts
@@ -1,3 +1,5 @@
+import type { Prettify } from "viem"
+
 /**
  * Returns the maximum value among the given BigInt arguments.
  * @param values - One or more BigInt values to compare.
@@ -37,11 +39,13 @@ export function bigIntToZeroPadded(value: bigint, totalDigits: number): string {
 }
 
 // Utility type to transform all bigint fields to string
-type ReplaceBigIntWithString<T> = {
+type ReplaceBigIntWithString<T> = Prettify<{
     [K in keyof T]: T[K] extends bigint ? string : T[K] extends object ? ReplaceBigIntWithString<T[K]> : T[K]
-}
+}>
 
-// Utility functions to serialize and deserialize bigint values
+/**
+ * Utility functions to serialize and deserialize bigint values
+ */
 export function serializeBigInt<T>(obj: T): ReplaceBigIntWithString<T> {
     if (typeof obj === "bigint") {
         return obj.toString() as ReplaceBigIntWithString<T>
