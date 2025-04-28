bb06859f99af497e645b07173b27585d3b1b84e5
not-reed <reed@happy.tech>
2025-04-28 13:21:05 -0700

    Add getNonce method to BoopClient and related account nonce functionality

--- packages/boop-sdk/build.config.ts
+++ packages/boop-sdk/build.config.ts
@@ -1,10 +1,14 @@
+import { deployment } from "@happy.tech/contracts/boop/sepolia"
 import { defineConfig } from "@happy.tech/happybuild"
 
 export default defineConfig({
     bunConfig: {
         define: {
+            // Production Values -
             "import.meta.env": JSON.stringify({
-                SUBMITTER_URL: process.env.SUBMITTER_URL,
+                SUBMITTER_URL: process.env.SUBMITTER_URL || "https://submitter.happy.tech",
+                RPC_URL: process.env.RPC_URL || "https://rpc.testnet.happy.tech",
+                ENTRYPOINT: deployment.EntryPoint,
             }),
         },
     },
