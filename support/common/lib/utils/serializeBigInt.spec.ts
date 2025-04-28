97e7b67e4ea28037c9ea2f3c411f433717450eda
ultraviolet10 <aritrac1998@hotmail.com>
2025-04-28 11:39:30 +0530

    update test description

--- support/common/lib/utils/serializeBigInt.spec.ts
+++ support/common/lib/utils/serializeBigInt.spec.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from "bun:test"
 import { serializeBigInt } from "./bigint"
 
 describe("bigint.lossy", () => {
-    it("converts bigints in object to strings", () => {
+    it("serializes bigint values to strings recursively in nested objects", () => {
         const before = { num: 1n, nested: { deeper: 2n } }
         const after = { num: "1", nested: { deeper: "2" } }
         expect(serializeBigInt(before)).toStrictEqual(after)
