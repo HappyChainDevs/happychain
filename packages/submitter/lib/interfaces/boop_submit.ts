6768eb85912f1a9c7c6324bbc4e794ff311c89e1
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-04-29 09:51:44 +0200

    move old endpoint docs to boop-sdk — not perfect but at least it's in the right place

--- packages/submitter/lib/interfaces/boop_submit.ts
+++ packages/submitter/lib/interfaces/boop_submit.ts
@@ -72,30 +72,7 @@ export type SubmitError = {
     description?: string
 }
 
-/**
- * POST `/api/v1/boop/submit`
- *
- * Given a Boop, sends it to the submitter which will either accept it and return its hash,
- * or fail with a rejection status.
- *
- * The submitter is nonce-aware and will buffer up to a certain amount of boop per nonce track,
- * depending on its configuration. It will submit boops whenever their nonces becomes eligible.
- *
- * The submitter will then attempt to submit the transaction onchain. The state of the Boop
- * can be queried with `submitter_state`.
- *
- * If the gas limits are provided, the submitter is free to perform or not perform simulation before
- * submitting.
- *
- * If the submitter already has a pending Boop with the same nonce for this account, it will
- * cancel the existing one on the condition that the new Boop passes validation. It can also
- * impose additional restrictions, such as requesting a higher submitterFee for the replacement
- * transaction.
- *
- * To cancel a pending Boop, simply call with an empty replacement transaction.
- */
-export declare function submitter_submit(input: Boop): SubmitOutput
-
+// TODO implement cancel
 export enum SubmitCancelStatus {
     /** The cancellation was successfully submitted — it can still fail. */
     Success = "executeCancelSuccess",
