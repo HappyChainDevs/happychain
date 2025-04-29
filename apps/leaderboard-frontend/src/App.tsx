8e6986e285658efb25e0ebb4815537ab29ec8908
Aryan Godara <aryangodara03@gmail.com>
2025-04-29 16:42:49 +0530

    remove tailwind from leaderboard-frontend, keep codebase minimal[skip ci]

--- apps/leaderboard-frontend/src/App.tsx
+++ apps/leaderboard-frontend/src/App.tsx
@@ -1,14 +1,11 @@
-import GameBreakdown from "./components/GameBreakdown"
-import LeaderBoard from "./components/LeaderBoard"
+import WalletConnect from "./components/WalletConnect"
 
 function App() {
     return (
-        <div className="min-h-dvh w-full flex flex-col bg-[url('/cat-frolicking.jpeg')] bg-center bg-no-repeat bg-contain">
-            <main className="flex-1 flex flex-col items-center justify-center">
-                <LeaderBoard />
-                <GameBreakdown />
-            </main>
-        </div>
+        <main className="app-root">
+            <WalletConnect />
+            <main className="main-content">{/* Main content */}</main>
+        </main>
     )
 }
 
