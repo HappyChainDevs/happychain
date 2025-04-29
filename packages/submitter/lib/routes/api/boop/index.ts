b2d60492dd81a346e08355a179aabf3708d850ee
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-04-29 09:36:09 +0200

    new makeResponse & HTTP code selection

--- packages/submitter/lib/routes/api/boop/index.ts
+++ packages/submitter/lib/routes/api/boop/index.ts
@@ -1,32 +1,18 @@
-import { serializeBigInt } from "@happy.tech/common"
 import { Hono } from "hono"
-import type { Result } from "neverthrow"
 import type { PendingBoopOutput, StateRequestOutput } from "#lib/client"
-import { HappyBaseError } from "#lib/errors"
-import { executeFromRoute } from "#lib/handlers/boop/execute"
+import { execute } from "#lib/handlers/boop/execute"
 import { pendingByAccount } from "#lib/handlers/boop/pendingByAccount"
 import { receiptByHash } from "#lib/handlers/boop/receiptByHash"
-import { simulateFromRoute } from "#lib/handlers/boop/simulate"
+import { simulate } from "#lib/handlers/boop/simulate"
 import { stateByHash } from "#lib/handlers/boop/stateByHash"
-import { submitFromRoute } from "#lib/handlers/boop/submit"
+import { submit } from "#lib/handlers/boop/submit"
+import { makeResponse, makeResponseOld } from "#lib/routes/api/makeResponse"
 import * as executeRoute from "./openApi/execute"
 import * as pendingByAccountRoute from "./openApi/pendingByAccount"
 import * as receiptByHashRoute from "./openApi/receiptByHash"
 import * as simulationRoute from "./openApi/simulate"
 import * as stateByHashRoute from "./openApi/stateByHash"
 import * as submitRoute from "./openApi/submit"
-
-// TODO: reintroduce this, switching on .status, instead of our per-route `fromRoute` functions
-function makeResponse<TOk>(output: Result<TOk, unknown>) {
-    if (output.isOk()) return [serializeBigInt(output.value), 200] as const
-
-    const error =
-        output.error instanceof HappyBaseError //
-            ? output.error.getResponseData()
-            : output.error // unknown error
-
-    return [serializeBigInt(error), 422] as const
-}
 
 export default new Hono()
     .post(
@@ -35,7 +21,7 @@ export default new Hono()
         simulationRoute.validation,
         async (c) => {
             const input = c.req.valid("json")
-            const [response, code] = await simulateFromRoute(input)
+            const [response, code] = makeResponse(await simulate(input))
             return c.json(response, code)
         },
     )
@@ -45,7 +31,7 @@ export default new Hono()
         submitRoute.validation,
         async (c) => {
             const input = c.req.valid("json")
-            const [response, code] = await submitFromRoute(input)
+            const [response, code] = makeResponse(await submit(input))
             return c.json(response, code)
         },
     )
@@ -55,7 +41,7 @@ export default new Hono()
         executeRoute.validation,
         async (c) => {
             const input = c.req.valid("json")
-            const [response, code] = await executeFromRoute(input)
+            const [response, code] = makeResponse(await execute(input))
             return c.json(response, code)
         },
     )
@@ -66,7 +52,7 @@ export default new Hono()
         async (c) => {
             const input = c.req.valid("param")
             const output = await stateByHash(input)
-            const [response, code] = makeResponse<StateRequestOutput>(output)
+            const [response, code] = makeResponseOld<StateRequestOutput>(output)
             return c.json(response, code)
         },
     )
@@ -79,7 +65,7 @@ export default new Hono()
             const { hash } = c.req.valid("param")
             const { timeout } = c.req.valid("query")
             const output = await receiptByHash({ hash, timeout })
-            const [response, code] = makeResponse<StateRequestOutput>(output)
+            const [response, code] = makeResponseOld<StateRequestOutput>(output)
             return c.json(response, code)
         },
     )
@@ -90,7 +76,7 @@ export default new Hono()
         async (c) => {
             const input = c.req.valid("param")
             const output = await pendingByAccount(input)
-            const [response, code] = makeResponse<PendingBoopOutput>(output)
+            const [response, code] = makeResponseOld<PendingBoopOutput>(output)
             return c.json(response, code)
         },
     )
