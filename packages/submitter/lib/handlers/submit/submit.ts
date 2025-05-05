fd7a2cd770aee04ea4c7631d2e2c32db9b8acee8
not-reed <reed@happy.tech>
2025-05-05 12:54:12 -0700

    Simplify submit flow and improve out-of-gas detection in submitter package

--- packages/submitter/lib/handlers/submit/submit.ts
+++ packages/submitter/lib/handlers/submit/submit.ts
@@ -1,10 +1,11 @@
+import type { Address, Hex } from "@happy.tech/common"
 import { abis, deployment, env } from "#lib/env"
 import { outputForGenericError } from "#lib/handlers/errors"
-import { simulate } from "#lib/handlers/simulate"
+import { type SimulateOutput, type SimulateSuccess, simulate } from "#lib/handlers/simulate"
 import { boopNonceManager, submitterService } from "#lib/services"
 import { computeBoopHash } from "#lib/services/computeBoopHash"
 import { findExecutionAccount } from "#lib/services/evmAccounts"
-import { type Boop, Onchain, SubmitterError } from "#lib/types"
+import { type Boop, Onchain } from "#lib/types"
 import { encodeBoop } from "#lib/utils/boop/encodeBoop"
 import { walletClient } from "#lib/utils/clients"
 import { logger } from "#lib/utils/logger"
@@ -20,14 +21,9 @@ export async function submit(input: SubmitInput): Promise<SubmitOutput> {
         // Save original boop to the database for historic purposes and data recovery.
         await submitterService.add(entryPoint, boop, boopHash)
 
-        let simulation = await simulate(input, true)
+        const simulation = await simulate(input, true)
 
-        if (simulation.status !== Onchain.Success) {
-            return {
-                ...simulation,
-                stage: "simulate",
-            }
-        }
+        if (simulation.status !== Onchain.Success) return { ...simulation, stage: "simulate" }
 
         if (simulation.validityUnknownDuringSimulation || simulation.paymentValidityUnknownDuringSimulation) {
             return {
@@ -37,67 +33,21 @@ export async function submit(input: SubmitInput): Promise<SubmitOutput> {
             }
         }
 
-        // If future nonce, wait until ready.
-        const isFutureNonce = simulation.futureNonceDuringSimulation
-        if (isFutureNonce && (await boopNonceManager.checkIfBlocked(entryPoint, boop))) {
-            logger.trace("boop has future nonce, waiting until it becomes unblocked", boopHash)
-            const resp = await boopNonceManager.pauseUntilUnblocked(entryPoint, boop)
-            logger.trace("boop unblocked", boopHash)
-            if (resp.isErr())
-                return {
-                    // TODO pauseUntilBlocked should provide this object
-                    status: SubmitterError.UnexpectedError,
-                    description: resp.error.message,
-                    stage: "submit",
-                }
-            simulation = await simulate(input) // update simulation
-            if (simulation.status !== Onchain.Success) return { ...simulation, stage: "simulate" }
+        let isBlocked: boolean
+        if (simulation.futureNonceDuringSimulation) {
+            const _isBlocked = await boopNonceManager.checkIfBlocked(entryPoint, boop)
+            if (_isBlocked.isErr()) return { ..._isBlocked.error, stage: "submit" }
+            isBlocked = _isBlocked.value
+        } else {
+            isBlocked = false
         }
 
-        const account = findExecutionAccount(input.boop)
-        logger.trace("Submitting to the chain using execution account", account.address, boopHash)
+        submitWhenPossible(simulation, entryPoint, boop, boopHash, input, isBlocked)
 
-        // TODO make simulate return an updated boop
-        const updatedBoop = {
-            ...input.boop,
-            gasLimit: simulation.gas, // TODO should probably be lower than the gas limit
-            validateGasLimit: simulation.validateGas,
-            validatePaymentGasLimit: simulation.validatePaymentGas,
-            executeGasLimit: simulation.executeGas,
-            maxFeePerGas: simulation.maxFeePerGas,
-            submitterFee: simulation.submitterFee,
-        } satisfies Boop
-
-        // TODO make sure this does no extra needless simulations
-        const txHash = await walletClient.writeContract({
-            address: input.entryPoint ?? deployment.EntryPoint,
-            args: [encodeBoop(updatedBoop)],
-            abi: abis.EntryPoint,
-            functionName: "submit",
-            gas: BigInt(simulation.gas),
-            maxFeePerGas: simulation.maxFeePerGas,
-            maxPriorityFeePerGas: 1n,
-            account,
-        })
-        logger.trace("Successfully submitted", boopHash, txHash)
-
-        boopNonceManager.incrementLocalNonce(boop)
-        // TODO save gas values
-        // TODO don't monitor unless asked
-        submitterService.monitorReceipt(boop, txHash)
-
-        // TODO we need to return way before!
         return {
             status: Onchain.Success,
             hash: boopHash,
-            txHash,
             entryPoint: input.entryPoint ?? deployment.EntryPoint,
-            gasLimit: updatedBoop.gasLimit,
-            validateGasLimit: updatedBoop.validateGasLimit,
-            validatePaymentGasLimit: updatedBoop.validatePaymentGasLimit,
-            executeGasLimit: updatedBoop.executeGasLimit,
-            maxFeePerGas: updatedBoop.maxFeePerGas,
-            submitterFee: updatedBoop.submitterFee,
         }
     } catch (error) {
         return {
@@ -105,4 +55,57 @@ export async function submit(input: SubmitInput): Promise<SubmitOutput> {
             stage: "submit",
         }
     }
+}
+
+async function submitWhenPossible(
+    _simulation: SimulateSuccess,
+    entryPoint: Address,
+    boop: Boop,
+    boopHash: Hex,
+    input: SubmitInput,
+    isBlocked: boolean,
+) {
+    let simulation: SimulateOutput = _simulation
+
+    // If future nonce, wait until ready.
+    if (isBlocked) {
+        logger.trace("boop has future nonce, waiting until it becomes unblocked", boopHash)
+        const resp = await boopNonceManager.pauseUntilUnblocked(entryPoint, boop)
+        logger.trace("boop unblocked", boopHash)
+        if (resp.isErr()) return resp.error
+        simulation = await simulate(input) // update simulation
+        if (simulation.status !== Onchain.Success) return { ...simulation, stage: "simulate" }
+    }
+
+    const account = findExecutionAccount(input.boop)
+    logger.trace("Submitting to the chain using execution account", account.address, boopHash)
+
+    // TODO make simulate return an updated boop
+    const updatedBoop = {
+        ...input.boop,
+        gasLimit: simulation.gas, // TODO should probably be lower than the gas limit
+        validateGasLimit: simulation.validateGas,
+        validatePaymentGasLimit: simulation.validatePaymentGas,
+        executeGasLimit: simulation.executeGas,
+        maxFeePerGas: simulation.maxFeePerGas,
+        submitterFee: simulation.submitterFee,
+    } satisfies Boop
+
+    // TODO make sure this does no extra needless simulations
+    const txHash = await walletClient.writeContract({
+        address: input.entryPoint ?? deployment.EntryPoint,
+        args: [encodeBoop(updatedBoop)],
+        abi: abis.EntryPoint,
+        functionName: "submit",
+        gas: BigInt(simulation.gas),
+        maxFeePerGas: simulation.maxFeePerGas,
+        maxPriorityFeePerGas: 1n,
+        account,
+    })
+    logger.trace("Successfully submitted", boopHash, txHash)
+
+    boopNonceManager.incrementLocalNonce(boop)
+    // TODO save gas values
+    // TODO don't monitor unless asked
+    submitterService.monitorReceipt(boop, txHash)
 }
