3daf0e65460b16edd129560df7f43b798c9b7104
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-05-05 03:10:59 +0200

    properly attribute logs to their boops

--- packages/submitter/lib/services/BoopReceiptService.ts
+++ packages/submitter/lib/services/BoopReceiptService.ts
@@ -1,8 +1,11 @@
-import type { Hash } from "@happy.tech/common"
+import type { Hash, Hex } from "@happy.tech/common"
+import { type Boop, computeBoopHash } from "#lib/client"
 import type { BoopReceiptRepository } from "#lib/database/repositories/BoopReceiptRepository"
+import { deployment, env } from "#lib/env"
 import { SubmitterError } from "#lib/errors"
-import type { BoopReceipt, OnchainStatus, Receipt, TransactionTypeName } from "#lib/types"
+import type { BoopReceipt, Log, OnchainStatus, Receipt, TransactionTypeName } from "#lib/types"
 import { publicClient } from "#lib/utils/clients"
+import { decodeEvent, getSelectorFromEventName } from "#lib/utils/parsing"
 
 export class BoopReceiptService {
     constructor(private boopReceiptRepository: BoopReceiptRepository) {}
@@ -19,7 +22,7 @@ export class BoopReceiptService {
             nonceTrack: boopReceipt.nonceTrack,
             nonceValue: boopReceipt.nonceValue,
             entryPoint: boopReceipt.entryPoint,
-            logs: transactionReceipt.logs.filter((l) => l.address === boopReceipt.entryPoint),
+            logs: this.#filterLogs(transactionReceipt.logs, boopHash),
             revertData: boopReceipt.revertData,
             gasUsed: boopReceipt.gasUsed,
             gasCost: boopReceipt.gasCost,
@@ -29,6 +32,33 @@ export class BoopReceiptService {
                 contractAddress: transactionReceipt.contractAddress || null,
             } satisfies Receipt,
         }
+    }
+
+    readonly #boopExecutionStartedSelector = getSelectorFromEventName("BoopExecutionStarted") as Hex
+    readonly #boopSubmittedSelector = getSelectorFromEventName("BoopSubmitted") as Hex
+
+    #filterLogs(logs: Log[], boopHash: Hash): Log[] {
+        let select = false
+        const filteredLogs: Log[] = []
+        for (const log of logs) {
+            const fromEntryPoint = log.address.toLowerCase() === deployment.EntryPoint.toLowerCase()
+            if (fromEntryPoint && log.topics[0] === this.#boopSubmittedSelector) {
+                const decodedLog = decodeEvent(log)
+                if (!decodedLog) throw new Error("Found BoopSumitted event but could not decode")
+                const decodedHash = computeBoopHash(env.CHAIN_ID, decodedLog.args as Boop)
+                if (decodedHash === boopHash) {
+                    return filteredLogs
+                } else {
+                    select = false
+                    filteredLogs.length = 0
+                }
+            } else if (select) {
+                filteredLogs.push(log)
+            } else if (fromEntryPoint && log.topics[0] === this.#boopExecutionStartedSelector) {
+                select = true
+            }
+        }
+        return []
     }
 
     async findByBoopHashWithTimeout(boopHash: Hash, timeout: number, pollInterval = 250) {
