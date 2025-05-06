2c6c06cb7877578087c32feb0475121d964dccf4
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-05-07 01:18:38 +0200

    remove unused chains definitions, closes HAPPY-438

--- support/wallet-common/lib/chains/definitions/README.md
+++ support/wallet-common/lib/chains/definitions/README.md
@@ -1,3 +1,1 @@
-### Note
-
 This directory contains chain definitions in a format suitable to pass to the `wallet_addEthereumChain` JSON-RPC method.
--- support/wallet-common/lib/chains/definitions/index.ts
+++ support/wallet-common/lib/chains/definitions/index.ts
@@ -1,15 +1,10 @@
-// Internal use only
 import { anvilDefinition } from "./anvil"
-import { ethereumDefinition } from "./ethereum"
 import { happyChainSepoliaDefinition } from "./happyChainSepolia"
-// export { happyChainSepoliaDefinition as happyChainSepolia }
-// export const defaultChain = happyChainSepoliaDefinition
 
 export const chainDefinitions = {
     defaultChain: happyChainSepoliaDefinition,
 
     // Internal use only
-    ethereum: ethereumDefinition,
     devnet: anvilDefinition,
     from: happyChainSepoliaDefinition,
     happyChainSepolia: happyChainSepoliaDefinition,
