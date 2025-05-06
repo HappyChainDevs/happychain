3ca322bec869d16955f12e2b380ed7966e91c896
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-05-07 00:43:13 +0200

    improve the eth_getTransaction logic

--- apps/iframe/src/requests/utils/boopCache.ts
+++ apps/iframe/src/requests/utils/boopCache.ts
@@ -7,5 +7,28 @@ export type BoopCacheEntry = {
     receipt?: BoopReceipt
 }
 
+class BoopCache extends FIFOCache<Hash, BoopCacheEntry> {
+    putBoop(hash: Hash, boop: Boop): BoopCacheEntry {
+        let entry = this.get(hash)
+        if (entry) {
+            entry.boop = boop
+        } else {
+            entry = { boop }
+            this.put(hash, entry)
+        }
+        return entry
+    }
+    putReceipt(hash: Hash, receipt: BoopReceipt): BoopCacheEntry {
+        let entry = this.get(hash)
+        if (entry) {
+            entry.receipt = receipt
+        } else {
+            entry = { receipt }
+            this.put(hash, entry)
+        }
+        return entry
+    }
+}
+
 /** Cache Boop receipts - store both the receipt & original transaction */
-export const boopCache = new FIFOCache<Hash, BoopCacheEntry>(100)
+export const boopCache = new BoopCache(100)
