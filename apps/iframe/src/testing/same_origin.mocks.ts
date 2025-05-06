1a12c85982d03d3ef8386e68149542df7544e157
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-05-07 01:13:32 +0200

    fix iframe tests

--- apps/iframe/src/testing/same_origin.mocks.ts
+++ apps/iframe/src/testing/same_origin.mocks.ts
@@ -2,22 +2,17 @@ import { type UUID, createUUID } from "@happy.tech/common"
 import type { AppURL } from "#src/utils/appURL"
 
 export const appURL = "http://localhost:4321" as AppURL
+export const iframeID = createUUID()
 
 export const appURLMock = async () => ({
     getAppURL: () => appURL,
     getIframeURL: () => appURL,
+    iframeID: () => iframeID,
     isApp: (_app: AppURL) => true,
     isIframe: (_app: AppURL) => true,
     isStandaloneIframe: () => true,
+    appForSourceID(sourceId: UUID): AppURL | undefined {
+        if (sourceId === iframeID) return appURL
+        return undefined
+    },
 })
-
-export const iframeID = createUUID()
-
-export const requestUtilsMock = (importUtils: () => Promise<typeof import("#src/utils/appURL")>) =>
-    importUtils().then((utils) => ({
-        ...utils,
-        appForSourceID(sourceId: UUID): AppURL | undefined {
-            if (sourceId === iframeID) return appURL
-            return undefined
-        },
-    }))
