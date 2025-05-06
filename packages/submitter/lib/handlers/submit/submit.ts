bae479fb438fedcc5eed7ca0916b0675dd2fd0b8
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-05-07 01:41:59 +0200

    reunite functions

--- packages/submitter/lib/handlers/submit/submit.ts
+++ packages/submitter/lib/handlers/submit/submit.ts
@@ -1,7 +1,6 @@
-import type { Address, Hex } from "@happy.tech/common"
 import { abis, deployment, env } from "#lib/env"
 import { outputForGenericError } from "#lib/handlers/errors"
-import { type SimulateOutput, type SimulateSuccess, simulate } from "#lib/handlers/simulate"
+import { simulate } from "#lib/handlers/simulate"
 import { boopNonceManager, submitterService } from "#lib/services"
 import { computeBoopHash } from "#lib/services/computeBoopHash"
 import { findExecutionAccount } from "#lib/services/evmAccounts"
@@ -20,7 +19,7 @@ export async function submit(input: SubmitInput): Promise<SubmitOutput> {
         // Save original boop to the database for historic purposes and data recovery.
         await submitterService.add(entryPoint, boop, boopHash)
 
-        const simulation = await simulate(input, true)
+        let simulation = await simulate(input, true)
 
         if (simulation.status !== Onchain.Success) return { ...simulation, stage: "simulate" }
 
@@ -32,16 +31,50 @@ export async function submit(input: SubmitInput): Promise<SubmitOutput> {
             }
         }
 
-        let isBlocked: boolean
         if (simulation.futureNonceDuringSimulation) {
-            const _isBlocked = await boopNonceManager.checkIfBlocked(entryPoint, boop)
-            if (_isBlocked.isErr()) return { ..._isBlocked.error, stage: "submit" }
-            isBlocked = _isBlocked.value
-        } else {
-            isBlocked = false
+            const isBlocked = await boopNonceManager.checkIfBlocked(entryPoint, boop)
+            if (isBlocked.isErr()) return { ...isBlocked.error, stage: "submit" }
+            // If future nonce, wait until ready.
+            if (isBlocked.value) {
+                logger.trace("boop has future nonce, waiting until it becomes unblocked", boopHash)
+                const resp = await boopNonceManager.pauseUntilUnblocked(entryPoint, boop)
+                logger.trace("boop unblocked", boopHash)
+                if (resp.isErr()) return resp.error
+                simulation = await simulate(input) // update simulation
+                if (simulation.status !== Onchain.Success) return { ...simulation, stage: "simulate" }
+            }
         }
 
-        submitWhenPossible(simulation, entryPoint, boop, boopHash, input, isBlocked)
+        const account = findExecutionAccount(input.boop)
+        logger.trace("Submitting to the chain using execution account", account.address, boopHash)
+
+        // TODO make simulate return an updated boop
+        const updatedBoop = {
+            ...input.boop,
+            gasLimit: simulation.gas, // TODO should probably be lower than the gas limit
+            validateGasLimit: simulation.validateGas,
+            validatePaymentGasLimit: simulation.validatePaymentGas,
+            executeGasLimit: simulation.executeGas,
+            maxFeePerGas: simulation.maxFeePerGas,
+            submitterFee: simulation.submitterFee,
+        } satisfies Boop
+
+        // TODO make sure this does no extra needless simulations
+        const txHash = await walletClient.writeContract({
+            address: input.entryPoint ?? deployment.EntryPoint,
+            args: [encodeBoop(updatedBoop)],
+            abi: abis.EntryPoint,
+            functionName: "submit",
+            gas: BigInt(simulation.gas),
+            maxFeePerGas: simulation.maxFeePerGas,
+            maxPriorityFeePerGas: 1n,
+            account,
+        })
+        logger.trace("Successfully submitted", boopHash, txHash)
+
+        boopNonceManager.incrementLocalNonce(boop)
+        // TODO don't monitor unless asked
+        submitterService.monitorReceipt(boop, txHash)
 
         return {
             status: Onchain.Success,
@@ -54,57 +87,4 @@ export async function submit(input: SubmitInput): Promise<SubmitOutput> {
             stage: "submit",
         }
     }
-}
-
-async function submitWhenPossible(
-    _simulation: SimulateSuccess,
-    entryPoint: Address,
-    boop: Boop,
-    boopHash: Hex,
-    input: SubmitInput,
-    isBlocked: boolean,
-) {
-    let simulation: SimulateOutput = _simulation
-
-    // If future nonce, wait until ready.
-    if (isBlocked) {
-        logger.trace("boop has future nonce, waiting until it becomes unblocked", boopHash)
-        const resp = await boopNonceManager.pauseUntilUnblocked(entryPoint, boop)
-        logger.trace("boop unblocked", boopHash)
-        if (resp.isErr()) return resp.error
-        simulation = await simulate(input) // update simulation
-        if (simulation.status !== Onchain.Success) return { ...simulation, stage: "simulate" }
-    }
-
-    const account = findExecutionAccount(input.boop)
-    logger.trace("Submitting to the chain using execution account", account.address, boopHash)
-
-    // TODO make simulate return an updated boop
-    const updatedBoop = {
-        ...input.boop,
-        gasLimit: simulation.gas, // TODO should probably be lower than the gas limit
-        validateGasLimit: simulation.validateGas,
-        validatePaymentGasLimit: simulation.validatePaymentGas,
-        executeGasLimit: simulation.executeGas,
-        maxFeePerGas: simulation.maxFeePerGas,
-        submitterFee: simulation.submitterFee,
-    } satisfies Boop
-
-    // TODO make sure this does no extra needless simulations
-    const txHash = await walletClient.writeContract({
-        address: input.entryPoint ?? deployment.EntryPoint,
-        args: [encodeBoop(updatedBoop)],
-        abi: abis.EntryPoint,
-        functionName: "submit",
-        gas: BigInt(simulation.gas),
-        maxFeePerGas: simulation.maxFeePerGas,
-        maxPriorityFeePerGas: 1n,
-        account,
-    })
-    logger.trace("Successfully submitted", boopHash, txHash)
-
-    boopNonceManager.incrementLocalNonce(boop)
-    // TODO save gas values
-    // TODO don't monitor unless asked
-    submitterService.monitorReceipt(boop, txHash)
 }
