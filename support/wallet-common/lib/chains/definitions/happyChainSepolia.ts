a6815ac6f8faa59bcb4d03c7a783daec07d6fbc9
norswap <norswap@gmail.com>
2025-05-10 01:12:41 +0200

    Apply suggestions from code review
    add comments

--- support/wallet-common/lib/chains/definitions/happyChainSepolia.ts
+++ support/wallet-common/lib/chains/definitions/happyChainSepolia.ts
@@ -4,7 +4,7 @@ export const happyChainSepoliaDefinition = {
     chainName: "HappyChain Sepolia",
     rpcUrls: ["https://rpc.testnet.happy.tech/http", "wss://rpc.testnet.happy.tech/ws"],
     nativeCurrency: { name: "HappyChain", symbol: "HAPPY", decimals: 18 },
-    chainId: "0xd8", // 216
+    chainId: "0xd8", // 216 — must be lowercased to enable comparison with chainId from Viem's `happychainTestnet` import
     blockExplorerUrls: ["https://explorer.testnet.happy.tech"],
     opStack: true,
 } as const satisfies ChainParameters
