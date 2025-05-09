a1f8493e9984323c6e8e1b6225d540ac6e20c5ea
norswap <norswap@gmail.com>
2025-05-10 01:12:41 +0200

    Apply suggestions from code review
    add comments

--- support/wallet-common/lib/chains/definitions/anvil.ts
+++ support/wallet-common/lib/chains/definitions/anvil.ts
@@ -4,5 +4,5 @@ export const anvilDefinition = {
     chainName: "Anvil",
     rpcUrls: ["http://127.0.0.1:8545", "ws://127.0.0.1:8545"],
     nativeCurrency: { decimals: 18, name: "Ether", symbol: "ETH" },
-    chainId: "0x7a69", // 31337
+    chainId: "0x7a69", // 31337 — must be lowercased to enable comparison with chainId from Viem's `anvil` import
 } satisfies ChainParameters
