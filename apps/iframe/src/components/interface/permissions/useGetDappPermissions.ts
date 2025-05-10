5c610ff4a311a154c1639b411dda7a670f642e70
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-05-10 18:54:21 +0200

    remove useless getDappPermissions

--- apps/iframe/src/components/interface/permissions/useGetDappPermissions.ts
+++
@@ -1,8 +0,0 @@
-import { type AppPermissions, getAppPermissions } from "#src/state/permissions"
-import type { AppURL } from "#src/utils/appURL"
-
-function useGetDappPermissions(dappUrl: AppURL): AppPermissions {
-    return getAppPermissions(dappUrl)
-}
-
-export { useGetDappPermissions }
--- apps/iframe/src/routes/embed/permissions.$dappId.lazy.tsx
+++ apps/iframe/src/routes/embed/permissions.$dappId.lazy.tsx
@@ -1,7 +1,7 @@
 import { createLazyFileRoute, useParams } from "@tanstack/react-router"
 import { ClearAllPermissions } from "#src/components/interface/permissions/ClearAllPermissions"
 import { ListDappPermissions } from "#src/components/interface/permissions/ListPermissions"
-import { useGetDappPermissions } from "#src/components/interface/permissions/useGetDappPermissions"
+import { getAppPermissions } from "#src/state/permissions"
 import type { AppURL } from "#src/utils/appURL"
 
 export const Route = createLazyFileRoute("/embed/permissions/$dappId")({
@@ -9,20 +9,22 @@ export const Route = createLazyFileRoute("/embed/permissions/$dappId")({
 })
 
 function DappPermissions() {
-    const dappUrl = useParams({
+    const appURL = useParams({
         from: "/embed/permissions/$dappId",
         select: (params) => decodeURI(params.dappId),
     })
-    const listAppPermissions = useGetDappPermissions(dappUrl as AppURL)
+    // Very purposefully not reactive: when revoking a permission, it will show as toggled off instead of vanishing,
+    // and can be toggle back on while we don't navigate away.
+    const listAppPermissions = getAppPermissions(appURL as AppURL)
     return (
         <div className="bg-base-100 pb-14">
             <div className="animate-appear">
                 <h2 className="sticky top-0 text-center bg-base-200 text-base-content font-bold p-1 text-sm">
-                    {dappUrl}
+                    {appURL}
                 </h2>
                 <p className="sr-only">Access and change the permissions of all dApps you interacted with.</p>
-                <ListDappPermissions dappUrl={dappUrl as AppURL} items={listAppPermissions} />
-                {Object.keys(listAppPermissions).length > 0 && <ClearAllPermissions url={dappUrl as AppURL} />}
+                <ListDappPermissions dappUrl={appURL as AppURL} items={listAppPermissions} />
+                {Object.keys(listAppPermissions).length > 0 && <ClearAllPermissions url={appURL as AppURL} />}
             </div>
         </div>
     )
