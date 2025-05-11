e135b315066167773693587f5a150e7ae362e782
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-05-11 08:57:50 +0200

    make simulation cache key on entrypoint too

--- apps/submitter/lib/handlers/waitForReceipt/waitForReceipt.ts
+++ apps/submitter/lib/handlers/waitForReceipt/waitForReceipt.ts
@@ -1,15 +1,17 @@
-import { env } from "#lib/env"
+import { deployment, env } from "#lib/env"
 import { boopReceiptService, simulationCache } from "#lib/services"
 import { SubmitterError } from "#lib/types"
 import { WaitForReceipt, type WaitForReceiptInput, type WaitForReceiptOutput } from "./types"
 
-export async function waitForReceipt({ hash, timeout }: WaitForReceiptInput): Promise<WaitForReceiptOutput> {
+export async function waitForReceipt(input: WaitForReceiptInput): Promise<WaitForReceiptOutput> {
+    const { hash, timeout } = input
+    const entryPoint = input.entryPoint ?? deployment.EntryPoint
     // TODO this needs a try-catch for proper error handling, and probably the services need to be more aware of their own errors
 
     const receipt = await boopReceiptService.find(hash, timeout ?? env.RECEIPT_TIMEOUT)
     if (receipt) return { status: WaitForReceipt.Success, receipt }
 
-    const simulation = await simulationCache.findSimulation(hash)
+    const simulation = await simulationCache.findSimulation(entryPoint, hash)
     if (simulation)
         return {
             status: SubmitterError.ReceiptTimeout,
