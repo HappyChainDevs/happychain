377aa2f7b97d269c54830aa414f41c81298f01c8
ultraviolet10 <aritrac1998@hotmail.com>
2025-05-12 17:55:18 +0530

    clean up demo scaffold

--- demos/react/src/demo-components/CallBatchDemo.tsx
+++ demos/react/src/demo-components/CallBatchDemo.tsx
@@ -1,34 +1,53 @@
+import { useHappyWallet } from "@happy.tech/react"
+import { parseEther } from "viem"
+import { walletClient } from "../clients"
+
 const CallBatchDemo = () => {
+    const { user } = useHappyWallet()
     async function getCapabilities() {
-        // const caps = await walletClient?.getCapabilities()
+        try {
+            const caps = await walletClient?.getCapabilities()
+            console.log({ caps })
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     async function sendBatch() {
-        // const bundles = await walletClient?.sendCalls()
-    }
-
-    async function getCallsStatus() {
-        // const stat = await walletClient?.getCallsStatus()
+        try {
+            const batchCalls = await walletClient?.sendCalls({
+                account: user?.address,
+                calls: [
+                    {
+                        to: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
+                        value: parseEther("1"),
+                    },
+                ],
+            })
+            console.log(batchCalls)
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     return (
         <div className="grid grid-cols-2 gap-4 backdrop-blur-sm bg-gray-200/35 p-4 rounded-lg">
             <div className="text-lg font-bold col-span-2">EIP-5792</div>
 
-            <button type="button" onClick={getCapabilities} className="rounded-lg bg-sky-300 p-2 shadow-xl font-mono">
+            <button
+                type="button"
+                onClick={getCapabilities}
+                className="rounded-lg bg-sky-300 p-2 shadow-xl font-mono truncate"
+            >
                 wallet_getCapabilities
             </button>
 
             <button
                 type="button"
                 onClick={sendBatch}
-                className="rounded-lg bg-sky-300 p-2 shadow-xl text-center font-mono"
+                className="rounded-lg bg-sky-300 p-2 shadow-xl text-center font-mono truncate"
             >
                 wallet_sendCalls
-            </button>
-
-            <button type="button" onClick={getCallsStatus} className="rounded-lg bg-sky-300 p-2 shadow-xl font-mono">
-                wallet_getCallsStatus
             </button>
         </div>
     )
