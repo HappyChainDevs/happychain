a1a9d503149a7d1d546bc6cd81357054b88b60e4
GabrielMartinezRodriguez <gabrielmartinezrdrgz@gmail.com>
2025-05-12 12:41:11 +0200

    chore(faucet): remove ms dependecy and create our own formatMs function

---
+++ support/common/lib/utils/time.ts
@@ -0,0 +1,18 @@
+const unit = {
+    ms: 1,
+    s: 1e3,
+    m: 60e3,
+    h: 3_600e3,
+    d: 86_400e3,
+  } as const;
+  
+export function formatMs(ms: number, long = false): string {
+    const abs = Math.abs(ms);
+    for (const [abbr, size] of Object.entries(unit).reverse()) {
+        if (abs >= size) {
+        const val = Math.round(ms / size);
+        return long ? `${val} ${abbr}${Math.abs(val) !== 1 ? 's' : ''}` : `${val}${abbr}`;
+        }
+    }
+    return `${ms}ms`;
+}
