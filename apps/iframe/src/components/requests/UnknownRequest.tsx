6b600ff76978556863d05d1d022bb4085eb2f188
ultraviolet10 <aritrac1998@hotmail.com>
2025-05-13 17:04:52 +0530

    feat: add component to handle unsupported request methods

---
+++ apps/iframe/src/components/requests/UnknownRequest.tsx
@@ -0,0 +1,29 @@
+import DisclosureSection from "./common/DisclosureSection"
+import { FormattedDetailsLine, Layout } from "./common/Layout"
+
+interface UnknownRequestProps {
+    method: string
+    params: unknown
+    reject: () => void
+}
+
+export const UnknownRequest = ({ method, params, reject }: UnknownRequestProps) => {
+    return (
+        <Layout
+            headline={<span className="text-error italic font-mono">{method}</span>}
+            description={<>This request method isn't supported by the HappyWallet.</>}
+            actions={{
+                reject: {
+                    children: "Go back",
+                    onClick: reject,
+                },
+            }}
+        >
+            <DisclosureSection title="Raw Request" isOpen>
+                <div className="grid gap-4 p-2">
+                    <FormattedDetailsLine isCode>{JSON.stringify({ method, params }, null, 2)}</FormattedDetailsLine>
+                </div>
+            </DisclosureSection>
+        </Layout>
+    )
+}
