65e01e3f6798c62211944df1b97502fc4d257198
not-reed <reed@happy.tech>
2025-05-13 12:12:15 -0700

    Refactor wagmi connection handling in setUserWithProvider

--- apps/iframe/src/actions/setUserWithProvider.ts
+++ apps/iframe/src/actions/setUserWithProvider.ts
@@ -2,16 +2,34 @@ import type { HappyUser } from "@happy.tech/wallet-common"
 import { AuthState } from "@happy.tech/wallet-common"
 import { getDefaultStore } from "jotai"
 import type { EIP1193Provider } from "viem"
+import { connectWagmi, disconnectWagmi } from "#src/wagmi/utils"
 import { authStateAtom } from "../state/authState"
 import { providerAtom } from "../state/provider"
 import { userAtom } from "../state/user"
 
 const store = getDefaultStore()
 
-export function setUserWithProvider(user: HappyUser | undefined, provider: EIP1193Provider | undefined) {
+/**
+ * Set the current user details along with the EIP 1193 Provider & auth state.
+ *
+ * If both user and provider are undefined, the intent is assumed to be a logout, and
+ * we will additionally disconnect the internal wagmi provider before clearing the state.
+ * If both are provided, the intent is assumed to be a login, and we will connect the internal wagmi
+ * provider once the jotai atoms state is set.
+ */
+export async function setUserWithProvider(user: undefined, provider: undefined): Promise<void>
+export async function setUserWithProvider(user: HappyUser, provider: EIP1193Provider): Promise<void>
+export async function setUserWithProvider(user: HappyUser | undefined, provider: EIP1193Provider | undefined) {
+    const isConnecting = Boolean(user && provider)
+
+    if (!isConnecting) await disconnectWagmi()
+
     store.set(providerAtom, provider)
     store.set(userAtom, user)
 
     // user auth state
-    store.set(authStateAtom, () => (user ? AuthState.Connected : AuthState.Disconnected))
+    store.set(authStateAtom, () => (isConnecting ? AuthState.Connected : AuthState.Disconnected))
+
+    // if wagmi wasn't previously successfully connected, this throws
+    if (isConnecting) await connectWagmi()
 }
