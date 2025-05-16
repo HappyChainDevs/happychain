79f87d18770111b043416bc9f5cc1cbaf78b0743
ultraviolet10 <aritrac1998@hotmail.com>
2025-05-16 16:03:31 +0530

    remove unneeded return

--- apps/iframe/src/requests/utils/sessionKeys.ts
+++ apps/iframe/src/requests/utils/sessionKeys.ts
@@ -182,7 +182,7 @@ export async function installNewSessionKey(app: AppURL, account: Address, target
     } else {
         await installSessionKeyExtension(account, target, sessionAccount.address)
     }
-    return authorizeSessionKey(app, account, target, sessionKey)
+    authorizeSessionKey(app, account, target, sessionKey)
 }
 
 /**
