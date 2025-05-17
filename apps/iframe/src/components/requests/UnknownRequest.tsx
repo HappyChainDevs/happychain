4afeb57dcdeddc3d36c51b6b122227dcaab0e25c
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-05-17 02:49:46 +0200

    lil makeover

--- apps/iframe/src/components/requests/UnknownRequest.tsx
+++ apps/iframe/src/components/requests/UnknownRequest.tsx
@@ -1,5 +1,6 @@
 import DisclosureSection from "./common/DisclosureSection"
 import { FormattedDetailsLine, Layout } from "./common/Layout"
+import { SectionError } from "./common/SectionError"
 
 interface UnknownRequestProps<T = unknown> {
     method: string
@@ -9,17 +10,9 @@ interface UnknownRequestProps<T = unknown> {
 
 export const UnknownRequest = ({ method, params, reject }: UnknownRequestProps) => {
     return (
-        <Layout
-            headline={<span className="text-error italic font-mono">{method}</span>}
-            description={<>This request method isn't supported by the HappyWallet.</>}
-            actions={{
-                reject: {
-                    children: "Go back",
-                    onClick: reject,
-                },
-            }}
-        >
-            <DisclosureSection title="Raw Request" isOpen>
+        <Layout headline={method} description="" actions={{ reject: { children: "Go back", onClick: reject } }}>
+            <SectionError>This request method is unknown or isn't supported by the Happy Wallet.</SectionError>
+            <DisclosureSection title="Raw Request">
                 <div className="grid gap-4 p-2">
                     <FormattedDetailsLine isCode>
                         {JSON.stringify({ method, params: params ?? null }, null, 2)}
