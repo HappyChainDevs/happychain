97de7a04aa6ad41a97096aa9ba41dbb9cd01db77
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-05-18 17:03:46 +0200

    no more ManagerService, rename env vars

--- apps/submitter/lib/env/schemas/limits.ts
+++ apps/submitter/lib/env/schemas/limits.ts
@@ -5,14 +5,32 @@ import { z } from "zod"
  */
 export const limitsSchema = z.object({
     /**
-     * Maximum number of boops that be queued up for a specific (account, nonceTrack) pair. Defaults to 50.
+     * Default timeout for waiting for receipts in milliseconds. Defaults to 8 seconds.
      */
-    LIMITS_EXECUTE_BUFFER_LIMIT: z.coerce.number().positive().default(50),
+    RECEIPT_TIMEOUT: z.coerce.number().positive().default(8000),
 
     /**
-     * Maximum number of boops that be queued up in the submitter accross all accounts. Defaults to 10_000.
+     * Max timeout that caller can specify for the `boop/receipt` route.
      */
-    LIMITS_EXECUTE_MAX_CAPACITY: z.coerce.number().positive().default(10_000),
+    MAX_RECEIPT_TIMEOUT: z.coerce.number().positive().default(8000),
+
+    /**
+     * Maximum number of boops that be queued up for a specific (account, nonceTrack) pair while waiting for boops
+     * with lower nonces to be submitted. Defaults to 50.
+     */
+    MAX_PENDING_PER_TRACK: z.coerce.number().positive().default(50),
+
+    /**
+     * Maximum number of boops that be queued up in the submitter accross all accounts
+     * while waiting for boops with lower nonces to be submitted. Defaults to 10_000.
+     */
+    MAX_TOTAL_PENDING: z.coerce.number().positive().default(10_000),
+
+    /**
+     * The maximum amount of time in milliseconds a transaction can remain pending before before being submitted
+     * (i.e. waiting for previous transactions to be submitted or a nonce gap to close). Defaults to 30 seconds.
+     */
+    MAX_SUBMIT_PENDING_TIME: z.coerce.number().positive().default(30_000),
 
     /**
      * The amount of simulation results cached in memory. This is used to supply a useful
@@ -25,20 +43,4 @@ export const limitsSchema = z.object({
      * simulation cache, in milliseconds. Defaults to 30 seconds.
      */
     SIMULATION_CACHE_TTL: z.coerce.number().positive().default(30_000),
-
-    /**
-     * The maximum amount of time in milliseconds a transaction can remain pending before before being
-     * submitted (i.e. waiting for previous transactions to be submitted or a nonce gap to close).
-     */
-    MAX_SUBMIT_PENDING_TIME: z.coerce.number().positive().default(30_000),
-
-    /**
-     * Default timeout for waiting for receipts in milliseconds. Defaults to 8 seconds.
-     */
-    RECEIPT_TIMEOUT: z.coerce.number().positive().default(8000),
-
-    /**
-     * Max timeout that caller can specify for the `boop/receipt` route.
-     */
-    MAX_RECEIPT_TIMEOUT: z.coerce.number().positive().default(8000),
 })
