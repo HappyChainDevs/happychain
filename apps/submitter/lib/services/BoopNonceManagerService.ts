4f53039293d39864942c2fae164e51b56c948e33
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-05-18 16:41:39 +0200

    simplify limits checking, removing complications from possible nonce refetch

--- apps/submitter/lib/services/BoopNonceManagerService.ts
+++ apps/submitter/lib/services/BoopNonceManagerService.ts
@@ -12,8 +12,6 @@ type NonceTrack = bigint
 type NonceValue = bigint
 type BlockedBoop = { boopHash: Hash; resolve: (value: undefined | SubmitError) => void }
 
-const NONCE_BUFFER_LIMIT = BigInt(env.LIMITS_EXECUTE_BUFFER_LIMIT)
-
 export class BoopNonceManagerService {
     #usedCapacity: bigint
     readonly #nonceMutexes: Map2<Address, NonceTrack, Mutex>
@@ -57,25 +55,19 @@ export class BoopNonceManagerService {
 
     async waitUntilUnblocked(entryPoint: Address, boop: Boop): Promise<undefined | SubmitError> {
         const { account, nonceTrack, nonceValue } = boop
-        let localNonce = await this.#getLocalNonce(entryPoint, account, nonceTrack)
+        const localNonce = await this.#getLocalNonce(entryPoint, account, nonceTrack)
 
         // Not actually blocked, proceed immediately.
         if (nonceValue === localNonce) return
 
         // Check if we can wait for the nonce.
-        if (this.#trackExceedsBuffer(boop))
+        const trackSize = this.#pendingBoopsMap.get(boop.account, boop.nonceTrack)?.size ?? 0
+        if (trackSize >= env.LIMITS_EXECUTE_BUFFER_LIMIT)
             return this.#makeSubmitError("Buffer full for this (account, nonceTrack) pair. Try again later.")
         if (this.#usedCapacity >= env.LIMITS_EXECUTE_MAX_CAPACITY)
             return this.#makeSubmitError("The submitter has reached its maximum capacity. Try again later.")
-        if (await this.#nonceOutOfRange(entryPoint, boop))
+        if (nonceValue >= localNonce + BigInt(env.LIMITS_EXECUTE_BUFFER_LIMIT))
             return this.#makeSubmitError("The supplied nonce is too far ahead of the current non value.")
-
-        // Check if it is still blocked — we have to check because of the
-        // previous await, otherwise we might wait and never get unblocked.
-        // TODO make this nicer?
-        // localNonce = await this.#getLocalNonce(entryPoint, account, nonceTrack)
-        localNonce = this.#nonces.get(account, nonceTrack) ?? localNonce
-        if (nonceValue === localNonce) return
 
         // If an old boop exists for th enonce, signal that it has been replaced.
         const previouslyBlocked = this.#pendingBoopsMap.get(account, nonceTrack)?.get(nonceValue)
@@ -101,23 +93,6 @@ export class BoopNonceManagerService {
 
         this.#usedCapacity++
         return await promise
-    }
-
-    #trackExceedsBuffer(boop: Boop) {
-        const track = this.#pendingBoopsMap.get(boop.account, boop.nonceTrack)
-        if (!track) return false
-        return track.size >= env.LIMITS_EXECUTE_BUFFER_LIMIT
-    }
-
-    async #nonceOutOfRange(entrypoint: `0x${string}`, boop: Boop) {
-        const possiblyCachedNonce = await this.#getLocalNonce(entrypoint, boop.account, boop.nonceTrack)
-        if (boop.nonceValue <= possiblyCachedNonce + NONCE_BUFFER_LIMIT) return false
-        // reset to force onchain lookup, abort if still out of range
-        logger.trace("Nonce out of range, forcing onchain re-lookup", computeHash(boop))
-        // TODO think harder about what the consequences of this are
-        this.resetLocalNonce(boop)
-        const onchainNonce = await this.#getLocalNonce(entrypoint, boop.account, boop.nonceTrack)
-        return boop.nonceValue > onchainNonce + NONCE_BUFFER_LIMIT
     }
 
     #removeNonce(account: Address, nonceTrack: bigint, nonceValue: bigint) {
