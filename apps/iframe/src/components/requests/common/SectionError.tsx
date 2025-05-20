933594d7a34814027a12c0da06652dd77788d11c
ultraviolet10 <aritrac1998@hotmail.com>
2025-05-20 14:58:28 +0530

    Inline error message into popup sections instead of changing the entire layout

--- apps/iframe/src/components/requests/common/SectionError.tsx
+++ apps/iframe/src/components/requests/common/SectionError.tsx
@@ -5,15 +5,15 @@ import { SectionBlock } from "#src/components/requests/common/Layout"
 export function SectionError({ children, className }: PropsWithChildren<{ className?: string }>) {
     return (
         <SectionBlock>
-            <div
+            <section
                 className={cx(
                     "grid border bg-error/40 border-error dark:bg-error/5 dark:border-error/20",
-                    "text-error-content/90 dark:text-error gap-2 text-sm py-[1em] px-[1.25em] rounded-lg w-full",
+                    "text-error-content/90 dark:text-error gap-2 text-sm py-2 px-2.5 rounded-lg w-full text-start",
                     className,
                 )}
             >
                 {children}
-            </div>
+            </section>
         </SectionBlock>
     )
 }
