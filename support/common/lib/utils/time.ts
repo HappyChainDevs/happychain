d7a60667985666dfb52fd5b6fee614ec52e526c3
not-reed <reed@happy.tech>
2025-05-21 08:33:27 -0700

    Fix code style and reorder imports

--- support/common/lib/utils/time.ts
+++ support/common/lib/utils/time.ts
@@ -4,15 +4,15 @@ const unit = {
     m: 60e3,
     h: 3_600e3,
     d: 86_400e3,
-  } as const;
-  
+} as const
+
 export function formatMs(ms: number, long = false): string {
-    const abs = Math.abs(ms);
+    const abs = Math.abs(ms)
     for (const [abbr, size] of Object.entries(unit).reverse()) {
         if (abs >= size) {
-        const val = Math.round(ms / size);
-        return long ? `${val} ${abbr}${Math.abs(val) !== 1 ? 's' : ''}` : `${val}${abbr}`;
+            const val = Math.round(ms / size)
+            return long ? `${val} ${abbr}${Math.abs(val) !== 1 ? "s" : ""}` : `${val}${abbr}`
         }
     }
-    return `${ms}ms`;
+    return `${ms}ms`
 }
