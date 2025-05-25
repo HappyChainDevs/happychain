ef1d45d6e0d94fab2b2353eca6040c6ddc323e0b
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-05-25 11:01:42 +0200

    canonicalize RPC URLs

--- apps/submitter/lib/utils/clients.ts
+++ apps/submitter/lib/utils/clients.ts
@@ -10,11 +10,23 @@ import { anvil, happychainTestnet } from "viem/chains"
 import { env } from "#lib/env"
 import { logger } from "#lib/utils/logger"
 
+function canonicalize(type: "http" | "ws", rpcs: string[]): string[] {
+    return uniques(
+        rpcs.map((rpc) => {
+            let result = rpc.trim().replace("127.0.0.1", "localhost")
+            if (!rpc.startsWith("http") && !rpc.startsWith("ws")) result = `${type}://${result}`
+            if (result.endsWith("/")) result = result.slice(0, result.length - 1)
+            if (result === "https://rpc.testnet.happy.tech") result = `${result}/http`
+            return result
+        }),
+    )
+}
+
 function getChain(): Chain {
     const chain = [anvil, happychainTestnet].find((chain) => chain.id === env.CHAIN_ID)
     if (chain) {
-        const http = uniques(array(env.RPC_HTTP_URL, ...chain.rpcUrls.default.http))
-        const webSocket = uniques(array(env.RPC_WS_URL, ...chain.rpcUrls.default.webSocket))
+        const http = canonicalize("http", array(env.RPC_HTTP_URL, ...chain.rpcUrls.default.http))
+        const webSocket = canonicalize("ws", array(env.RPC_WS_URL, ...chain.rpcUrls.default.webSocket))
         return { ...chain, rpcUrls: { default: { http, webSocket } } }
     }
     if (!env.RPC_HTTP_URL) {
