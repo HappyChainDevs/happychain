f1097054a919bbe6f6fff912186d8e787f2858e8
ultraviolet10 <aritrac1998@hotmail.com>
2025-05-26 13:58:39 +0530

    refactor address import in ImportTokensDialog and improve comment clarity in address utility

--- support/common/lib/utils/address.ts
+++ support/common/lib/utils/address.ts
@@ -4,7 +4,7 @@ import type { Address } from "./types"
 /**
  * Often used in conjunction with transforms/toAddress, this viem wrapper
  * makes for a simple zod refinement. The option { strict: false } ensures
- * this uses the same position in the internal LRU cache viem implements
+ * this uses the same position in the internal LRU cache that viem implements
  * for address validation with getAddress.
  */
 export function isAddress(str: string): str is Address {
