d2000378a78df536b031c960bce3d30f783ab0a0
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-06-04 06:38:32 +0200

    sort pending boops

--- apps/submitter/lib/handlers/getPending/getPending.ts
+++ apps/submitter/lib/handlers/getPending/getPending.ts
@@ -4,17 +4,22 @@ import { GetPending, type GetPendingInput, type GetPendingOutput, type PendingBo
 
 export async function getPending({ account }: GetPendingInput): Promise<GetPendingOutput> {
     try {
-        const pending = boopStore.getByAccount(account).map((boop) => {
-            const boopHash = computeHash(boop)
-            return {
-                boopHash,
-                entryPoint: boopStore.getEntryPoint(boopHash)!,
-                nonceTrack: boop.nonceTrack,
-                nonceValue: boop.nonceValue,
-                // If the boop isn't blocked but is in the store, it must have been submitted (or it is about to be).
-                submitted: !boopNonceManager.has(boop),
-            } satisfies PendingBoopInfo
-        })
+        const pending = boopStore
+            .getByAccount(account)
+            .map((boop) => {
+                const boopHash = computeHash(boop)
+                return {
+                    boopHash,
+                    entryPoint: boopStore.getEntryPoint(boopHash)!,
+                    nonceTrack: boop.nonceTrack,
+                    nonceValue: boop.nonceValue,
+                    // If the boop isn't blocked but is in the store, it must have been submitted (or it is about to be).
+                    submitted: !boopNonceManager.has(boop),
+                } satisfies PendingBoopInfo
+            })
+            .sort((a, b) => {
+                return Math.sign(Number(a.nonceTrack - b.nonceTrack)) || Math.sign(Number(a.nonceValue - b.nonceValue))
+            })
         return { status: GetPending.Success, account, pending }
     } catch (error) {
         return outputForGenericError(error)
