d7ea45dab140522e366634bd636fc6a11a4225dd
Aryan Godara <aryangodara03@gmail.com>
2025-06-05 20:35:00 +0530

    chore: minor cleanup

--- apps/submitter/lib/server/boopRoute.ts
+++ apps/submitter/lib/server/boopRoute.ts
@@ -34,7 +34,6 @@ export default new Hono()
         async (c) => {
             const input = c.req.valid("json")
             const output = await simulate(input)
-            // validateOutput(output, simulateOutputValidation, "simulate response")
             const [body, code] = makeResponse(output)
             validateOutput(body, simulateOutputValidation, "simulate response")
             return c.json(body, code)
