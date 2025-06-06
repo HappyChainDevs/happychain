ed241b39047c039d4d187a12765c273a3566dafa
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-06-06 23:54:00 +0200

    suppress receipt not found errors

--- apps/submitter/lib/handlers/createAccount/createAccount.ts
+++ apps/submitter/lib/handlers/createAccount/createAccount.ts
@@ -72,8 +72,12 @@ async function waitForTransactionReceipt(hash: `0x${string}`): Promise<Transacti
     const timeout = setTimeout(() => reject(new Error("Account creation timed out")), 8_000)
 
     const unwatch = BlockService.instance.onBlock(async (_block) => {
-        const receipt: TransactionReceipt = await publicClient.getTransactionReceipt({ hash })
-        if (receipt) resolve(receipt)
+        try {
+            const receipt: TransactionReceipt = await publicClient.getTransactionReceipt({ hash })
+            if (receipt) resolve(receipt)
+        } catch (_) {
+            // This errors when the receipt is not found — ignore the error.
+        }
     })
 
     try {
