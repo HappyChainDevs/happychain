4edb0e5fe5d0e81fa8b7ff04111617e8b8b3e863
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-06-07 18:28:25 +0200

    lazy & error utils

--- support/common/lib/utils/error.ts
+++ support/common/lib/utils/error.ts
@@ -1,3 +1,42 @@
+import { type Lazy, force } from "./functions"
+import type { UnionFill } from "./types"
+
 export function unknownToError(u: unknown): Error {
     return u instanceof Error ? u : new Error(JSON.stringify(u, null, 2))
+}
+
+export type Result<T, E> = UnionFill<{ result: T } | { error: E }>
+
+/**
+ * Returns `{ result: fn(), error: undefined }` if no exception is
+ * thrown, and `{ error, result: undefined }` otherwise.
+ */
+export function tryCatch<T, E = unknown>(fn: () => T): Result<T, E> {
+    // biome-ignore format: terse
+    try { return { result: fn() } }
+    catch (e) { return { error: e as E } }
+}
+
+/**
+ * Returns `{ result: fn(), error: undefined }` if no exception is
+ * thrown, and `{ error, result: undefined }` otherwise.
+ */
+export async function tryCatchAsync<T, E = unknown>(promise: Lazy<Promise<T>>): Promise<Result<T, E>> {
+    // biome-ignore format: terse
+    try { return { result: await force(promise) } }
+    catch (e) { return { error: e as E } }
+}
+
+/** Returns `fn()` if no exception is thrown, undefined otherwise. */
+export function tryCatchU<T>(fn: () => T): T | undefined {
+    // biome-ignore format: terse
+    try { return fn() }
+    catch { return undefined }
+}
+
+/** Returns `fn()` if no exception is thrown, undefined otherwise. */
+export async function tryCatchAsyncU<T>(promise: Lazy<Promise<T>>): Promise<T | undefined> {
+    // biome-ignore format: terse
+    try { return await force(promise) }
+    catch { return undefined }
 }
