2cd9c4bf3d6616926bb5a1bf1e98c8ca6de6e927
GabrielMartinezRodriguez <gabrielmartinezrdrgz@gmail.com>
2025-06-09 15:16:41 +0200

    chore(submitter): format

--- apps/submitter/lib/telemetry/instrumentation.ts
+++ apps/submitter/lib/telemetry/instrumentation.ts
@@ -1,10 +1,10 @@
+import { trace } from "@opentelemetry/api"
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http/build/esnext"
 import { Resource } from "@opentelemetry/resources"
 import { NodeSDK } from "@opentelemetry/sdk-node"
 import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from "@opentelemetry/semantic-conventions"
 import pkg from "../../package.json" assert { type: "json" }
 import { env } from "../env"
-import { trace } from "@opentelemetry/api"
 
 export const serviceName = `submitter-${env.NODE_ENV}`
 
@@ -14,7 +14,6 @@ const resource = Resource.default().merge(
         [ATTR_SERVICE_VERSION]: pkg.version,
     }),
 )
-
 
 const traceExporter = env.TRACES_ENDPOINT
     ? new OTLPTraceExporter({
