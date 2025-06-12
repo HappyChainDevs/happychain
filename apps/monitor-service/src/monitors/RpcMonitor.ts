3071f1b182271f15f97ca6b6e1ab1837273835d6
GabrielMartinezRodriguez <gabrielmartinezrdrgz@gmail.com>
2025-06-12 12:01:09 +0200

    chore: format

--- apps/monitor-service/src/monitors/RpcMonitor.ts
+++ apps/monitor-service/src/monitors/RpcMonitor.ts
@@ -63,7 +63,7 @@ export class RpcMonitor {
                     changedAt: new Date(),
                     unhealthyAt: undefined,
                     healthyAt: undefined,
-                }
+                },
             }
 
             this.rpcClients[rpcUrl] = createViemPublicClient(env.CHAIN_ID, rpcUrl)
