702990334219931ee098ed0f3c21673bd4046387
norswap <norswap@gmail.com>
2025-06-21 03:59:13 +0200

    Apply suggestions from code review

--- packages/core/build.config.ts
+++ packages/core/build.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig, inlineCssPlugin } from "@happy.tech/happybuild"
 
-if (!process.env.HAPPY_IFRAME_URL || process.env.HAPPY_IFRAME_URL.includes("localhost")) {
-    throw new Error(`${process.env.HAPPY_IFRAME_URL} is not a valid HAPPY_IFRAME_URL. It should be a production URL.`)
-}
-
 export default defineConfig({
     bunConfig: {
         define: {
-            // When building, `import.meta.env` is not defined. We use it such that monorepo demos can
-            // run in Vite (where `process.env` is not defined) without building. These definitions makes `import.meta.env`
-            // variables also available when building.
+            // Inline environment variable definition when building — they would otherwise not be available.
+            // In dev, the environment variables from the importing monorepo package are used instead.
+            // (This is unfortunate, we would rather read from this package's `.env` file but this is not trivially achievable.)
             "import.meta.env.HAPPY_IFRAME_URL": process.env.HAPPY_IFRAME_URL as string,
             "import.meta.env.HAPPY_RPC_OVERRIDE": process.env.HAPPY_RPC_OVERRIDE || "''",
             "import.meta.env.PROD": "true",
