de963989bf02a49a0d6bd4194884d347ccada995
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-06-21 08:39:26 +0200

    debounce counter + add counter for spinner

--- demos/react/src/demo-components/ContractInteractionDemo.tsx
+++ demos/react/src/demo-components/ContractInteractionDemo.tsx
@@ -1,14 +1,15 @@
 import { connect, getCurrentUser } from "@happy.tech/core"
 import { useHappyWallet } from "@happy.tech/react"
 import { ArrowCounterClockwiseIcon, CalculatorIcon, CoinsIcon, SpinnerIcon } from "@phosphor-icons/react"
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
+import { type Root, createRoot } from "react-dom/client"
 import { toast } from "sonner"
 import { type Address, formatUnits } from "viem"
 import { publicClient, walletClient } from "../clients"
 import { abis, deployment } from "../deployments"
 
 export const ContractInteractionDemo = () => {
-    const { counter, isLoading, increment, reset } = useCounterActions()
+    const { count, isLoading, increment, reset } = useCounterActions()
     const { mint } = useTestTokenActions()
 
     return (
@@ -47,8 +48,8 @@ export const ContractInteractionDemo = () => {
             {/* Counter Display */}
             <div className="col-span-2 text-center mt-2">
                 <pre className="text-sm font-semibold">Current counter value</pre>
-                {counter !== undefined ? (
-                    <div className="text-4xl font-bold">{counter.toString()}</div>
+                {count !== undefined ? (
+                    <div className="text-4xl font-bold">{count.toString()}</div>
                 ) : isLoading ? (
                     <SpinnerIcon className="animate-spin mx-auto" size="2.25rem" />
                 ) : (
@@ -60,53 +61,70 @@ export const ContractInteractionDemo = () => {
 }
 
 function useCounterActions() {
-    const [counter, setCounter] = useState<bigint | undefined>()
-    const [isLoading, setLoading] = useState(false)
     const { user } = useHappyWallet()
+    const countRef = useRef<bigint | undefined>(undefined)
+    const [count, setCount_] = useState<bigint | undefined>()
+    const [isLoading, setLoading] = useState(false) // initial load
+    const txsSent = useRef(0)
+    const txsSettled = useRef(0)
+    const timeout = useRef<NodeJS.Timeout | undefined>(undefined)
+    const spinner = useRef<HTMLDivElement | null>(null)
+    const spinnerRoot = useRef<Root | null>(null)
 
-    const refetch = useCallback(async (account: Address) => {
-        const count = await publicClient.readContract({
-            address: deployment.HappyCounter,
-            abi: abis.HappyCounter,
-            functionName: "getCount",
-            account,
-        })
-        setCounter(count)
-        return count
+    const setCount = useCallback((count_: bigint | undefined) => {
+        countRef.current = count_
+        setCount_(count_)
     }, [])
+
+    const refetch = useCallback(
+        async (account: Address) => {
+            const onchainCount = await publicClient.readContract({
+                address: deployment.HappyCounter,
+                abi: abis.HappyCounter,
+                functionName: "getCount",
+                account,
+            })
+            if (onchainCount > (countRef.current ?? 0n)) setCount(onchainCount)
+            return onchainCount
+        },
+        [setCount],
+    )
 
     // Sync counter value with current active user
     useEffect(() => {
         setLoading(true)
         const updateCounterValue = async () => {
-            if (!user?.address) return setCounter(undefined)
+            if (!user?.address) return setCount(undefined)
             await refetch(user?.address)
         }
 
         updateCounterValue().finally(() => setLoading(false))
-    }, [user, refetch])
+    }, [user, refetch, setCount])
 
-    let localCounter = counter ?? 0n
     async function increment() {
         try {
+            updateInFlightSpinner()
             const hash = await walletClient.writeContract({
                 address: deployment.HappyCounter,
                 abi: abis.HappyCounter,
                 functionName: "increment",
             })
-
             if (!hash) throw new Error("No hash returned")
+
             const receipt = await publicClient.waitForTransactionReceipt({ hash })
             if (receipt.status === "reverted") {
                 toast.error("Boop reverted!")
                 return
             }
 
-            localCounter += 1n
-            await refetch(getCurrentUser()!.address)
-            toast.success(`Counter incremented to ${localCounter}`)
+            if (countRef.current) setCount(countRef.current + 1n)
+            toast.success(`Counter incremented to ${countRef.current}`)
+            void refetch(getCurrentUser()!.address)
         } catch {
             toast.error("Failed to increment counter")
+        } finally {
+            txsSettled.current++
+            updateInFlightSpinner()
         }
     }
     async function reset() {
@@ -123,16 +141,50 @@ function useCounterActions() {
                 return
             }
 
-            await refetch(getCurrentUser()!.address)
+            setCount(0n)
             toast.success("Counter reset to 0")
+            void refetch(getCurrentUser()!.address)
         } catch {
             toast.error("Failed to reset counter")
         }
     }
 
+    function updateInFlightSpinner() {
+        function showSpinner() {
+            if (spinner.current) return
+            spinner.current = document.createElement("div")
+            document.body.appendChild(spinner.current)
+            spinnerRoot.current = createRoot(spinner.current)
+            spinnerRoot.current.render(
+                <div className="fixed bottom-4 left-4 pointer-events-none">
+                    <div className="backdrop-blur-sm bg-gray-200/35 p-4 rounded-xl">
+                        <SpinnerIcon className="animate-spin mx-auto" size="2.25rem" />
+                    </div>
+                </div>,
+            )
+        }
+
+        function hideSpinner() {
+            if (!spinner.current || !spinnerRoot.current) return
+            spinnerRoot.current.unmount()
+            document.body.removeChild(spinner.current)
+            spinnerRoot.current = null
+            spinner.current = null
+        }
+
+        clearTimeout(timeout.current)
+        if (txsSent.current > txsSettled.current) {
+            showSpinner()
+            timeout.current = setTimeout(() => hideSpinner, 15_000)
+        } else {
+            console.log("hiding")
+            hideSpinner()
+        }
+    }
+
     return {
         isLoading,
-        counter,
+        count,
         refetch,
         increment,
         reset,
