0da517ed08fe3ffe25fb11c1114281b59e4b0416
aodhgan <gawnieg@gmail.com>
2025-06-23 09:58:52 -0700

    add posthog

--- apps/docs/src/layout.tsx
+++ apps/docs/src/layout.tsx
@@ -1,10 +1,24 @@
 import { HappyWalletProvider } from "@happy.tech/react"
 import type { PropsWithChildren } from "react"
+import posthog from "posthog-js"
+import { PostHogProvider } from "posthog-js/react"
+
+if (typeof window !== "undefined") {
+    const posthogKey = import.meta.env.HAPPY_POSTHOG_PUB_KEY
+    const posthogHost = import.meta.env.HAPPY_POSTHOG_HOST
+    if (!posthogKey || !posthogHost) {
+        throw new Error("Environment variables HAPPY_POSTHOG_PUB_KEY and HAPPY_POSTHOG_HOST must be defined.")
+    }
+    posthog.init(posthogKey, {
+        api_host: posthogHost,
+        autocapture: true,
+    })
+}
 
 export default function Layout({ children }: PropsWithChildren) {
     return (
-        <div>
+        <PostHogProvider client={posthog}>
             <HappyWalletProvider>{children}</HappyWalletProvider>
-        </div>
+        </PostHogProvider>
     )
 }
