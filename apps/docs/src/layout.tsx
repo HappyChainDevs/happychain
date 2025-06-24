8d708ab673df3d87139c435e7b51726c922a771f
Nicolas "Norswap" Laurent <norswap@gmail.com>
2025-06-24 03:35:02 +0200

    don't do posthog if the variables are not defined

--- apps/docs/src/layout.tsx
+++ apps/docs/src/layout.tsx
@@ -1,18 +1,12 @@
 import { HappyWalletProvider } from "@happy.tech/react"
-import type { PropsWithChildren } from "react"
 import posthog from "posthog-js"
 import { PostHogProvider } from "posthog-js/react"
+import type { PropsWithChildren } from "react"
 
 if (typeof window !== "undefined") {
     const posthogKey = import.meta.env.HAPPY_POSTHOG_PUB_KEY
     const posthogHost = import.meta.env.HAPPY_POSTHOG_HOST
-    if (!posthogKey || !posthogHost) {
-        throw new Error("Environment variables HAPPY_POSTHOG_PUB_KEY and HAPPY_POSTHOG_HOST must be defined.")
-    }
-    posthog.init(posthogKey, {
-        api_host: posthogHost,
-        autocapture: true,
-    })
+    if (posthogKey && posthogHost) posthog.init(posthogKey, { api_host: posthogHost, autocapture: true })
 }
 
 export default function Layout({ children }: PropsWithChildren) {
